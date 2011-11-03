c6a4e0669b1f24f7334ebdb6b1a0df0f72bb3ac7
Marta Girdea <marta.girdea@gmail.com>
2011-11-02 21:14:47 -0400

    [cleanup] whitespace cleanup in js source file

--- hpo-search/src/main/webapp/ontology-browser.js
+++ hpo-search/src/main/webapp/ontology-browser.js
@@ -12,10 +12,10 @@ var MS = (function (MS) {
     resultParent : {
       selector : 'arr[name=is_a] str',
       processingFunction : function (text){
-	var data = {};
-	data.id = text.replace(/\s+/gm, ' ').replace(/(HP:[0-9]+)\s*!\s*(.*)/m, "$1");
-	data.value = text.replace(/\s+/gm, ' ').replace(/(HP:[0-9]+)\s*!\s*(.*)/m, "$2");
-	return data;
+        var data = {};
+        data.id = text.replace(/\s+/gm, ' ').replace(/(HP:[0-9]+)\s*!\s*(.*)/m, "$1");
+        data.value = text.replace(/\s+/gm, ' ').replace(/(HP:[0-9]+)\s*!\s*(.*)/m, "$2");
+        return data;
       }
     },
     noresults: "No sub-terms",
@@ -26,7 +26,7 @@ var MS = (function (MS) {
                             'is_a' : { 'stub': false, 'activationRegex' : 'HP:[0-9]+' }
                          })
   },
-  
+
   initialize: function(suggest, dialog, options) {
     this.options = Object.extend(Object.clone(this.options), options || { });
     this.suggest = suggest;
@@ -35,14 +35,14 @@ var MS = (function (MS) {
       this.dialog = dialog;
     } else {
       this.dialog = new MS.widgets.ModalPopup(this.loadingMessage, {}, {
-	title : "Related terms", 
-	titleColor: "#333", 
-	borderColor: "#cedeee",
-	verticalPosition : "top"
+        title : "Related terms",
+        titleColor: "#333",
+        borderColor: "#cedeee",
+        verticalPosition : "top"
       });
     }
   },
-  
+
   load : function(id) {
       this.setDialogContent(this.loadingMessage);
       var query = id;
@@ -59,13 +59,13 @@ var MS = (function (MS) {
         onSuccess: function (response) {
           //var tree = this.buildTree(response.responseXML);
           this.setDialogContent(this.buildTree(response.responseXML));
-	}.bind(this),
+        }.bind(this),
         onFailure: function (response) {
           this.setDialogContent("Failed to retrieve data : " + respose.statusText);
         }.bind(this)
       });
   },
-  
+
   expand : function (element, doPopulate) {
       var query = element.__termId;
       if (this.options.expandQueryProcessor != null && typeof(this.options.expandQueryProcessor.processQuery) == "function") {
@@ -74,27 +74,27 @@ var MS = (function (MS) {
       var url = this.options.script + this.options.varname + "=" + encodeURIComponent(query);
       var headers = {};
       headers.Accept = this.options.responseFormat;
-      
+
       this._lockExpandTool(element);
 
       var ajx = new Ajax.Request(url, {
         method: this.options.method,
         requestHeaders: headers,
         onSuccess: function (response) {
-	  var memo = {};
-	  if (doPopulate) {
-	    memo.data = this.buildDescendentsList(response.responseXML);
-	  } else {
-	    memo.count = this.countDescendents(response.responseXML);
-	  }
+          var memo = {};
+          if (doPopulate) {
+            memo.data = this.buildDescendentsList(response.responseXML);
+          } else {
+            memo.count = this.countDescendents(response.responseXML);
+          }
           Event.fire(element, 'obrowser:expand:done', memo);
-	}.bind(this),
+        }.bind(this),
         onFailure: function (response) {
           Event.fire(element, 'obrowser:expand:failed', {data: new Element('div', {'class' : 'error'}).update("Failed to retrieve data : " + respose.statusText), count: -1});
         }
       });
   },
-  
+
   buildTree : function (xml) {
     var results = xml.getElementsByTagName(this.options.resultsParameter);
     if (results.length == 0) {
@@ -123,21 +123,21 @@ var MS = (function (MS) {
     this._toggleExpandState(root);
     return newContent;
   },
-  
+
   countDescendents : function(xml) {
     return xml.getElementsByTagName(this.options.resultsParameter).length;
   },
-  
+
   buildDescendentsList : function(xml) {
     var results = xml.getElementsByTagName(this.options.resultsParameter);
     var list = new Element('ul', {'class' : 'descendents'});
     for (var i = 0; i < results.length; i++) {
       if (results[i].hasChildNodes()) {
-	var data = {
+        var data = {
           id : Element.down(results[i], this.options.resultId).firstChild.nodeValue,
           value : Element.down(results[i], this.options.resultValue).firstChild.nodeValue
          };
-	 list.insert({'bottom': this._createDescendentBranch(data)});
+         list.insert({'bottom': this._createDescendentBranch(data)});
       }
     }
     if (list.hasChildNodes()) {
@@ -145,31 +145,31 @@ var MS = (function (MS) {
     }
     return new Element('div', {'class' : 'descendents hint empty'}).update(this.options.noresults);
   },
-  
+
   _createBranch: function (eltName, className, data, expandable) {
     var element =  new Element(eltName, {'class' : 'entry ' + className});
     element.__termId = data.id;
     var wrapper = new Element('div', {'class' : 'entry-data'});
-    wrapper.insert({'top': 
-		   new Element('span', {'class' : 'info'}).insert(
-		     {'bottom' : new Element('span', {'class' : 'key'}).update('[' + data.id + ']')}).insert({'bottom' : ' '}).insert(
-		     {'bottom' : new Element('span', {'class' : 'value'}).update(data.value)})
+    wrapper.insert({'top':
+                   new Element('span', {'class' : 'info'}).insert(
+                     {'bottom' : new Element('span', {'class' : 'key'}).update('[' + data.id + ']')}).insert({'bottom' : ' '}).insert(
+                     {'bottom' : new Element('span', {'class' : 'value'}).update(data.value)})
     });
     wrapper.insert({'bottom':
-		   new Element('span', {'class' : 'entry-tools'}).insert(
-		     {'bottom' : this._createTool('&#x260c;', 'browse-tool', "Browse related terms", this._browseEntry)}).insert(
-		     {'bottom' : this._createTool('i', 'info-tool', "Information about this term", this._showEntryInfo)}).insert(
-		     {'bottom' : this._createTool('&#x2713;', 'accept-tool', "Add this phenotype", this._acceptEntry)})
+                   new Element('span', {'class' : 'entry-tools'}).insert(
+                     {'bottom' : this._createTool('&#x260c;', 'browse-tool', "Browse related terms", this._browseEntry)}).insert(
+                     {'bottom' : this._createTool('i', 'info-tool', "Information about this term", this._showEntryInfo)}).insert(
+                     {'bottom' : this._createTool('&#x2713;', 'accept-tool', "Add this phenotype", this._acceptEntry)})
     });
     wrapper.down('.info').observe('click', this._acceptEntry.bindAsEventListener(this));
     element.update(wrapper);
     if (expandable) {
       var expandTool = new Element('span', {'class' : 'expand-tool'}).update(this._getExpandCollapseSymbol(true));
       expandTool.observe('click', function(event) {
-	var entry = event.element().up('.entry');
-	if (!this._isExpandToolLocked(entry)){
-	  this._toggleExpandState(entry);
-	}
+        var entry = event.element().up('.entry');
+        if (!this._isExpandToolLocked(entry)){
+          this._toggleExpandState(entry);
+        }
       }.bindAsEventListener(this));
       wrapper.insert({'top': expandTool});
       this.expand(element, element.hasClassName('root'));
@@ -181,24 +181,24 @@ var MS = (function (MS) {
     }
     return element;
   },
-  
+
   _toggleExpandState : function(target) {
     if (target) {
       if (target.down('.descendents')) {
         target.toggleClassName('collapsed');
       } else {
         target.removeClassName('collapsed');
-	if (target.down(".error")) {
-	  target.down(".error").remove();
-	}
+        if (target.down(".error")) {
+          target.down(".error").remove();
+        }
         this.expand(target, true);
-	target.observe('obrowser:expand:done', this._obrowserExpandEventHandler.bindAsEventListener(this));
-	target.observe('obrowser:expand:failed', this._obrowserExpandEventHandler.bindAsEventListener(this));
+        target.observe('obrowser:expand:done', this._obrowserExpandEventHandler.bindAsEventListener(this));
+        target.observe('obrowser:expand:failed', this._obrowserExpandEventHandler.bindAsEventListener(this));
       }
       target.down('.expand-tool').update(this._getExpandCollapseSymbol(target.hasClassName("collapsed")));
     }
   },
-  
+
   _obrowserExpandEventHandler : function(event) {
      var element = event.element();
      if (!event.memo) {
@@ -219,35 +219,35 @@ var MS = (function (MS) {
     element.stopObserving('obrowser:expand:failed');
     this._unlockExpandTool(element);
   },
-  
+
   _lockExpandTool : function(element) {
     var expandTool = element.down('.expand-tool');
     if (expandTool) {
       expandTool.addClassName('locked');
     }
   },
-  
+
   _unlockExpandTool : function(element) {
     var expandTool = element.down('.expand-tool');
     if (expandTool) {
       expandTool.removeClassName('locked');
     }
   },
-  
+
   _isExpandToolLocked : function(element) {
     if (element.down('.expand-tool.locked')) {
       return true;
     }
     return false;
   },
-  
+
   _getExpandCollapseSymbol : function (isCollapsed) {
     if (isCollapsed) {
       return "&#x25ba;";
     }
     return "&#x25bc;";
   },
-  
+
   _createTool : function (text, className, title, method) {
     var element = new Element('span', {'class' : 'entry-tool ' + className, "title" : title}).update(text);
     element.observe('click', method.bindAsEventListener(this));
@@ -274,32 +274,32 @@ var MS = (function (MS) {
     var elt = event.element().up('.entry');
     this.load(elt.__termId);
   },
-  
+
   _createParentBranch: function (parent) {
     var parent = this._createBranch('li', 'parent', parent, false);
     //parent
     return parent;
   },
-  
+
   _createRoot : function (data) {
     return this._createBranch('div', 'root', data, true);
   },
-  
+
   _createDescendentBranch : function(data) {
     return this._createBranch('li', 'descendent', data, true);
   },
-  
+
   setDialogContent : function(content) {
     this.dialog.setContent(new Element('div', {'class' : 'ontology-tree'}).update(content));
   },
-  
+
   show : function(id) {
     if (id) {
       this.dialog.showDialog();
       this.load (id);
     }
   },
-  
+
   hide : function() {
     this.dialog.closeDialog();
   }
