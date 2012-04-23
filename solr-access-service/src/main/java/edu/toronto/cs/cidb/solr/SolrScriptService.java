60d25ab8915a2ae6ef04bf7bf122f61975105923
Marta Girdea <marta.girdea@gmail.com>
2012-04-23 15:58:31 -0400

    Issue #62: Sorting not supported by current implementation of the solr access service component

--- solr-access-service/src/main/java/edu/toronto/cs/cidb/solr/SolrScriptService.java
+++ solr-access-service/src/main/java/edu/toronto/cs/cidb/solr/SolrScriptService.java
@@ -30,6 +30,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
@@ -62,7 +63,9 @@ import edu.toronto.cs.cidb.obo2solr.TermData;
 @Singleton
 public class SolrScriptService implements ScriptService, Initializable
 {
-    /** Delimiter between the field name and the searched value used in the Lucene query language. */
+    /**
+     * Delimiter between the field name and the searched value used in the Lucene query language.
+     */
     private static final String FIELD_VALUE_SEPARATOR = ":";
 
     /** Logging helper object. */
@@ -98,6 +101,19 @@ public class SolrScriptService implements ScriptService, Initializable
      * Search for HPO terms matching the specified query, using the Lucene query language.
      * 
      * @param queryParameters a Lucene query
+     * @param sort sorting criteria
+     * @return the list of matching documents, empty if there are no matching terms
+     */
+    public SolrDocumentList search(final String queryParameters, final String sort)
+    {
+        MapSolrParams params = new MapSolrParams(getSolrQuery(queryParameters, sort, -1, 0));
+        return search(params);
+    }
+
+    /**
+     * Search for HPO terms matching the specified query, using the Lucene query language.
+     *
+     * @param queryParameters a Lucene query
      * @param rows the number of items to return, or -1 to use the default number of results
      * @param start the number of items to skip, i.e. the index of the first hit to return, 0-based
      * @return the list of matching documents, empty if there are no matching terms
@@ -105,6 +121,21 @@ public class SolrScriptService implements ScriptService, Initializable
     public SolrDocumentList search(final String queryParameters, final int rows, final int start)
     {
         MapSolrParams params = new MapSolrParams(getSolrQuery(queryParameters, rows, start));
+        return search(params);
+    }
+
+    /**
+     * Search for HPO terms matching the specified query, using the Lucene query language.
+     *
+     * @param queryParameters a Lucene query
+     * @param sort sorting criteria
+     * @param rows the number of items to return, or -1 to use the default number of results
+     * @param start the number of items to skip, i.e. the index of the first hit to return, 0-based
+     * @return the list of matching documents, empty if there are no matching terms
+     */
+    public SolrDocumentList search(final String queryParameters, final String sort, final int rows, final int start)
+    {
+        MapSolrParams params = new MapSolrParams(getSolrQuery(queryParameters, sort, rows, start));
         return search(params);
     }
 
@@ -126,7 +157,21 @@ public class SolrScriptService implements ScriptService, Initializable
      * keywords.
      * 
      * @param fieldValues the map of values to search for, where each key is the name of an indexed field and the value
-     *        is the keywords to match for that field
+     *            is the keywords to match for that field
+     * @param sort sorting criteria
+     * @return the list of matching documents, empty if there are no matching terms
+     */
+    public SolrDocumentList search(final Map<String, String> fieldValues, String sort)
+    {
+        return search(fieldValues, sort, -1, 0);
+    }
+
+    /**
+     * Search for HPO terms matching the specified query, where the query is specified as a map of field name and
+     * keywords.
+     *
+     * @param fieldValues the map of values to search for, where each key is the name of an indexed field and the value
+     *            is the keywords to match for that field
      * @param rows the number of items to return, or -1 to use the default number of results
      * @param start the number of items to skip, i.e. the index of the first hit to return, 0-based
      * @return the list of matching documents, empty if there are no matching terms
@@ -134,6 +179,24 @@ public class SolrScriptService implements ScriptService, Initializable
     public SolrDocumentList search(final Map<String, String> fieldValues, final int rows, final int start)
     {
         MapSolrParams params = new MapSolrParams(getSolrQuery(fieldValues, rows, start));
+        return search(params);
+    }
+
+    /**
+     * Search for HPO terms matching the specified query, where the query is specified as a map of field name and
+     * keywords.
+     *
+     * @param fieldValues the map of values to search for, where each key is the name of an indexed field and the value
+     *            is the keywords to match for that field
+     * @param sort sorting criteria
+     * @param rows the number of items to return, or -1 to use the default number of results
+     * @param start the number of items to skip, i.e. the index of the first hit to return, 0-based
+     * @return the list of matching documents, empty if there are no matching terms
+     */
+    public SolrDocumentList search(final Map<String, String> fieldValues, final String sort, final int rows,
+        final int start)
+    {
+        MapSolrParams params = new MapSolrParams(getSolrQuery(fieldValues, sort, rows, start));
         return search(params);
     }
 
@@ -248,10 +311,12 @@ public class SolrScriptService implements ScriptService, Initializable
             SolrDocumentList results = response.getResults();
             if (results.size() == 0 && !response.getSpellCheckResponse().isCorrectlySpelled()) {
                 String suggestedQuery = response.getSpellCheckResponse().getCollatedResult();
-                // The spellcheck doesn't preserve the identifiers, manually correct this
+                // The spellcheck doesn't preserve the identifiers, manually
+                // correct this
                 suggestedQuery = suggestedQuery.replaceAll("term_category:hip", "term_category:HP");
-                MapSolrParams newParams = new MapSolrParams(getSolrQuery(
-                    suggestedQuery, params.getInt(CommonParams.ROWS, -1), params.getInt(CommonParams.START, 0)));
+                MapSolrParams newParams =
+                    new MapSolrParams(getSolrQuery(suggestedQuery, params.get(CommonParams.SORT), params.getInt(
+                        CommonParams.ROWS, -1), params.getInt(CommonParams.START, 0)));
                 return this.server.query(newParams).getResults();
             } else {
                 return results;
@@ -275,6 +340,23 @@ public class SolrScriptService implements ScriptService, Initializable
      */
     private Map<String, String> getSolrQuery(Map<String, String> fieldValues, int rows, int start)
     {
+        return getSolrQuery(fieldValues, "", rows, start);
+    }
+
+    /**
+     * Create Solr parameters based on the specified search terms. More specifically, concatenates the specified field
+     * values into a Lucene query which is used as the "q" parameter, and adds parameters for requesting a spellcheck
+     * result.
+     *
+     * @param fieldValues the map of values to search for, where each key is the name of an indexed field and the value
+     *            is the keywords to match for that field
+     * @param sort the sort criteria ("fiel_name order')
+     * @param rows the number of items to return, or -1 to use the default number of results
+     * @param start the number of items to skip, i.e. the index of the first hit to return, 0-based
+     * @return a map of Solr query parameter ready to be used for constructing a {@link MapSolrParams} object
+     */
+    private Map<String, String> getSolrQuery(Map<String, String> fieldValues, String sort, int rows, int start)
+    {
         StringBuilder query = new StringBuilder();
         for (Map.Entry<String, String> field : fieldValues.entrySet()) {
             String value = field.getValue();
@@ -287,7 +369,7 @@ public class SolrScriptService implements ScriptService, Initializable
                 query.append(field.getKey()).append(FIELD_VALUE_SEPARATOR).append(val).append(" ");
             }
         }
-        return getSolrQuery(query.toString().trim(), rows, start);
+        return getSolrQuery(query.toString().trim(), sort, rows, start);
     }
 
     /**
@@ -301,12 +383,30 @@ public class SolrScriptService implements ScriptService, Initializable
      */
     private Map<String, String> getSolrQuery(String query, int rows, int start)
     {
+        return getSolrQuery(query, "", rows, start);
+    }
+
+    /**
+     * Convert a Lucene query string into a map of Solr parameters. More specifically, places the input query under the
+     * "q" parameter, and adds parameters for requesting a spellcheck result.
+     *
+     * @param query the lucene query string to use
+     * @param sort the sort criteria ("fiel_name order')
+     * @param rows the number of items to return, or -1 to use the default number of results
+     * @param start the number of items to skip, i.e. the index of the first hit to return, 0-based
+     * @return a map of Solr query parameter ready to be used for constructing a {@link MapSolrParams} object
+     */
+    private Map<String, String> getSolrQuery(String query, String sort, int rows, int start)
+    {
         Map<String, String> result = new HashMap<String, String>();
         result.put(CommonParams.START, start + "");
         if (rows > 0) {
             result.put(CommonParams.ROWS, rows + "");
         }
         result.put(CommonParams.Q, query);
+        if (!StringUtils.isBlank(sort)) {
+            result.put(CommonParams.SORT, sort);
+        }
         result.put("spellcheck", Boolean.toString(true));
         result.put("spellcheck.collate", Boolean.toString(true));
         result.put("spellcheck.onlyMorePopular", Boolean.toString(true));
