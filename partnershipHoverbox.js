be8ffe05bbb22940ae959663e5177046d695aa1f
Michael <michael.kozakov@gmail.com>
2012-08-20 23:22:49 -0400

    Removing a node by clicking on the Delete button is always recursive

--- partnershipHoverbox.js
+++ partnershipHoverbox.js
@@ -35,8 +35,7 @@ var PartnershipHoverbox = Class.create(AbstractHoverbox, {
     generateButtons: function($super) {
         var me = this;
         var action = function() {
-            var confirmation = confirm("Are you sure you want to delete this partnership?");
-            confirmation && me.getNode().remove(false, true)
+            me.getNode().remove(true);
         };
         var path = "M24.778,21.419 19.276,15.917 24.777,10.415 21.949,7.585 16.447,13.087 10.945,7.585 8.117,10.415 13.618,15.917 8.116,21.419 10.946,24.248 16.447,18.746 21.948,24.248z";
         var attributes = editor.attributes.deleteBtnIcon;
