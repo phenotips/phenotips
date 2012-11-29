3d63b0035351cb4ff65e789dcf4014dc5e4f4917
Michael <michael.kozakov@gmail.com>
2012-11-28 20:19:36 -0500

    Fixes #337.
    Adds undo/redo for node removal

--- abstractNode.js
+++ abstractNode.js
@@ -139,12 +139,28 @@ var AbstractNode = Class.create( {
     remove: function(isRecursive) {
         var me = this;
         var toRemove = [];
+        var affectedNeighbors = [];
         if(isRecursive) {
             toRemove.push(me);
             this.getNeighbors().each(function(neighbor) {
                 var result = neighbor.isRelatedTo(editor.getGraph().getProband(), toRemove.clone());
                 if(!result[0]) {
                     toRemove = result[1];
+                }
+                else if(neighbor.getType() == "Partnership") {
+                    affectedNeighbors.push(neighbor);
+                    neighbor.getPregnancies().each(function(preg) {
+                        affectedNeighbors.push(preg);
+                        if(preg.isPlaceHolderPregnancy()) {
+                            affectedNeighbors = affectedNeighbors.concat(preg.getChildren());
+                        }
+                    });
+                }
+                else if(neighbor.getType() == "Pregnancy") {
+                    affectedNeighbors.push(neighbor);
+                    if(neighbor.isPlaceHolderPregnancy()) {
+                        affectedNeighbors = affectedNeighbors.concat(neighbor.getChildren());
+                    }
                 }
             });
             toRemove.each(function(node) {
@@ -158,9 +174,29 @@ var AbstractNode = Class.create( {
                 confirmation = confirm("Removing this person will also remove all the related connections. Are you sure you want to proceed?");
             }
             if(confirmation) {
-                toRemove.reverse().each(function(node) {
-                    node && node.remove(false);
+                var nodes = {
+                    PersonNodes : [],
+                    PartnershipNodes : [],
+                    PregnancyNodes : [],
+                    PersonGroupNodes : [],
+                    PlaceHolderNodes : []
+                };
+                toRemove.concat(affectedNeighbors).each(function(node) {
+                    nodes[node.getType() + "Nodes"].push(node.getInfo());
                 });
+                var nodeID = me.getID();
+                var placeholders = [];
+                toRemove.reverse().each(function(node) {
+                    if(node) {
+                        if(node.getID() == nodeID) {
+                            var ph = node.remove(false);
+                            ph && placeholders.push(ph.getInfo());
+                        }
+                        else
+                            node.remove(false);
+                    }
+                });
+                this.removeAction(nodes, placeholders);
             }
             else {
                 toRemove.each(function(node) {
@@ -168,6 +204,105 @@ var AbstractNode = Class.create( {
                 });
             }
         }
+    },
+
+    removeAction: function(removed, placeholders) {
+        var phPartnerships = [],
+            phPregnancies = [];
+        placeholders.each(function(ph) {
+            var node = editor.getGraph().getNodeMap()[ph.id];
+            if(node) {
+                var parentPreg = node.getParentPregnancy();
+                parentPreg && phPregnancies.push(parentPreg.getInfo());
+                node.getPartnerships().each(function(p) {
+                    phPartnerships.push(p.getInfo());
+                    p.getPregnancies().each(function(preg) {
+                        phPregnancies.push(preg.getInfo());
+                    });
+                });
+            }
+        });
+        var undoFunct = function() {
+            placeholders.each(function(ph) {
+                var node = editor.getGraph().getNodeMap()[ph.id];
+                node && node.remove(false);
+            });
+            removed.PersonNodes.each(function(p) {
+                editor.getGraph().addPerson(p.x, p.y, p.gender, p.id).loadInfo(p);
+            });
+
+            removed.PlaceHolderNodes.each(function(p) {
+                editor.getGraph().addPlaceHolder(p.x, p.y, p.gender, p.id).loadInfo(p);
+            });
+
+            removed.PersonGroupNodes.each(function(p) {
+                editor.getGraph().addPersonGroup(p.x, p.y, p.id).loadInfo(p);
+            });
+
+            removed.PartnershipNodes.each(function(p) {
+                var p1 = editor.getGraph().getNodeMap()[p.partner1ID];
+                var p2 = editor.getGraph().getNodeMap()[p.partner2ID];
+                if(p1 && p2)
+                    editor.getGraph().addPartnership(p.x, p.y, p1, p2, p.id).loadInfo(p);
+            });
+
+            removed.PregnancyNodes.each(function(p) {
+                var partnership = editor.getGraph().getNodeMap()[p.partnershipID];
+                if(partnership)
+                    editor.getGraph().addPregnancy(p.x, p.y, partnership, p.id).loadInfo(p);
+            });
+        };
+
+        var redoFunct = function() {
+            removed.PregnancyNodes.each(function(p) {
+                var node = editor.getGraph().getNodeMap()[p.id];
+                node && node.remove(false);
+            });
+
+            removed.PartnershipNodes.each(function(p) {
+                var node = editor.getGraph().getNodeMap()[p.id];
+                node && node.remove(false);
+            });
+
+            removed.PersonGroupNodes.each(function(p) {
+                var node = editor.getGraph().getNodeMap()[p.id];
+                node && node.remove(false);
+            });
+
+            removed.PlaceHolderNodes.each(function(p) {
+                var node = editor.getGraph().getNodeMap()[p.id];
+                node && node.remove(false);
+            });
+
+            removed.PersonNodes.each(function(p) {
+                var node = editor.getGraph().getNodeMap()[p.id];
+                node && node.remove(false);
+            });
+
+            placeholders.each(function(p) {
+                editor.getGraph().addPlaceHolder(p.x, p.y, p.gender, p.id).loadInfo(p);
+            });
+
+            phPartnerships.each(function(p) {
+                var p1 = editor.getGraph().getNodeMap()[p.partner1ID];
+                var p2 = editor.getGraph().getNodeMap()[p.partner2ID];
+                if(p1 && p2) {
+                    editor.getGraph().addPartnership(p.x, p.y, p1, p2, p.id).loadInfo(p);
+                }
+            });
+
+            phPregnancies.each(function(p) {
+                var partnership = editor.getGraph().getNodeMap()[p.partnershipID];
+                if(partnership) {
+                    var preg = editor.getGraph().addPregnancy(p.x, p.y, partnership, p.id).loadInfo(p);
+                    p.childrenIDs.each(function(id){
+                        var child = editor.getGraph().getNodeMap()[id].loadInfo(p);
+                        child && preg.addChild(child);
+                    });
+                }
+            });
+        };
+        editor.getActionStack().push({undo: undoFunct, redo: redoFunct});
     },
 
     isRelatedTo: function(node, visited) {
