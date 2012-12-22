39fcb2c09a34bd09b6f9c19076de27611fa96bf3
Sergiu Dumitriu <sergiu@xwiki.org>
2012-12-21 19:07:00 -0500

    Issue #107: Improve diagnosis speed by using Solr Converted the differential diagnosis service to Solr

--- solr-access-service/src/main/java/edu/toronto/cs/cidb/solr/OmimScriptService.java
+++ solr-access-service/src/main/java/edu/toronto/cs/cidb/solr/OmimScriptService.java
@@ -19,10 +19,32 @@
  */
 package edu.toronto.cs.cidb.solr;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.common.params.MapSolrParams;
+import org.apache.solr.common.util.SimpleOrderedMap;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.script.service.ScriptService;
+
+import edu.toronto.cs.cidb.obo2solr.maps.CounterMap;
+import edu.toronto.cs.cidb.obo2solr.maps.SumMap;
 
 /**
  * Provides access to the Solr server, with the main purpose of providing access to the HPO ontology, and secondary
@@ -37,9 +59,66 @@ import org.xwiki.component.annotation.Component;
 @Singleton
 public class OmimScriptService extends AbstractSolrScriptService
 {
+    @Inject
+    @Named("hpo")
+    private ScriptService service;
+
     @Override
     protected String getName()
     {
         return "omim";
+    }
+
+    public List<SearchResult> getDifferentialPhenotypes(Collection<String> phenotypes, int limit)
+    {
+        Map<String, String> params = new HashMap<String, String>();
+        HPOScriptService hpoService = (HPOScriptService) this.service;
+        params.put(CommonParams.Q, "symptom:" + StringUtils.join(phenotypes, " symptom:").replaceAll("HP:", "HP\\\\:"));
+        params.put(CommonParams.ROWS, "100");
+        params.put(CommonParams.START, "0");
+        params.put(CommonParams.DEBUG_QUERY, "true");
+        params.put(CommonParams.EXPLAIN_STRUCT, "true");
+
+        MapSolrParams solrParams = new MapSolrParams(params);
+        QueryResponse response;
+        List<SearchResult> result = new LinkedList<SearchResult>();
+        try {
+            response = this.server.query(solrParams);
+        } catch (SolrServerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return result;
+        }
+        SolrDocumentList matchingDisorders = response.getResults();
+        Map< ? , ? > explanations = response.getExplainMap();
+        SumMap<String> cummulativeScore = new SumMap<String>();
+        CounterMap<String> matchCounter = new CounterMap<String>();
+        Set<String> allAncestors = new HashSet<String>();
+        for (String phenotype : phenotypes) {
+            allAncestors.addAll(hpoService.getAllAncestorsAndSelfIDs(phenotype));
+        }
+        for (SolrDocument disorder : matchingDisorders) {
+            String omimId = (String) disorder.getFieldValue(ID_FIELD_NAME);
+            float score = ((SimpleOrderedMap<Float>) explanations.get(omimId)).get("value");
+            for (Object hpoId : disorder.getFieldValues("actual_symptom")) {
+                if (allAncestors.contains(hpoId)) {
+                    continue;
+                }
+                cummulativeScore.addTo((String) hpoId, (double) score);
+                matchCounter.addTo((String) hpoId);
+            }
+        }
+        if (matchCounter.getMinValue() <= matchingDisorders.size() / 2) {
+            for (String hpoId : cummulativeScore.keySet()) {
+                SolrDocument term = hpoService.get(hpoId);
+                if (term == null) {
+                    continue;
+                }
+                result.add(new SearchResult(hpoId, (String) term.getFieldValue("name"), cummulativeScore.get(hpoId)
+                    / (matchCounter.get(hpoId) * matchCounter.get(hpoId))));
+            }
+            Collections.sort(result);
+        }
+        return result.subList(0, limit);
     }
 }
---
+++ solr-access-service/src/main/java/edu/toronto/cs/cidb/solr/SearchResult.java
@@ -0,0 +1,104 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package edu.toronto.cs.cidb.solr;
+
+public class SearchResult implements Comparable<SearchResult>
+{
+    private final String id;
+
+    private final String name;
+
+    private final double score;
+
+    public SearchResult(String id, String name, double score)
+    {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getId()
+    {
+        return this.id;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public double getScore()
+    {
+        return this.score;
+    }
+
+    @Override
+    public int compareTo(SearchResult other)
+    {
+        if (other == null) {
+            return 0;
+        }
+        return (int) -Math.signum(this.score - (other).getScore());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(this.score);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SearchResult other = (SearchResult) obj;
+        if (this.id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!this.id.equals(other.id)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.score) != Double.doubleToLongBits(other.score)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.id + '\t' + this.name + '\t' + this.score;
+    }
+}
