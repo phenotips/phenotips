1049a37d99cd1522ad34bf77e66433101cb59f61
Michael <michael.kozakov@gmail.com>
2013-04-24 16:49:18 -0400

    Edited  documentation for Legend

--- pedigree/src/main/resources/pedigree/legend.js
+++ pedigree/src/main/resources/pedigree/legend.js
@@ -1,6 +1,9 @@
-/*
+/**
  * Class responsible for keeping track of disorders and their properties.
  * This information is graphically displayed in a 'Legend' box
+ *
+ * @class Legend
+ * @constructor
  */
 
 var Legend = Class.create( {
@@ -29,67 +32,85 @@ var Legend = Class.create( {
         Droppables.add(editor.getWorkspace().canvas, {accept: 'disorder', onDrop: this._onDropDisorder.bind(this)});
     },
 
-    /*
-     * Returns an object of disorder ID's mapped to Disorder objects.
+    /**
+     * Returns a Hash of disorder ID's mapped to Disorder objects.
+     *
+     * @method getDisorders
+     * @return {Hash} PrototypeJS Hash object
      */
     getDisorders: function() {
         return this._disorders;
     },
 
-    /*
+    /**
      * Replaces the disorder Hash with the Hash passed in the parameter, and updates
      * the graphical 'Legend' to represent the new information
      *
-     * @param hash is a Hash object that has disorder IDs as keys and Disorder objects as values
+     * @method getDisorders
+     * @param {Hash} hash A PrototypeJS Hash with disorder IDs as keys and Disorder objects as values
      */
     setDisorders: function(hash) {
         this._disorders = hash;
     },
 
-    /*
+    /**
      * Returns an object of evaluation ID's mapped to the evaluation properties.
+     *
+     * @method getEvaluations
+     * @return {Object}
      */
     getEvaluations: function() {
         return this._evaluations;
     },
 
-    /*
+    /**
      * Replaces the evaluation map with the map passed in the parameter, and updates
      * the graphical 'Legend' to represent the new information
      *
-     * @param map is an object formatted as follows { E1 : {conclusion: '+', result: '(36n/18n)', numAEvaluations: 4}}
+     * @method setEvaluations
+     * @param {Object} map Formatted as follows:
+     *
+     {
+        E1 : {conclusion: '+', result: '(36n/18n)', numAEvaluations: 4},
+        ...
+     }
      */
     setEvaluations: function(map) {
         this._evaluations = map;
         //TODO: this.buildLegend
     },
 
-    /*
+    /**
      * Returns the Disorder object with the given disorder ID.
      *
-     * @param disorderID the id number for the disorder, taken from the OMIM database
+     * @method getDisorder
+     * @param {String|Number} disorderID The id for the disorder, taken from the OMIM database
+     * @return {Disorder}
      */
     getDisorder: function(disorderID) {
         return this.getDisorders().get(disorderID);
     },
 
-    /*
+    /**
      * Replaces the Disorder object with the given disorder ID.
      *
-     * @param disorderID the id number for the disorder, taken from the OMIM database
-     * @param disorder a Disorder object
+     * @method setDisorder
+     * @param {String|Number} disorderID The id for the disorder, taken from the OMIM database
+     * @param {Disorder} disorder
      */
     setDisorder: function(disorderID, disorder) {
         this.getDisorders().set(disorderID, disorder);
     },
 
-    /*
+    /**
      * Registers an occurrence of a disorder. If disorder hasn't been documented yet,
      * designates a color for it.
      *
-     * @param disorder an object with fields 'id' and 'value', where id is the id number
+     * @method addCase
+     * @param {Object} disorder Has fields 'id' and 'value', where id is the id number
      * for the disorder, taken from the OMIM database, and 'value' is the name of the disorder.
      * eg. {id: 33244, value: 'Down Syndrome'}
+     * @param {AbstractNode} node
      */
     addCase: function(disorder, node) {
         if (!this.containsDisorder(disorder['id'])) {
@@ -104,13 +125,14 @@ var Legend = Class.create( {
         this._displayCasesForDisorder(disorder['id']);
     },
 
-    /*
+    /**
      * Removes an occurrence of a disorder if there are any. Removes the disorder
      * from the 'Legend' box if this disorder is not registered in any individual.
      *
-     * @param disorder an object with fields 'id' and 'value', where id is the id number
+     * @param {Object} disorder Has fields 'id' and 'value', where id is the id number
      * for the disorder, taken from the OMIM database, and 'value' is the name of the disorder.
      * eg. {id: 33244, value: 'Down Syndrome'}
+     * @param {AbstractNode} node
      */
     removeCase: function(disorder, node) {
         if (this.containsDisorder(disorder['id'])) {
@@ -132,8 +154,10 @@ var Legend = Class.create( {
     
     /**
      * Update the displayed number of affected cases for for a disorder in the legend UI.
-     * 
+     *
+     * @method __displayCasesForDisorder
      * @param disorderID the identifier of the disorder to update
+     * @private
      */
     _displayCasesForDisorder : function(disorderID) {
       var label = this._legendBox.down('li#disorder-' + disorderID + ' .disorder-cases');
@@ -143,19 +167,22 @@ var Legend = Class.create( {
       }
     },
 
-    /*
+    /**
      * Returns true if the disorder with the given ID is registered in the Legend
      *
-     * @param disorderID the id number for the disorder, taken from the OMIM database
+     * @method containsDisorder
+     * @param {String|Number} disorderID The id for the disorder, taken from the OMIM database
      */
     containsDisorder: function(disorderID) {
         return this.getDisorders().keys().indexOf(disorderID)  > -1;
     },
 
-    /*
+    /**
      * Adds color to the list of colors used in the Legend
      *
-     * @param color is a string representing a CSS color e.g. 'blue' or '#DADADA'
+     * @method addUsedColor
+     * @param {String|Number} disorderID The id for the disorder, taken from the OMIM database
+     * @param {String} color CSS color e.g. 'blue' or '#DADADA'
      */
     addUsedColor: function(disorderID, color) {
         this.getDisorderColors().set(disorderID, color);
@@ -165,24 +192,32 @@ var Legend = Class.create( {
     /**
      * Retrieve the color associated to a specific disorder
      *
-     * @param disorderID the identifier of the disorder
-     * @return the color for that disorder, as a string #rrggbb, or undefined if the disorder is not registered
+     * @method getDisorderColor
+     * @param {String|Number} disorderID The id for the disorder, taken from the OMIM database
+     * @return {String} CSS color value for that disorder
      */
     getDisorderColor: function(disorderID) {
         return this._disorderColors.get(disorderID);
     },
 
+    /**
+     * Returns a Hash with disorder IDs mapped to CSS colors
+     *
+     * @method getDisorderColors
+     * @return {Hash} PrototypeJS Hash
+     */
     getDisorderColors: function() {
         return this._disorderColors;
     },
 
     /**
      * Generate the element that will display information about the given disorder in the legend
-     * 
-     * @param id the (internal) identifier of the disorder
-     * @param name the human-readable disorder name
-     * @param color the color associated with the disorder, displayed on affected nodes in the pedigree
-     * @return a 'li' element to be insert in the legend's list
+     *
+     * @method generateDisorderElement
+     * @param {String|Number} id The id for the disorder, taken from the OMIM database
+     * @param {String} name the human-readable disorder name
+     * @param {String} color CSS color associated with the disorder, displayed on affected nodes in the pedigree
+     * @return {HTMLLIElement} List element to be insert in the legend
      */
     generateDisorderElement: function(id, name, color) {
         var item = new Element('li', {'class' : 'disorder', 'id' : 'disorder-' + id}).update(new Element('span', {'class' : 'disorder-name'}).update(name));
@@ -225,15 +260,24 @@ var Legend = Class.create( {
         return item;
     },
 
-    _onDropDisorder: function(disorder, target, event) {
+    /**
+     * Callback for dragging a disorder from the legend onto-nodes
+     *
+     * @method _onDropDisorder
+     * @param {HTMLElement} [disorderName]
+     * @param {HTMLElement} [target]
+     * @param {Event} [event]
+     * @private
+     */
+    _onDropDisorder: function(disorderName, target, event) {
         var divPos = editor.getWorkspace().viewportToDiv(event.pointerX(), event.pointerY());
         var pos = editor.getWorkspace().divToCanvas(divPos.x,divPos.y);
         var node = editor.getNodeIndex().getNodeNear(pos.x, pos.y);
         if (node && node.getType() == 'Person') {
             var disorderObj = {};
-            disorderObj.id = disorder.id.substring( disorder.id.indexOf('-') + 1);
-            disorderObj.value = disorder.down('.disorder-name').firstChild.nodeValue;
+            disorderObj.id = disorderName.id.substring( disorder.id.indexOf('-') + 1);
+            disorderObj.value = disorderName.down('.disorder-name').firstChild.nodeValue;
             node.addDisorderAction(disorderObj);
         }
-    },
+    }
 });
