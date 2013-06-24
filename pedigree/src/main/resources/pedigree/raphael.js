b3c38dbec6ae79bfdf965ac7c0e5d4cc3375faba
Sergiu Dumitriu <sergiu@xwiki.org>
2013-06-24 15:46:01 -0400

    Issue #492: The Pedigree editor fails to load since 1.0M6 Fixed

--- pedigree/src/main/resources/pedigree/raphael.js
+++ pedigree/src/main/resources/pedigree/raphael.js
@@ -220,6 +220,7 @@
         return "You are running Eve " + version;
     };
     (typeof module != "undefined" && module.exports) ? (module.exports = eve) : (typeof define != "undefined" ? (define("eve", [], function() { return eve; })) : (glob.eve = eve));
+    glob.eve = eve;
 })(this);
 
 
