bc7cc6e0285157c8403cb74c0c7f2d3a7c32f391
ravi <ravi@ravi-K53SV.(none)>
2013-07-28 15:59:31 +0530

    Correcting Ontology Mistakes

--- components/ontology-access/src/main/java/org/phenotips/ontology/internal/solr/HumanPhenotypeOntology.java
+++ components/ontology-access/src/main/java/org/phenotips/ontology/internal/solr/HumanPhenotypeOntology.java
@@ -1,22 +1,22 @@
 /*
-* See the NOTICE file distributed with this work for additional
-* information regarding copyright ownership.
-*
-* This is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version 2.1 of
-* the License, or (at your option) any later version.
-*
-* This software is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this software; if not, write to the Free
-* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-*/
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.phenotips.ontology.internal.solr;
 
 import org.phenotips.ontology.OntologyTerm;
@@ -32,19 +32,19 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 
 /**
-* Provides access to the Human Phenotype Ontology (HPO). The ontology prefix is {@code HP}.
-*
-* @version $Id$
-* @since 1.0M8
-*/
+ * Provides access to the Human Phenotype Ontology (HPO). The ontology prefix is {@code HP}.
+ *
+ * @version $Id$
+ * @since 1.0M8
+ */
 @Component
 @Named("hpo")
 @Singleton
 public class HumanPhenotypeOntology extends AbstractSolrOntologyService
 {
     /**
-* The name of the Alternative ID field, used for older aliases of updated HPO terms.
-*/
+     * The name of the Alternative ID field, used for older aliases of updated HPO terms.
+     */
     protected static final String ALTERNATIVE_ID_FIELD_NAME = "alt_id";
 
     @Override
