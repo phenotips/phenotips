d2ef61e3a550e6e0339a57ba4c1437fb6f3e244c
Andrew M <asm@asm.(none)>
2013-09-20 15:35:44 -0400

    implement autoLayout, clearGraph and setProperty event handlers; fix infinite loop during console.log of events in Chrome

--- pedigree/src/main/resources/pedigree/controller.js
+++ pedigree/src/main/resources/pedigree/controller.js
@@ -4,22 +4,73 @@
  * @class Controller
  * @constructor
  */
+
+// TODO: undo/redo in all handlers
+
 var Controller = Class.create({
     initialize: function() {     
+        document.observe("pedigree:autolayout",                this.handleAutoLayout);
+        document.observe("pedigree:graph:clear",               this.handleClearGraph);
+        //document.observe("pedigree:undo",                      this.handleUndo);
+        //document.observe("pedigree:redo",                      this.handleRedo);
+        document.observe("pedigree:node:remove",               this.handleRemove);
+        document.observe("pedigree:node:setproperty",          this.handleSetProperty);
         document.observe("pedigree:person:drag:newparent",     this.handlePersonDragToNewParent);
         document.observe("pedigree:person:newparent",          this.handlePersonNewParents);
         document.observe("pedigree:person:newpartnerandchild", this.handlePersonNewPartnerAndChild);        
         document.observe("pedigree:partnership:newchild",      this.handleRelationshipNewChild);
     },      
     
+    handleAutoLayout: function(event)
+    {
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));
+        var changeSet = editor.getGraph().redrawAll();
+        editor.getGraphicsSet().applyChanges(changeSet, true);
+    },
+    
+    handleClearGraph: function(event)
+    {
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));
+        var changeSet = editor.getGraph().clearAll();
+        editor.getGraphicsSet().applyChanges(changeSet, true);        
+    },
+    
+    handleRemove: function(event)
+    {
+        //TODO
+        //get the list of affected nodes
+        //editor.getGraph().getDisconnectedSetIfNodeRemoved(nodeID);
+        // show message box
+        // proceed if user agrees
+        // ...
+    },
+    
+    handleSetProperty: function(event)
+    {
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));
+        var nodeID     = event.memo.nodeID;
+        var properties = event.memo.properties;
+        
+        var node = editor.getGraphicsSet().getNode(nodeID);
+        
+        for (propertySetFunction in properties)
+            if (properties.hasOwnProperty(propertySetFunction)) {
+                var propValue = properties[propertySetFunction];                
+                node[propertySetFunction](propValue);
+            }
+        
+        var allProperties = node.getProperties();              
+        editor.getGraph().setProperties( nodeID, allProperties );
+    },
+    
     handlePersonDragToNewParent: function(event)
     {
-        console.log("event: " + stringifyObject(event));
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));
         
         var personID = event.memo.personID;
         var parentID = event.memo.parentID;
         if (!editor.getGraph().isPerson(personID) || !editor.getGraph().isValidID(parentID)) return;
-                
+                        
         var changeSet = editor.getGraph().assignParent(parentID, personID);
         editor.getGraphicsSet().applyChanges(changeSet, true);
         editor.getNode(nodeId).getGraphics().getHoverBox().hideParentHandle();
@@ -27,7 +78,7 @@ var Controller = Class.create({
     
     handlePersonNewParents: function(event)
     {   
-        console.log("event: " + stringifyObject(event));
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));        
         
         var personID = event.memo.personID;
         if (!editor.getGraph().isPerson(personID)) return;        
@@ -39,7 +90,7 @@ var Controller = Class.create({
     
     handlePersonNewPartnerAndChild: function(event)
     {
-        console.log("event: " + stringifyObject(event));
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));        
         
         var personID    = event.memo.personID;
         if (!editor.getGraph().isPerson(personID)) return;        
@@ -52,7 +103,7 @@ var Controller = Class.create({
         
     handleRelationshipNewChild: function(event)
     {
-        console.log("event: " + stringifyObject(event));
+        console.log("event: " + event.eventName + ", memo: " + stringifyObject(event.memo));        
         
         var partnershipID = event.memo.partnershipID;
         if (!editor.getGraph().isRelationship(partnershipID)) return;
