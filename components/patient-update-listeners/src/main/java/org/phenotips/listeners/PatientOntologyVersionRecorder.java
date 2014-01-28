579085cdf84b9008aa84771f4379b6a4e8ea01f8
Anton Kats <anton.kats@gmail.com>
2014-01-28 17:16:33 -0500

    Issue #691: Store the current version of the HPO ontology in each patient record. Created new event listener and a new XWiki class.

---
+++ components/patient-update-listeners/src/main/java/org/phenotips/listeners/PatientOntologyVersionRecorder.java
@@ -0,0 +1,134 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.phenotips.listeners;
+
+import org.phenotips.Constants;
+import org.phenotips.ontology.OntologyService;
+
+import org.xwiki.bridge.event.DocumentCreatingEvent;
+import org.xwiki.bridge.event.DocumentUpdatingEvent;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.observation.EventListener;
+import org.xwiki.observation.event.Event;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+/**
+ * Store versions (in the form name:String, verstion:String) in the patient record.
+ *
+ * FIXME - version.
+ *
+ * @version $Id$
+ */
+@Component
+@Named("patient-version-recorder")
+@Singleton
+public class PatientOntologyVersionRecorder implements EventListener
+{
+    /** The name of the class where version info (name, version) is stored. */
+    private static final EntityReference VERSION_RECORDER_REFERENCE = new EntityReference("PatientVersionRecorder",
+        EntityType.DOCUMENT, Constants.CODE_SPACE_REFERENCE);
+
+    private static final String NAME_FIELD = "name";
+
+    /** Access to services that are needed to get the ontology version. */
+    @Inject
+    @Named("hpo")
+    private OntologyService humanPhenotypeOntology;
+
+    @Override
+    public String getName()
+    {
+        return "patient-version-recorder";
+    }
+
+    @Override
+    public List<Event> getEvents()
+    {
+        // The list of events this listener listens to
+        return Arrays.<Event>asList(new DocumentCreatingEvent(), new DocumentUpdatingEvent());
+    }
+
+    @Override
+    public void onEvent(Event event, Object source, Object data)
+    {
+        XWikiContext context = (XWikiContext) data;
+        XWikiDocument doc = (XWikiDocument) source;
+
+        BaseObject patientRecordObj =
+            doc.getXObject(new DocumentReference(doc.getDocumentReference().getWikiReference().getName(),
+                Constants.CODE_SPACE, "PatientClass"));
+        if (patientRecordObj == null) {
+            return;
+        }
+        //BaseClass patientRecordClass = patientRecordObj.getXClass(context);
+        List<BaseObject> existingVersionObjects = doc.getXObjects(VERSION_RECORDER_REFERENCE);
+        try {
+            for (Map.Entry<String, String> versionType : this.versionTypes().entrySet()) {
+                boolean skip = false;
+                if (existingVersionObjects != null) {
+                    for (BaseObject existingVersionObject : existingVersionObjects) {
+                        String name = existingVersionObject.getField(NAME_FIELD).toFormString();
+                        if (name.equalsIgnoreCase(versionType.getKey())) {
+                            skip = true;
+                            break;
+                        }
+                    }
+                    if (skip) {
+                        continue;
+                    }
+                }
+                BaseObject versionObject = doc.newXObject(VERSION_RECORDER_REFERENCE, context);
+                versionObject.set(NAME_FIELD, versionType.getKey(), context);
+                versionObject.set("version", versionType.getValue(), context);
+            }
+        } catch (XWikiException ex) {
+            //Storage Error. Shouldn't happen.
+        }
+    }
+
+    /**
+     * Modify this function to add more version types.
+     *
+     * @return Map of all the version types. Each entry becomes an object in the patient record.
+     */
+    public Map<String, String> versionTypes()
+    {
+        Map<String, String> result = new HashMap();
+
+        result.put("hpo", humanPhenotypeOntology.getVersion());
+        return result;
+    }
+}
