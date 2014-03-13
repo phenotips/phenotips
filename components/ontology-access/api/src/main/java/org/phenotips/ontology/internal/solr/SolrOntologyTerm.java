phenotips/phenotips

commit 8f4ade9f239444b2fd6b9d1c65e994a290e37153

Issue #679. Inconsistent implementation/name of OntologyTerm.getAncestors.

components/ontology-access/api/src/main/java/org/phenotips/ontology/internal/solr/SolrOntologyTerm.java
@@ -22,9 +22,11 @@ package org.phenotips.ontology.internal.solr;
 import org.phenotips.ontology.OntologyService;
 import org.phenotips.ontology.OntologyTerm;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -39,6 +41,8 @@ import org.apache.solr.common.SolrDocument;
  */
 public class SolrOntologyTerm implements OntologyTerm
 {
+    private static final String TERM_CATEGORY = "term_category";
+
     /** The Solr document representing this term. */
     private SolrDocument doc;
 
@@ -74,9 +78,25 @@ public class SolrOntologyTerm implements OntologyTerm
         this.doc = doc;
         this.ontology = ontology;
         if (doc != null) {
+            this.removeSelfDuplicate();
             this.parents = new LazySolrTermSet(doc.getFieldValues("is_a"), ontology);
-            this.ancestors = new LazySolrTermSet(doc.getFieldValues("term_category"), ontology);
+            this.ancestors = new LazySolrTermSet(doc.getFieldValues(TERM_CATEGORY), ontology);
         }
+    }
+
+    /**
+     * The field "term_category" in {@code this.doc} can contain the term itself. It appears that this only happens with
+     * HPO. To avoid this problem, and to avoid writing a separate implementation for HPO specifically, this method
+     * checks for existence of the term in the term_category and takes it out.
+     */
+    private void removeSelfDuplicate()
+    {
+        Object value = this.doc.getFieldValue(TERM_CATEGORY);
+        if (!(value instanceof List)) {
+            return;
+        }
+        List listValue = (List) value;
+        listValue.remove(this.getId());
     }
 
     @Override
@@ -107,6 +127,15 @@ public class SolrOntologyTerm implements OntologyTerm
     public Set<OntologyTerm> getAncestors()
     {
         return this.ancestors != null ? this.ancestors : Collections.<OntologyTerm> emptySet();
+    }
+
+    @Override
+    public Set<OntologyTerm> getAncestorsAndSelf()
+    {
+        Collection<Object> termSet = new HashSet<Object>();
+        termSet.add(this.getId());
+        termSet.addAll(doc.getFieldValues(TERM_CATEGORY));
+        return new LazySolrTermSet(termSet, this.ontology);
     }
 
     @Override

components/ontology-access/api/src/main/java/org/phenotips/ontology/OntologyTerm.java
@@ -72,6 +72,13 @@ public interface OntologyTerm
     Set<OntologyTerm> getAncestors();
 
     /**
+     * Returns the ancestors (both direct and indirect ancestors) of this term <em>and</em> the term itself.
+     *
+     * @return a set of ontology terms, or a set with one term if the term doesn't have any ancestors in the ontology
+     */
+    Set<OntologyTerm> getAncestorsAndSelf();
+
+    /**
      * Find the distance to another term in the ontology structure.
      * 
      * @param other the term to which the distance should be computed

commit 3379a4b5d1a13ad4e2de746636d5fe9b082e084d

[misc] Fixed performance issue caused by 8f4ade9f239444b2fd6b9d1c65e994a290e37153

components/ontology-access/api/src/main/java/org/phenotips/ontology/internal/solr/SolrOntologyTerm.java
@@ -68,6 +68,13 @@ public class SolrOntologyTerm implements OntologyTerm
     private Set<OntologyTerm> ancestors;
 
     /**
+     * A set containing the term itself and its ancestors, transformed from a set of IDs into a real set of terms.
+     * 
+     * @see #getAncestorsAndSelf()
+     */
+    private Set<OntologyTerm> ancestorsAndSelf;
+
+    /**
      * Constructor that provides the backing {@link #doc Solr document} and the {@link #ontology owner ontology}.
      * 
      * @param doc the {@link #doc Solr document} representing this term
@@ -81,6 +88,12 @@ public class SolrOntologyTerm implements OntologyTerm
             this.removeSelfDuplicate();
             this.parents = new LazySolrTermSet(doc.getFieldValues("is_a"), ontology);
             this.ancestors = new LazySolrTermSet(doc.getFieldValues(TERM_CATEGORY), ontology);
+            Collection<Object> termSet = new HashSet<Object>();
+            termSet.add(this.getId());
+            if (doc.getFieldValues(TERM_CATEGORY) != null) {
+                termSet.addAll(doc.getFieldValues(TERM_CATEGORY));
+            }
+            this.ancestorsAndSelf = new LazySolrTermSet(termSet, this.ontology);
         }
     }
 
@@ -132,10 +145,7 @@ public class SolrOntologyTerm implements OntologyTerm
     @Override
     public Set<OntologyTerm> getAncestorsAndSelf()
     {
-        Collection<Object> termSet = new HashSet<Object>();
-        termSet.add(this.getId());
-        termSet.addAll(doc.getFieldValues(TERM_CATEGORY));
-        return new LazySolrTermSet(termSet, this.ontology);
+        return this.ancestorsAndSelf != null ? this.ancestorsAndSelf : Collections.<OntologyTerm>emptySet();
     }
 
     @Override