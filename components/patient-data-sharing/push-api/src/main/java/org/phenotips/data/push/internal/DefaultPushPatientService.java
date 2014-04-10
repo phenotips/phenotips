5dad6f18d911c47af30a2a1a1f7a24b5ae572b35
Sergiu Dumitriu <sergiu@phenotips.org>
2014-04-10 18:13:50 -0400

    [cleanup] Formatted code

--- components/patient-data-sharing/push-api/src/main/java/org/phenotips/data/push/internal/DefaultPushPatientService.java
+++ components/patient-data-sharing/push-api/src/main/java/org/phenotips/data/push/internal/DefaultPushPatientService.java
@@ -19,49 +19,46 @@
  */
 package org.phenotips.data.push.internal;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.TreeMap;
-
-import javax.inject.Inject;
-
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import org.phenotips.Constants;
 import org.phenotips.data.Patient;
 import org.phenotips.data.PatientRepository;
 import org.phenotips.data.permissions.AccessLevel;
 import org.phenotips.data.permissions.PatientAccess;
 import org.phenotips.data.permissions.PermissionsManager;
+import org.phenotips.data.push.PatientPushHistory;
 import org.phenotips.data.push.PushPatientData;
 import org.phenotips.data.push.PushPatientService;
 import org.phenotips.data.push.PushServerConfigurationResponse;
 import org.phenotips.data.push.PushServerGetPatientIDResponse;
 import org.phenotips.data.push.PushServerInfo;
-import org.phenotips.data.push.PatientPushHistory;
 import org.phenotips.data.push.PushServerSendPatientResponse;
-import org.phenotips.data.push.internal.DefaultPushServerConfigurationResponse;
-import org.phenotips.data.push.internal.DefaultPushServerGetPatientIDResponse;
-import org.phenotips.data.push.internal.DefaultPushServerSendPatientResponse;
-import org.phenotips.data.securestorage.SecureStorageManager;
-import org.phenotips.data.securestorage.RemoteLoginData;
 import org.phenotips.data.securestorage.PatientPushedToInfo;
-import org.slf4j.Logger;
+import org.phenotips.data.securestorage.RemoteLoginData;
+import org.phenotips.data.securestorage.SecureStorageManager;
+
 import org.xwiki.component.annotation.Component;
 import org.xwiki.context.Execution;
-
-import groovy.lang.Singleton;
-
 import org.xwiki.model.reference.DocumentReference;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
 
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.BaseObject;
+
+import groovy.lang.Singleton;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  * Default implementation for the {@link PushPatientData} component.
@@ -92,10 +89,9 @@ public class DefaultPushPatientService implements PushPatientService
     @Inject
     private PatientRepository patientRepository;
 
-    /** Used to check user right to push a patient when pushig using a string */
+    /** Used to check user right to push a patient when pushing using a string */
     @Inject
     private PermissionsManager permisionManager;
-
 
     protected RemoteLoginData getStoredData(String remoteServerIdentifier)
     {
@@ -119,8 +115,9 @@ public class DefaultPushPatientService implements PushPatientService
     {
         XWikiContext context = getXContext();
 
-        if (context.getUserReference() == null)
+        if (context.getUserReference() == null) {
             return null;
+        }
 
         return context.getUserReference().getName();
     }
@@ -144,15 +141,22 @@ public class DefaultPushPatientService implements PushPatientService
             XWiki xwiki = context.getWiki();
             XWikiDocument prefsDoc =
                 xwiki.getDocument(new DocumentReference(xwiki.getDatabase(), "XWiki", "XWikiPreferences"), context);
-            List<BaseObject> servers = prefsDoc.getXObjects(new DocumentReference(xwiki.getDatabase(), Constants.CODE_SPACE, "PushPatientServer"));
+            List<BaseObject> servers = prefsDoc.getXObjects(new DocumentReference(xwiki.getDatabase(),
+                Constants.CODE_SPACE, "PushPatientServer"));
 
             Set<PushServerInfo> response = new TreeSet<PushServerInfo>();
             for (BaseObject serverConfiguration : servers) {
-                if (!serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_TOKEN_PROPERTY_NAME).equals("")) {
-                    this.logger.warn("   ...available: [{}]", serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_ID_PROPERTY_NAME));
-                    PushServerInfo info = new DefaultPushServerInfo(serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_ID_PROPERTY_NAME),
-                                                                    serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_URL_PROPERTY_NAME),
-                                                                    serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_DESC_PROPERTY_NAME));
+                if (!serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_TOKEN_PROPERTY_NAME)
+                    .equals("")) {
+                    this.logger.warn("   ...available: [{}]",
+                        serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_ID_PROPERTY_NAME));
+                    PushServerInfo info =
+                        new DefaultPushServerInfo(
+                            serverConfiguration.getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_ID_PROPERTY_NAME),
+                            serverConfiguration
+                                .getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_URL_PROPERTY_NAME),
+                            serverConfiguration
+                                .getStringValue(DefaultPushPatientData.PUSH_SERVER_CONFIG_DESC_PROPERTY_NAME));
                     response.add(info);
                 }
             }
@@ -170,10 +174,9 @@ public class DefaultPushPatientService implements PushPatientService
 
         Map<PushServerInfo, PatientPushHistory> response = new TreeMap<PushServerInfo, PatientPushHistory>();
 
-        for (PushServerInfo server: servers) {
+        for (PushServerInfo server : servers) {
             PatientPushedToInfo pushInfo = this.storageManager.getPatientPushInfo(localPatientID, server.getServerID());
-            PatientPushHistory history = (pushInfo == null) ? null :
-                                         new DefaultPatientPushHistory(pushInfo);
+            PatientPushHistory history = (pushInfo == null) ? null : new DefaultPatientPushHistory(pushInfo);
             response.put(server, history);
         }
         return response;
@@ -182,15 +185,19 @@ public class DefaultPushPatientService implements PushPatientService
     private Patient getPatientByID(String patientID, String accessLevelName)
     {
         Patient patient = this.patientRepository.getPatientById(patientID);
-        if (patient == null) return null;
+        if (patient == null) {
+            return null;
+        }
 
-        if (accessLevelName.equals("push"))
+        if (accessLevelName.equals("push")) {
             accessLevelName = "view";
+        }
 
         PatientAccess access = this.permisionManager.getPatientAccess(patient);
         AccessLevel requiredAccess = this.permisionManager.resolveAccessLevel(accessLevelName);
         if (!access.hasAccessLevel(requiredAccess)) {
-            this.logger.warn("Can't access patient [{}] at level [{}]: access level violation", patientID, accessLevelName);
+            this.logger.warn("Can't access patient [{}] at level [{}]: access level violation", patientID,
+                accessLevelName);
             return null;
         }
 
@@ -205,7 +212,7 @@ public class DefaultPushPatientService implements PushPatientService
                 JSONArray fields = JSONArray.fromObject(listOfStrings);
                 if (fields != null) {
                     fieldSet = new TreeSet<String>();
-                    for (Object field: fields) {
+                    for (Object field : fields) {
                         fieldSet.add(field.toString());
                     }
                 }
@@ -228,15 +235,17 @@ public class DefaultPushPatientService implements PushPatientService
     }
 
     @Override
-    public PushServerConfigurationResponse getRemoteConfiguration(String remoteServerIdentifier, String remoteUserName, String password, boolean saveUserToken)
+    public PushServerConfigurationResponse getRemoteConfiguration(String remoteServerIdentifier, String remoteUserName,
+        String password, boolean saveUserToken)
     {
-        PushServerConfigurationResponse response = this.internalService.getRemoteConfiguration(remoteServerIdentifier, remoteUserName, password, null);
+        PushServerConfigurationResponse response =
+            this.internalService.getRemoteConfiguration(remoteServerIdentifier, remoteUserName, password, null);
 
         if (response != null && response.isSuccessful()) {
             if (saveUserToken) {
-                storeUserData(remoteServerIdentifier, remoteUserName, response.getRemoteUserToken());   // note: even if newly received token is null
-            }
-            else {
+                // note: even if newly received token is null
+                storeUserData(remoteServerIdentifier, remoteUserName, response.getRemoteUserToken());
+            } else {
                 removeUserData(remoteServerIdentifier);
             }
 
@@ -255,14 +264,18 @@ public class DefaultPushPatientService implements PushPatientService
     public PushServerConfigurationResponse getRemoteConfiguration(String remoteServerIdentifier)
     {
         RemoteLoginData storedData = getStoredData(remoteServerIdentifier);
-        if (storedData == null || storedData.getRemoteUserName() == null || storedData.getLoginToken() == null)
-            return new DefaultPushServerConfigurationResponse(DefaultPushServerResponse.generateIncorrectCredentialsJSON());
+        if (storedData == null || storedData.getRemoteUserName() == null || storedData.getLoginToken() == null) {
+            return new DefaultPushServerConfigurationResponse(
+                DefaultPushServerResponse.generateIncorrectCredentialsJSON());
+        }
 
         PushServerConfigurationResponse response = this.internalService.getRemoteConfiguration(remoteServerIdentifier,
-                                                      storedData.getRemoteUserName(), null, storedData.getLoginToken());
+            storedData.getRemoteUserName(), null, storedData.getLoginToken());
 
-        if (response != null && response.getRemoteUserToken() != null && response.getRemoteUserToken() != storedData.getLoginToken()) {
-            storeUserData(remoteServerIdentifier, storedData.getRemoteUserName(), response.getRemoteUserToken());  // update token to newly received one
+        if (response != null && response.getRemoteUserToken() != null
+            && response.getRemoteUserToken() != storedData.getLoginToken()) {
+            // update token to newly received one
+            storeUserData(remoteServerIdentifier, storedData.getRemoteUserName(), response.getRemoteUserToken());
         }
 
         return response;
@@ -270,32 +283,33 @@ public class DefaultPushPatientService implements PushPatientService
 
     @Override
     public PushServerSendPatientResponse sendPatient(String patientID, String exportFieldListJSON, String groupName,
-                                                     String remoteGUID, String remoteServerIdentifier)
+        String remoteGUID, String remoteServerIdentifier)
     {
         Patient patient = getPatientByID(patientID, "push");
         if (patient == null) {
             return new DefaultPushServerSendPatientResponse(DefaultPushServerResponse.generateActionFailedJSON());
         }
         RemoteLoginData storedData = getStoredData(remoteServerIdentifier);
-        if (storedData == null || storedData.getRemoteUserName() == null || storedData.getLoginToken() == null)
-            return new DefaultPushServerSendPatientResponse(DefaultPushServerResponse.generateIncorrectCredentialsJSON());
+        if (storedData == null || storedData.getRemoteUserName() == null || storedData.getLoginToken() == null) {
+            return new DefaultPushServerSendPatientResponse(
+                DefaultPushServerResponse.generateIncorrectCredentialsJSON());
+        }
 
         Set<String> exportFields = parseJSONArrayIntoSet(exportFieldListJSON);
 
-        PushServerSendPatientResponse response =  this.internalService.sendPatient(patient, exportFields, groupName, remoteGUID, remoteServerIdentifier,
-                                                                                   storedData.getRemoteUserName(), null, storedData.getLoginToken());
+        PushServerSendPatientResponse response = this.internalService.sendPatient(patient, exportFields, groupName,
+            remoteGUID, remoteServerIdentifier, storedData.getRemoteUserName(), null, storedData.getLoginToken());
 
         if (response != null && response.isSuccessful()) {
             this.storageManager.storePatientPushInfo(patient.getDocument().getName(), remoteServerIdentifier,
-                                                     response.getRemotePatientGUID(), response.getRemotePatientID(), response.getRemotePatientURL());
+                response.getRemotePatientGUID(), response.getRemotePatientID(), response.getRemotePatientURL());
         }
         return response;
     }
 
     @Override
     public PushServerSendPatientResponse sendPatient(String patientID, String exportFieldListJSON, String groupName,
-                                                     String remoteGUID, String remoteServerIdentifier,
-                                                     String remoteUserName, String password)
+        String remoteGUID, String remoteServerIdentifier, String remoteUserName, String password)
     {
         Patient patient = getPatientByID(patientID, "push");
         if (patient == null) {
@@ -304,13 +318,14 @@ public class DefaultPushPatientService implements PushPatientService
 
         Set<String> exportFields = parseJSONArrayIntoSet(exportFieldListJSON);
 
-        PushServerSendPatientResponse response = this.internalService.sendPatient(patient, exportFields, groupName, remoteGUID, remoteServerIdentifier,
-                                                                                  remoteUserName, password, null);
+        PushServerSendPatientResponse response = this.internalService.sendPatient(patient, exportFields, groupName,
+            remoteGUID, remoteServerIdentifier, remoteUserName, password, null);
 
         if (response != null && response.isSuccessful()) {
             this.storageManager.storePatientPushInfo(patient.getDocument().getName(), remoteServerIdentifier,
-                                                     response.getRemotePatientGUID(), response.getRemotePatientID(), response.getRemotePatientURL());
+                response.getRemotePatientGUID(), response.getRemotePatientID(), response.getRemotePatientURL());
         }
+
         return response;
     }
 
@@ -318,7 +333,9 @@ public class DefaultPushPatientService implements PushPatientService
     public String getRemoteUsername(String remoteServerIdentifier)
     {
         RemoteLoginData storedData = getStoredData(remoteServerIdentifier);
-        if (storedData == null) return null;
+        if (storedData == null) {
+            return null;
+        }
         return storedData.getRemoteUserName();
     }
 
@@ -326,17 +343,20 @@ public class DefaultPushPatientService implements PushPatientService
     public PushServerGetPatientIDResponse getPatientURL(String remoteServerIdentifier, String remotePatientGUID)
     {
         RemoteLoginData storedData = getStoredData(remoteServerIdentifier);
-        if (storedData == null || storedData.getRemoteUserName() == null || storedData.getLoginToken() == null)
-            return new DefaultPushServerGetPatientIDResponse(DefaultPushServerResponse.generateIncorrectCredentialsJSON());
+        if (storedData == null || storedData.getRemoteUserName() == null || storedData.getLoginToken() == null) {
+            return new DefaultPushServerGetPatientIDResponse(
+                DefaultPushServerResponse.generateIncorrectCredentialsJSON());
+        }
 
         return this.internalService.getPatientURL(remoteServerIdentifier, remotePatientGUID,
-                                                  storedData.getRemoteUserName(), null, storedData.getLoginToken());
+            storedData.getRemoteUserName(), null, storedData.getLoginToken());
     }
 
     @Override
-    public PushServerGetPatientIDResponse getPatientURL(String remoteServerIdentifier, String remotePatientGUID, String remoteUserName, String password)
+    public PushServerGetPatientIDResponse getPatientURL(String remoteServerIdentifier, String remotePatientGUID,
+        String remoteUserName, String password)
     {
-        return this.internalService.getPatientURL(remoteServerIdentifier, remotePatientGUID, remoteUserName, password, null);
-
+        return this.internalService.getPatientURL(remoteServerIdentifier, remotePatientGUID, remoteUserName, password,
+            null);
     }
 }
