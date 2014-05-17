f60d22972ef422532f27c0fa892bd24f8313ddf8
Anton Kats <antonkats@gmail.com>
2014-05-17 14:40:07 -0400

    Issue #580: The patient phenotypes should always be displayed in a generic category even if the active mapping doesn't contain dedicated categories.

--- components/patient-tools/src/main/java/org/phenotips/tools/PropertyDisplayer.java
+++ components/patient-tools/src/main/java/org/phenotips/tools/PropertyDisplayer.java
@@ -33,7 +33,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.WordUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.xpn.xwiki.api.Property;
