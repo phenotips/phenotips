3a4333870a3baa968849c035eedd7901fabc0d83
Andrew Misyura <asm@phenotips.org>
2014-06-16 12:41:17 -0400

    Issue #926: fix pedigree import dialog ungraceful resizing; hide suggestions when node menu is closed

--- pedigree/src/main/resources/pedigree/view/nodeMenu.js
+++ pedigree/src/main/resources/pedigree/view/nodeMenu.js
@@ -262,7 +262,7 @@ NodeMenu = Class.create({
         'date-picker' : function (data) {
             var result = this._generateEmptyField(data);
             var datePicker = new Element('input', {type: 'text', 'class': 'xwiki-date', name: data.name, 'title': data.format, alt : '' });
-            result.insert(datePicker);
+            result.insert(datePicker);            
             datePicker._getValue = function() { return [this.alt && Date.parseISO_8601(this.alt)]; }.bind(datePicker);
             this._attachFieldEventListeners(datePicker, ['xwiki:date:changed']);
             return result;
@@ -333,6 +333,7 @@ NodeMenu = Class.create({
     },
         
     hide : function() {
+        this.hideSuggestPicker();
         this._onscreen = false;
         //console.log("nodeMenu hide");
         document.stopObserving('mousedown', this._onClickOutside);
@@ -343,7 +344,15 @@ NodeMenu = Class.create({
         this.menuBox.hide();
         this._clearCrtData();
     },
-
+    
+    hideSuggestPicker: function() {
+        this.form.select('input.suggest-omim').each(function(item) {
+            if (item._suggest) {
+                item._suggest.clearSuggestions();
+            }
+        });
+    },
+    
     isVisible: function() {
         return this._onscreen;
     },
@@ -369,7 +378,7 @@ NodeMenu = Class.create({
           this._height   = this.menuBox.getHeight();
           this.menuBox.style.height   = '';
           this.menuBox.style.overflow = '';
-          this.menuBox.style.left = x + 'px';
+          this.menuBox.style.left = x + 'px';          
       } else {
           if (this.menuBox.getHeight() <= this._height) return;
           this._height = this.menuBox.getHeight();
@@ -396,11 +405,12 @@ NodeMenu = Class.create({
             _this._setFieldValue[_this.fieldMap[name].type].call(_this, _this.fieldMap[name].element, _this.fieldMap[name].crtValue);
             _this.fieldMap[name].inactive = false;
         });
-    },
-
+    },    
+    
     _setCrtData : function (data) {
         var _this = this;
-        Object.keys(this.fieldMap).each(function (name) {
+        Object.keys(this.fieldMap).each(function (name) {            
+            
             _this.fieldMap[name].crtValue = data && data[name] && typeof(data[name].value) != "undefined" ? data[name].value : _this.fieldMap[name].crtValue || _this.fieldMap[name]["default"];
             _this.fieldMap[name].inactive = (data && data[name] && (typeof(data[name].inactive) == 'boolean' || typeof(data[name].inactive) == 'object')) ? data[name].inactive : _this.fieldMap[name].inactive;
             _this.fieldMap[name].disabled = (data && data[name] && (typeof(data[name].disabled) == 'boolean' || typeof(data[name].disabled) == 'object')) ? data[name].disabled : _this.fieldMap[name].disabled;
@@ -408,10 +418,11 @@ NodeMenu = Class.create({
             _this._setFieldInactive[_this.fieldMap[name].type].call(_this, _this.fieldMap[name].element, _this.fieldMap[name].inactive);
             _this._setFieldDisabled[_this.fieldMap[name].type].call(_this, _this.fieldMap[name].element, _this.fieldMap[name].disabled);
             //_this._updatedDependency(_this.fieldMap[name].element, _this.fieldMap[name].element);
+            
             //console.log("name = " + name + ", data = " + stringifyObject(data[name]) + ", inactive: " + stringifyObject(_this.fieldMap[name].inactive));            
         });
     },
-
+    
     _setFieldValue : {
         'radio' : function (container, value) {
             var target = container.down('input[type=radio][value=' + value + ']');
@@ -436,7 +447,7 @@ NodeMenu = Class.create({
             if (target) {
                 target.value = value;
             }
-        },
+        },        
         'date-picker' : function (container, value) {
             var target = container.down('input[type=text].xwiki-date');
             if (target) {
@@ -473,7 +484,7 @@ NodeMenu = Class.create({
             }
         }
     },
-
+    
     _toggleFieldVisibility : function(container, doHide) {
         if (doHide) {
           container.addClassName('hidden');
@@ -481,7 +492,7 @@ NodeMenu = Class.create({
           container.removeClassName('hidden');
         }
     },
-
+    
     _setFieldInactive : {
         'radio' : function (container, inactive) {
             if (inactive === true) {
@@ -531,8 +542,8 @@ NodeMenu = Class.create({
                 container.addClassName('hidden');
             } else {
                 container.removeClassName('hidden');
-                container.select('input[type=radio]').each(function(item) {
-                    if (disabled && Object.prototype.toString.call(disabled) === '[object Array]')
+                container.select('input[type=radio]').each(function(item) {                    
+                    if (disabled && Object.prototype.toString.call(disabled) === '[object Array]')                        
                         item.disabled = (disabled.indexOf(item.value) >= 0);
                     if (!disabled)
                         item.disabled = false;
