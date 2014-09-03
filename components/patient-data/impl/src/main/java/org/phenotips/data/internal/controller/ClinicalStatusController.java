61b9c338cb3ee3b2bb064d5b68f9b1affa2946fc
Anton Kats <antonkats@gmail.com>
2014-09-03 16:58:36 -0400

    Issue #1086: Implemented the Clinical Status controller properly.

---
+++ components/patient-data/impl/src/main/java/org/phenotips/data/internal/controller/ClinicalStatusController.java
@@ -0,0 +1,130 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.phenotips.data.internal.controller;
+
+import org.phenotips.data.Patient;
+import org.phenotips.data.PatientData;
+import org.phenotips.data.PatientDataController;
+import org.phenotips.data.SimpleValuePatientData;
+
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.annotation.Component;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+import com.xpn.xwiki.objects.BaseProperty;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Has only one field corresponding to whether the patient is affected, normal, or pre-symptomatic.
+ *
+ * @version $Id$
+ * @since 1.0RC2
+ */
+@Component(roles = { PatientDataController.class })
+@Named("clinicalStatus")
+@Singleton
+public class ClinicalStatusController implements PatientDataController<String>
+{
+    /** Provides access to the underlying data storage. */
+    @Inject
+    protected DocumentAccessBridge documentAccessBridge;
+
+    /** Logging helper object. */
+    @Inject
+    private Logger logger;
+
+    @Override
+    public String getName()
+    {
+        return "clinicalStatus";
+    }
+
+    @Override
+    public PatientData<String> load(Patient patient)
+    {
+        String unaffected = "unaffected";
+        try {
+            XWikiDocument doc = (XWikiDocument) this.documentAccessBridge.getDocument(patient.getDocument());
+            BaseObject data = doc.getXObject(Patient.CLASS_REFERENCE);
+            if (data == null) {
+                throw new NullPointerException("The patient does not have a PatientClass");
+            }
+            BaseProperty field = (BaseProperty) data.getField(unaffected);
+            if (field != null) {
+                Object propertyValue = field.getValue();
+                /* If the controller only works with codes, store the Ontology Instances rather than Strings */
+                if (propertyValue == 0) {
+                    return new SimpleValuePatientData<String>(getName(), unaffected);
+                } else if (propertyValue == 1) {
+                    return new SimpleValuePatientData<String>(getName(), "affected");
+                }
+            }
+        } catch (Exception e) {
+            this.logger.error(
+                "Could not find requested document or some unforeseen error has occurred during controller loading");
+        }
+        return null;
+    }
+
+    @Override
+    public void writeJSON(Patient patient, JSONObject json, Collection<String> selectedFieldNames)
+    {
+        PatientData<String> data = patient.getData(getName());
+        if (data == null) {
+            return;
+        }
+        JSONObject container = json.getJSONObject(getName());
+
+        if (container == null || container.isNullObject()) {
+            // put() is placed here because we want to create the property iff at least one field is set/enabled
+            json.put(getName(), new JSONObject());
+            container = json.getJSONObject(getName());
+        }
+        container.put(data.getName(), data.getValue());
+    }
+
+    @Override
+    public void writeJSON(Patient patient, JSONObject json)
+    {
+        writeJSON(patient, json, null);
+    }
+
+    @Override
+    public void save(Patient patient)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public PatientData<String> readJSON(JSONObject json)
+    {
+        throw new UnsupportedOperationException();
+    }
+}
