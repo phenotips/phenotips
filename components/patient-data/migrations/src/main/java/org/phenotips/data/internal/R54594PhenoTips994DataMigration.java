ae1cf75b1992b92589e90fb2e06e9da654810e0b
Sergiu Dumitriu <sergiu@phenotips.org>
2014-10-01 17:08:46 -0400

    Issue #994: Update the "Generic phenotype configuration" JSON to use prenatal HPO terms for the prenatal measurements Added migrator

---
+++ components/patient-data/migrations/src/main/java/org/phenotips/data/internal/R54594PhenoTips994DataMigration.java
@@ -0,0 +1,188 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.phenotips.data.internal;
+
+import org.phenotips.data.Patient;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.model.reference.EntityReferenceSerializer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+import com.xpn.xwiki.objects.DBStringListProperty;
+import com.xpn.xwiki.store.XWikiHibernateBaseStore;
+import com.xpn.xwiki.store.XWikiHibernateStore;
+import com.xpn.xwiki.store.migration.DataMigrationException;
+import com.xpn.xwiki.store.migration.XWikiDBVersion;
+import com.xpn.xwiki.store.migration.hibernate.AbstractHibernateDataMigration;
+
+/**
+ * Migration for PhenoTips issue #994: Update prenatal phenotypes to use the specific congenital terms instead of the
+ * generic adult ones.
+ *
+ * @version $Id$
+ * @since 1.0M13
+ */
+@Component
+@Named("R54594PhenoTips#994")
+@Singleton
+public class R54594PhenoTips994DataMigration extends AbstractHibernateDataMigration
+{
+    /** Resolves unprefixed document names to the current wiki. */
+    @Inject
+    @Named("current")
+    private DocumentReferenceResolver<String> resolver;
+
+    /** Serializes the class name without the wiki prefix, to be used in the database query. */
+    @Inject
+    @Named("compactwiki")
+    private EntityReferenceSerializer<String> serializer;
+
+    @Override
+    public String getDescription()
+    {
+        return "Update prenatal phenotypes to use the specific congenital terms instead of the generic adult ones";
+    }
+
+    @Override
+    public XWikiDBVersion getVersion()
+    {
+        return new XWikiDBVersion(54594);
+    }
+
+    @Override
+    public void hibernateMigrate() throws DataMigrationException, XWikiException
+    {
+        getStore().executeWrite(getXWikiContext(), new UpdatePrenatalPhenotypesCallback());
+    }
+
+    /**
+     * Searches for all documents containing pedigree images containing a wrongly placed quote and fixes them.
+     */
+    private class UpdatePrenatalPhenotypesCallback implements XWikiHibernateBaseStore.HibernateCallback<Object>
+    {
+        /** The name of the property to fix. */
+        private final String[] propertyNames = { "prenatal_phenotype", "negative_prenatal_phenotype" };
+
+        private final Map<String, String> translations = new HashMap<>();
+
+        UpdatePrenatalPhenotypesCallback()
+        {
+            this.translations.put("HP:0004325", "HP:0001518");
+            this.translations.put("HP:0004324", "HP:0001520");
+            this.translations.put("HP:0004322", "HP:0003561");
+            this.translations.put("HP:0000098", "HP:0003517");
+            this.translations.put("HP:0000252", "HP:0011451");
+            this.translations.put("HP:0000256", "HP:0004488");
+        }
+
+        @Override
+        public Object doInHibernate(Session session) throws HibernateException, XWikiException
+        {
+            XWikiContext context = getXWikiContext();
+            XWiki xwiki = context.getWiki();
+            Query q =
+                session.createQuery("select distinct o.name from BaseObject o, DBStringListProperty p join p.list as i"
+                    + " where p.id.id = o.id and o.className = ? and p.id.name in (?, ?)");
+            q.setParameter(0, R54594PhenoTips994DataMigration.this.serializer.serialize(Patient.CLASS_REFERENCE));
+            q.setParameter(1, this.propertyNames[0]);
+            q.setParameter(2, this.propertyNames[1]);
+            @SuppressWarnings("unchecked")
+            List<String> documents = q.list();
+            for (String docName : documents) {
+                XWikiDocument doc =
+                    xwiki.getDocument(R54594PhenoTips994DataMigration.this.resolver.resolve(docName), context);
+                boolean modified = false;
+                for (BaseObject object : doc.getXObjects(Patient.CLASS_REFERENCE)) {
+                    if (object == null) {
+                        continue;
+                    }
+                    for (String propertyName : this.propertyNames) {
+                        DBStringListProperty property = (DBStringListProperty) object.get(propertyName);
+                        DBStringListProperty extendedProperty =
+                            (DBStringListProperty) object.get("extended_" + propertyName);
+                        modified = modified || fixList(property, extendedProperty);
+                    }
+                }
+                if (!modified) {
+                    continue;
+                }
+                doc.setComment(R54594PhenoTips994DataMigration.this.getDescription());
+                doc.setMinorEdit(true);
+                try {
+                    // There's a bug in XWiki which prevents saving an object in the same session that it was loaded,
+                    // so we must clear the session cache first.
+                    session.clear();
+                    ((XWikiHibernateStore) getStore()).saveXWikiDoc(doc, context, false);
+                    session.flush();
+                } catch (DataMigrationException e) {
+                    // We're in the middle of a migration, we're not expecting another migration
+                }
+            }
+            return null;
+        }
+
+        private boolean fixList(DBStringListProperty property, DBStringListProperty extendedProperty)
+        {
+            if (property == null || property.getList().isEmpty()) {
+                return false;
+            }
+            boolean result = false;
+            List<String> values = property.getList();
+            List<String> extendedValues = null;
+            if (extendedProperty != null) {
+                extendedValues = extendedProperty.getList();
+            }
+            for (Map.Entry<String, String> translation : this.translations.entrySet()) {
+                if (values.contains(translation.getKey())) {
+                    values.remove(translation.getKey());
+                    values.add(translation.getValue());
+                    if (extendedValues != null) {
+                        extendedValues.add(translation.getValue());
+                    }
+                    result = true;
+                }
+            }
+            if (result) {
+                property.setList(values);
+                if (extendedValues != null) {
+                    extendedProperty.setList(extendedValues);
+                }
+            }
+            return result;
+        }
+    }
+}
