65b5b676e567a7e42ae43d0dfab3411173f399f0
Denys Pavlov <denys.pavlov@gmail.com>
2014-10-29 17:09:00 -0400

    fixed .poms, added depends on lang3

--- components/diagnosis-suggestion/api/src/main/java/org/phenotips/boqa/internal/DefaultDiagnosisService.java
+++ components/diagnosis-suggestion/api/src/main/java/org/phenotips/boqa/internal/DefaultDiagnosisService.java
@@ -46,7 +46,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
