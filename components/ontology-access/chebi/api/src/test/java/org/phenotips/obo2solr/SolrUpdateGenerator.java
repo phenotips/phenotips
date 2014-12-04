717c57f896dff3f856250147f74fb492ce21d406
Sergiu Dumitriu <sergiu@phenotips.org>
2014-12-03 19:14:04 -0500

    [cleanup] Deleted unused code, replaced proprietary exception with a standard java.lang one

--- components/ontology-access/chebi/api/src/test/java/org/phenotips/obo2solr/SolrUpdateGenerator.java
+++ components/ontology-access/chebi/api/src/test/java/org/phenotips/obo2solr/SolrUpdateGenerator.java
@@ -22,49 +22,13 @@ package org.phenotips.obo2solr;
 import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
-
-import org.xml.sax.ContentHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.AttributesImpl;
-
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class SolrUpdateGenerator
 {
-    private static final String TERM_MARKER = "[Term]";
-
-    /** Not all entities are terms prompted by the presence of a {@link #TERM_MARKER} */
-    private static final String ENTITY_SEPARATION_REGEX = "^\\[[a-zA-Z]+\\]$";
-
-    private static final String ROOT_ELEMENT_NAME = "add";
-
-    private static final String TERM_ELEMENT_NAME = "doc";
-
-    private static final String FIELD_ELEMENT_NAME = "field";
-
-    private static final String FIELD_ATTRIBUTE_NAME = "name";
-
-    private static final String FIELD_ATTRIBUTE_BOOST = "boost";
-
-    private static final String FIELD_NAME_VALUE_SEPARATOR = "\\s*:\\s+";
-
-    private AttributesImpl atts;
-
-    private ContentHandler hd;
-
-    private int counter;
-
-    private TermData crtTerm = new TermData();
-
-    private Map<String, TermData> data = new LinkedHashMap<String, TermData>();
-
-    private Map<String, Double> fieldSelection;
-
     public void transform(File input, File output, Map<String, Double> fieldSelection)
     {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     public Map<String, TermData> transform(String ontologyUrl, Map<String, Double> fieldSelection)
@@ -74,61 +38,6 @@ public class SolrUpdateGenerator
 
     public Map<String, TermData> transform(URL input, Map<String, Double> fieldSelection)
     {
-        throw new NotImplementedException();
-    }
-
-    private void storeCrtTerm()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void writeTerm(String id) throws SAXException
-    {
-        writeTerm(this.data.get(id));
-    }
-
-    private void writeTerm(TermData term) throws SAXException
-    {
-        throw new NotImplementedException();
-    }
-
-    private void writeField(String name, String value) throws SAXException
-    {
-        throw new NotImplementedException();
-    }
-
-    private boolean isFieldSelected(String name)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void loadField(String name, String value)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void propagateAncestors()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void startElement(String qName) throws SAXException
-    {
-        throw new NotImplementedException();
-    }
-
-    private void endElement(String qName) throws SAXException
-    {
-        throw new NotImplementedException();
-    }
-
-    private void addAttribute(String qName, Object value) throws SAXException
-    {
-        throw new NotImplementedException();
-    }
-
-    private void characters(Object value) throws SAXException
-    {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
