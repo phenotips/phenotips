a97a716d1c6d11725f6957e8443eca4ebf0db18d
mjshepherd <mjshepherd@users.noreply.github.com>
2014-12-23 10:05:32 -0500

    Issue-#1327:"Workgroup Administrators" group should have "Workgroup" as the parent document

--- components/users/api/src/main/java/org/phenotips/groups/internal/listeners/GroupSetupEventListener.java
+++ components/users/api/src/main/java/org/phenotips/groups/internal/listeners/GroupSetupEventListener.java
@@ -88,6 +88,7 @@ public class GroupSetupEventListener implements EventListener
         return Collections.<Event>singletonList(new DocumentCreatingEvent());
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onEvent(Event event, Object source, Object data)
     {
@@ -109,6 +110,7 @@ public class GroupSetupEventListener implements EventListener
             XWikiDocument adminsDoc = xwiki.getDocument(adminsReference, context);
             setMembers(this.dab.getCurrentUserReference(), adminsDoc, context);
             setRights(adminsReference, adminsDoc, context);
+            adminsDoc.setParent(docReference.toString());
             xwiki.saveDocument(adminsDoc, "Automatically created administrative group", true, context);
 
             // Setup the new group
