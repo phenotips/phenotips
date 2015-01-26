554a20a1c331c97deb83d8f9bb6f4cda4012c079
mjshepherd <matthewshepherd@live.com>
2015-01-26 10:41:17 -0500

    Issue #1224: Phenotype mapping documents should use plain JSON syntax

--- components/patient-tools/src/main/java/org/phenotips/tools/PhenotypeMappingService.java
+++ components/patient-tools/src/main/java/org/phenotips/tools/PhenotypeMappingService.java
@@ -58,6 +58,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.velocity.VelocityContext;
 import org.slf4j.Logger;
 
+import net.sf.json.JSONObject;
+
 /**
  * Provides access to the phenotype mappings configured for the current space. The field mappings are defined by a
  * Groovy script contained in a document. The name of that document must be configured in the "phenotypeMapping" field
@@ -226,13 +228,9 @@ public class PhenotypeMappingService implements ScriptService, EventListener, In
         DocumentReference mappingDoc = getMappingDocument();
         Object result = getMapping(mappingDoc, mappingName);
         if (result == null) {
-            Map<String, Object> mappings;
             try {
-                if (this.bridge.getDocumentContentForDefaultLanguage(mappingDoc).contains("{{velocity")) {
-                    mappings = parseVelocityMapping(mappingDoc);
-                } else {
-                    mappings = parseGroovyMapping(mappingDoc);
-                }
+                JSONObject mappings =
+                    JSONObject.fromObject(this.bridge.getDocumentContentForDefaultLanguage(mappingDoc));
                 result = mappings.get(mappingName);
             } catch (Exception ex) {
                 this.logger.warn("Failed to access mapping: {}", ex.getMessage());
