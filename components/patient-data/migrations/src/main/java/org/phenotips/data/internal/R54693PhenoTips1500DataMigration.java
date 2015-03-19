8425ba6b6cdf619ade67019a8a79cdbda73d3bc5
mjshepherd <matthewshepherd@live.com>
2015-03-19 15:26:04 -0400

    Issue #1500: Mode of inheritance should be multiselect

---
+++ components/patient-data/migrations/src/main/java/org/phenotips/data/internal/R54693PhenoTips1500DataMigration.java
@@ -0,0 +1,141 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.phenotips.data.internal;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.model.reference.EntityReferenceSerializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.objects.StringListProperty;
+import com.xpn.xwiki.objects.StringProperty;
+import com.xpn.xwiki.store.XWikiHibernateBaseStore;
+import com.xpn.xwiki.store.migration.DataMigrationException;
+import com.xpn.xwiki.store.migration.XWikiDBVersion;
+import com.xpn.xwiki.store.migration.hibernate.AbstractHibernateDataMigration;
+
+/**
+ * Migration for PhenoTips issue #1500: Mode of inheritance should be multiselect.
+ *
+ * @version $Id$
+ * @since 1.2M0
+ */
+@Component
+@Named("R54693PhenoTips#1500")
+@Singleton
+public class R54693PhenoTips1500DataMigration extends AbstractHibernateDataMigration
+{
+    /** Logging helper object. */
+    @Inject
+    private Logger logger;
+
+    /** Resolves unprefixed document names to the current wiki. */
+    @Inject
+    @Named("current")
+    private DocumentReferenceResolver<String> resolver;
+
+    /** Serializes the class name without the wiki prefix, to be used in the database query. */
+    @Inject
+    @Named("compactwiki")
+    private EntityReferenceSerializer<String> serializer;
+
+    @Override
+    public String getDescription()
+    {
+        return "Multiple values should be selectable for global mode of inheritance.";
+    }
+
+    @Override
+    public XWikiDBVersion getVersion()
+    {
+        return new XWikiDBVersion(54693);
+    }
+
+    @Override
+    public void hibernateMigrate() throws DataMigrationException, XWikiException
+    {
+        getStore().executeWrite(getXWikiContext(), new UpdateModeOfInheritanceCallback());
+    }
+
+    /**
+     * Searches for all documents containing a global_mode_of_inheritance property and changes that property type from
+     * {@code StringProperty} to {@code StringListProperty}
+     */
+    private final class UpdateModeOfInheritanceCallback implements XWikiHibernateBaseStore.HibernateCallback<Object>
+    {
+        /** The names of the properties to fix. */
+        private final String propertyName = "global_mode_of_inheritance";
+
+        @Override
+        public Object doInHibernate(Session session) throws HibernateException, XWikiException
+        {
+
+            XWikiContext context = R54693PhenoTips1500DataMigration.this.getXWikiContext();
+            XWiki xwiki = context.getWiki();
+            DocumentReference classReference =
+                new DocumentReference(context.getDatabase(), "PhenoTips", "PatientClass");
+            Query q =
+                session.createQuery("select distinct o.name from BaseObject o, StringProperty p where o.className = '"
+                    + R54693PhenoTips1500DataMigration.this.serializer.serialize(classReference)
+                    + "and p.id.id = o.id and p.id.name =" + this.propertyName + "'");
+
+            @SuppressWarnings("unchecked")
+            List<StringProperty> properties = q.list();
+            R54693PhenoTips1500DataMigration.this.logger.debug("Found {} global mode of inheritance properties",
+                properties.size());
+            for (StringProperty oldProperty : properties) {
+                try {
+                    StringListProperty newProperty = new StringListProperty();
+                    newProperty.setName(oldProperty.getName());
+                    newProperty.setId(oldProperty.getId());
+
+                    List<String> newValue = new ArrayList<String>();
+                    if (oldProperty.getValue() != null) {
+                        newValue.add(oldProperty.getValue());
+                        newProperty.setValue(newValue);
+                    }
+                    session.delete(oldProperty);
+                    session.save(newProperty);
+                } catch (Exception e) {
+                    R54693PhenoTips1500DataMigration.this.logger
+                        .warn("Failed to update a global mode of inheritance property. Exception message: {}",
+                            e.getMessage());
+                }
+
+            }
+            return null;
+        }
+    }
+}
