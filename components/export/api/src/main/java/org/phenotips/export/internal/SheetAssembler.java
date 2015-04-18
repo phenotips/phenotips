a2adc35d12a205859c9986d7b578bb819d017283
Anton Kats <antonkats@gmail.com>
2015-04-18 18:15:02 -0400

    Issue-1575: Information about Solved cases is not included in the Excel export.
    (cherry picked from commit 260686551b2349d7c07261982bde1699634ce663)

--- components/export/api/src/main/java/org/phenotips/export/internal/SheetAssembler.java
+++ components/export/api/src/main/java/org/phenotips/export/internal/SheetAssembler.java
@@ -123,6 +123,7 @@ public class SheetAssembler
             patientSections.add(converter.phenotypeBody(patient));
             patientSections.add(converter.genesBody(patient));
             patientSections.add(converter.disordersBody(patient));
+            patientSections.add(converter.isSolvedBody(patient));
 
             /* Null section filter */
             Iterator<DataSection> it = patientSections.iterator();
@@ -155,6 +156,7 @@ public class SheetAssembler
         headerSections.add(converter.phenotypeHeader());
         headerSections.add(converter.genesHeader());
         headerSections.add(converter.disordersHeaders(enabledFields));
+        headerSections.add(converter.isSolvedHeader(enabledFields));
 
         Iterator<DataSection> it = headerSections.iterator();
         while (it.hasNext()) {
