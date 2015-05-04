d65482dbee9f62285f06d1e293362cc55f5da0c8
veronikaslc <veronika.koltunov@gmail.com>
2015-05-04 17:49:59 -0400

    Issue #1531: "Patient toJSON() should include more fields" tests coverage changed from 0.16 to 0.15

--- components/patient-data/impl/src/main/java/org/phenotips/data/internal/controller/RejectedGeneListController.java
+++ components/patient-data/impl/src/main/java/org/phenotips/data/internal/controller/RejectedGeneListController.java
@@ -36,10 +36,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.BaseObject;
@@ -67,11 +69,16 @@ public class RejectedGeneListController extends AbstractComplexController<Map<St
 
     private static final String CONTROLLER_NAME = REJECTEDGENES_STRING;
 
-    private static final String REJECTEDGENES_ENABLING_FIELD_NAME          = REJECTEDGENES_STRING;
+    private static final String REJECTEDGENES_ENABLING_FIELD_NAME = REJECTEDGENES_STRING;
+
     private static final String REJECTEDGENES_COMMENTS_ENABLING_FIELD_NAME = "rejectedGenes_comments";
 
-    private static final String GENE_KEY     = "gene";
+    private static final String GENE_KEY = "gene";
+
     private static final String COMMENTS_KEY = "comments";
+
+    @Inject
+    private Logger logger;
 
     @Override
     public String getName()
@@ -126,7 +133,8 @@ public class RejectedGeneListController extends AbstractComplexController<Map<St
             }
             return new IndexedPatientData<Map<String, String>>(getName(), allGenes);
         } catch (Exception e) {
-            // TODO. Log an error.
+            this.logger.error("Could not find requested document or some unforeseen "
+                + "error has occurred during controller loading ", e.getMessage());
         }
         return null;
     }
@@ -155,13 +163,16 @@ public class RejectedGeneListController extends AbstractComplexController<Map<St
         while (iterator.hasNext()) {
             Map<String, String> item = iterator.next();
 
-            if (StringUtils.isBlank(item.get(COMMENTS_KEY))
-                || (selectedFieldNames != null
-                    && !selectedFieldNames.contains(REJECTEDGENES_COMMENTS_ENABLING_FIELD_NAME))) {
-                item.remove(COMMENTS_KEY);
-            }
+            if (!StringUtils.isBlank(item.get(GENE_KEY))) {
 
-            container.add(item);
+                if (StringUtils.isBlank(item.get(COMMENTS_KEY))
+                    || (selectedFieldNames != null
+                    && !selectedFieldNames.contains(REJECTEDGENES_COMMENTS_ENABLING_FIELD_NAME))) {
+                    item.remove(COMMENTS_KEY);
+                }
+
+                container.add(item);
+            }
         }
     }
 }
