33c020c4d06928dbf01fb0969d4883d49978e654
veronikaslc <veronika.koltunov@gmail.com>
2015-06-03 16:16:00 -0400

    some fixes

--- components/vocabularies/hgnc/api/src/main/java/org/phenotips/vocabulary/internal/GeneNomenclature.java
+++ components/vocabularies/hgnc/api/src/main/java/org/phenotips/vocabulary/internal/GeneNomenclature.java
@@ -299,7 +299,7 @@ public class GeneNomenclature implements Vocabulary, Initializable
     public Set<String> getAliases()
     {
         Set<String> result = new HashSet<String>();
-        result.add("hgnc");
+        result.add("hgnc-remote");
         result.add("HGNC");
         return result;
     }
