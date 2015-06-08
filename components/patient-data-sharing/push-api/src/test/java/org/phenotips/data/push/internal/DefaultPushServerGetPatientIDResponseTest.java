4c113f1be05e12fd3b2ad76d69f838de61243f79
JKereliuk <j.kereliuk@mail.utoronto.ca>
2015-06-08 11:27:03 -0400

    added tests for this class

---
+++ components/patient-data-sharing/push-api/src/test/java/org/phenotips/data/push/internal/DefaultPushServerGetPatientIDResponseTest.java
@@ -0,0 +1,57 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.data.push.internal;
+
+
+import net.sf.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultPushServerGetPatientIDResponseTest {
+
+    private DefaultPushServerGetPatientIDResponse pushServerResponse;
+
+    @Before
+    public void setUp()
+    {
+        JSONObject response = new JSONObject();
+        pushServerResponse = new DefaultPushServerGetPatientIDResponse(response);
+    }
+
+    @Test
+    public void getRemotePatientGUIDReturnsCorrectValue()
+    {
+        pushServerResponse.response.accumulate("patient_guid", "a");
+        Assert.assertEquals("a", pushServerResponse.getRemotePatientGUID());
+    }
+
+    @Test
+    public void getRemotePatientURLReturnsCorrectValue()
+    {
+        pushServerResponse.response.accumulate("patient_url", "b");
+        Assert.assertEquals("b", pushServerResponse.getRemotePatientURL());
+    }
+
+    @Test
+    public void getRemotePatientIdReturnsCorrectValue()
+    {
+        pushServerResponse.response.accumulate("patient_id", "c");
+        Assert.assertEquals("c", pushServerResponse.getRemotePatientID());
+    }
+}
