6708f9af95e5472015364600e514cc7ff3fff75d
JKereliuk <j.kereliuk@mail.utoronto.ca>
2015-06-08 12:01:49 -0400

    Added tests for DefaultPushServerInfo

---
+++ components/patient-data-sharing/push-api/src/test/java/org/phenotips/data/push/internal/DefaultPushServerInfoTest.java
@@ -0,0 +1,66 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.data.push.internal;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultPushServerInfoTest {
+
+    private DefaultPushServerInfo serverInfo;
+
+    @Before
+    public void setUp()
+    {
+        serverInfo = new DefaultPushServerInfo("serverId", "serverURL", "serverDescription");
+    }
+
+    @Test
+    public void getServerIDReturnsCorrectID()
+    {
+        Assert.assertEquals("serverId", serverInfo.getServerID());
+    }
+
+    @Test
+    public void getServerURLReturnsCorrectURL()
+    {
+        Assert.assertEquals("serverURL", serverInfo.getServerURL());
+    }
+
+    @Test
+    public void getServerDescriptionReturnsCorrectDescription()
+    {
+        Assert.assertEquals("serverDescription", serverInfo.getServerDescription());
+    }
+
+    @Test
+    public void compareToIdentifiesSameServerID()
+    {
+        DefaultPushServerInfo otherInfo = new DefaultPushServerInfo("serverId", "otherURL", "otherDescription");
+        Assert.assertEquals(serverInfo.compareTo(otherInfo), 0);
+    }
+
+    @Test
+    public void compareToIdentifiesDifferentServerID()
+    {
+        DefaultPushServerInfo otherInfo = new DefaultPushServerInfo("otherId", "otherURL", "otherDescription");
+        Assert.assertNotEquals(serverInfo.compareTo((otherInfo)), 0);
+    }
+
+}
