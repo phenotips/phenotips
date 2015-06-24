34d8b857ea8378c8cb19a9c87e086d0af635bb48
DeanWay <deanway93@gmail.com>
2015-06-24 16:46:31 -0400

    Added Test for object contruction of ConfigureISODatesInJson class

--- components/patient-data/rest/src/test/java/org/phenotips/data/rest/internal/ConfigureISODatesInJsonTest.java
+++ components/patient-data/rest/src/test/java/org/phenotips/data/rest/internal/ConfigureISODatesInJsonTest.java
@@ -1,7 +1,25 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
 package org.phenotips.data.rest.internal;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig.Feature;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,6 +30,7 @@ import org.restlet.engine.Engine;
 import org.restlet.engine.converter.ConverterHelper;
 import org.restlet.ext.jackson.JacksonConverter;
 import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.observation.event.ApplicationStartedEvent;
 import org.xwiki.observation.event.Event;
 import org.xwiki.observation.EventListener;
 import org.xwiki.test.mockito.MockitoComponentMockingRule;
@@ -21,7 +40,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by Dean on 2015-06-24.
+ * Tests for the {@link ConfigureISODatesInJson} component.
+ *
  */
 public class ConfigureISODatesInJsonTest{
 
@@ -56,6 +76,13 @@ public class ConfigureISODatesInJsonTest{
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         converterList = new LinkedList<>();
+    }
+
+    @Test
+    public void checkConstruction() {
+        ConfigureISODatesInJson testInstance = new ConfigureISODatesInJson();
+        Assert.assertEquals("ConfigureISODatesInJson", testInstance.getName());
+        Assert.assertTrue(testInstance.getEvents().get(0).getClass() == ApplicationStartedEvent.class);
     }
 
     @Test
