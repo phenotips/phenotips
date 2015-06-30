5f54ed673bcee996e3403a22bd8ef4c64eb49a3e
DeanWay <deanway93@gmail.com>
2015-06-30 16:59:02 -0400

    Added comment header to DefaultPatientResourceImplTest class

--- components/patient-data/rest/src/test/java/org/phenotips/data/rest/internal/DefaultPatientResourceImplTest.java
+++ components/patient-data/rest/src/test/java/org/phenotips/data/rest/internal/DefaultPatientResourceImplTest.java
@@ -71,7 +71,10 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
+/**
+ * Tests for the {@link DefaultPatientResourceImpl} component.
+ *
+ */
 public class DefaultPatientResourceImplTest {
 
     @Rule
