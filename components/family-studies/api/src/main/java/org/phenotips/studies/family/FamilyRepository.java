a302a2b1e01837198fbdc1cadc5898528f90b3fd
ItaiGershtansky <itaig.phenotips@gmail.com>
2015-07-03 17:40:33 -0400

    family-studies - Improve support for family studies Resolved conflicting merge in comment.

--- components/family-studies/api/src/main/java/org/phenotips/studies/family/FamilyRepository.java
+++ components/family-studies/api/src/main/java/org/phenotips/studies/family/FamilyRepository.java
@@ -43,11 +43,8 @@ public interface FamilyRepository
     Family getFamilyById(String id);
 
     /**
-<<<<<<< HEAD
      * Creates a new empty family.
      *
-=======
->>>>>>> 06e2cb761467ea9a1c6ed72090afd676d891e7de
      * @return new Family object
      */
     Family createFamily();
