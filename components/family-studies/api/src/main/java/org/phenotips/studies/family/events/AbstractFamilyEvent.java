977aa486f2eaf16861153ebee682134372bfe772
Anton Kats <antonkats@gmail.com>
2015-07-03 11:38:35 -0400

    Added a part of the event listnening module

---
+++ components/family-studies/api/src/main/java/org/phenotips/studies/family/events/AbstractFamilyEvent.java
@@ -0,0 +1,88 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.studies.family.events;
+
+import org.xwiki.users.User;
+import org.phenotips.studies.family.Family;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Base class for implementing {@link FamilyEvent}.
+ *
+ * @version $Id$
+ * @since 1.0RC1
+ */
+public abstract class AbstractFamilyEvent implements FamilyEvent
+{
+    /** The type of this event. */
+    protected final String eventType;
+
+    /** The affected family. */
+    protected final Family family;
+
+    /** The user performing this action. */
+    protected final User author;
+
+    /**
+     * Constructor initializing the required fields.
+     *
+     * @param eventType the type of this event
+     * @param family the affected family
+     * @param author the user performing this action
+     */
+    protected AbstractFamilyEvent(String eventType, Family family, User author)
+    {
+        this.eventType = eventType;
+        this.family = family;
+        this.author = author;
+    }
+
+    @Override
+    public boolean matches(Object otherEvent)
+    {
+        if (otherEvent instanceof FamilyEvent) {
+            FamilyEvent otherFamilyEvent = (FamilyEvent) otherEvent;
+            if (!StringUtils.equals(otherFamilyEvent.getEventType(), this.eventType)) {
+                return false;
+            }
+            return this.family == null || this.family.getDocumentReference() == null
+                || (otherFamilyEvent.getFamily() != null && this.family.getDocumentReference().equals(
+                    otherFamilyEvent.getFamily().getDocumentReference()));
+        }
+        return false;
+    }
+
+    @Override
+    public String getEventType()
+    {
+        return this.eventType;
+    }
+
+    @Override
+    public Family getFamily()
+    {
+        return this.family;
+    }
+
+    @Override
+    public User getAuthor()
+    {
+        return this.author;
+    }
+}
