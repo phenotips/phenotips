6de577d9cd52f2428451ee552dc567cc715b7799
ItaiGershtansky <itaig.phenotips@gmail.com>
2015-07-22 16:53:29 -0400

    issue-1775: Add 'Allergies- environmental, food, medication' Created AbstractPatientDataUpdater with implementation of two methods that are used in multiple PatientXUpdater-s.

---
+++ components/patient-data/impl/src/main/java/org/phenotips/data/internal/AbstractPatientDataUpdater.java
@@ -0,0 +1,95 @@
+package org.phenotips.data.internal;
+
+import org.phenotips.Constants;
+import org.phenotips.data.Patient;
+
+import org.xwiki.container.Container;
+import org.xwiki.container.Request;
+import org.xwiki.observation.AbstractEventListener;
+import org.xwiki.observation.ObservationManager;
+import org.xwiki.observation.event.Event;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+/**
+ * Some method implementations that can be used by objects that update patient data.
+ *
+ * @version $Id$
+ */
+public abstract class AbstractPatientDataUpdater extends AbstractEventListener
+{
+    /** Needed for getting access to the request. */
+    @Inject
+    private Container container;
+
+    /**
+     * @param name the listener's name. It's a free form text identifying this listener instance in a unique manner.
+     *            This name is used for some operations in {@link ObservationManager}.
+     * @param events the list of events this listener is configured to receive. This listener will be automatically
+     *            registered with this list of events against the {@link ObservationManager}. When an event occurs, for
+     *            each matching event in this list, the {@link #onEvent(Event, Object, Object)} method will be called.
+     */
+    public AbstractPatientDataUpdater(String name, List<? extends Event> events)
+    {
+        super(name, events);
+    }
+
+    /**
+     * @param name the listener's name. It's a free form text identifying this listener instance in a unique manner.
+     *            This name is used for some operations in {@link ObservationManager}.
+     * @param events the list of events this listener is configured to receive. This listener will be automatically
+     *            registered with this list of events against the {@link ObservationManager}. When an event occurs, for
+     *            each matching event in this list, the {@link #onEvent(Event, Object, Object)} method will be called.
+     */
+    public AbstractPatientDataUpdater(String name, Event... events)
+    {
+        super(name, events);
+    }
+
+    /**
+     * Read a property from the request.
+     *
+     * @param propertyName the name of the property as it would appear in the class, for example
+     *            {@code age_of_onset_years}
+     * @param objectNumber the object's number
+     * @return the value sent in the request, or false if not set
+     */
+    protected boolean getParameter(String propertyName, int objectNumber)
+    {
+        String parameterName = Constants.CODE_SPACE + ".PatientClass_" + objectNumber + "_" + propertyName;
+        Request request = this.container.getRequest();
+        if (request == null) {
+            return false;
+        }
+        String value = (String) request.getProperty(parameterName);
+        if (!StringUtils.isNumeric(value)) {
+            return false;
+        }
+        return (Integer.valueOf(value) == 1);
+    }
+
+    /**
+     * Returns a patient record base object.
+     *
+     * @param source the event source i.e. the object for which the event was triggered.
+     * @return a patient record object
+     */
+    protected BaseObject getPatientRecord(Object source)
+    {
+        if (source instanceof XWikiDocument) {
+            XWikiDocument doc = (XWikiDocument) source;
+            BaseObject patientRecordObj = doc.getXObject(Patient.CLASS_REFERENCE);
+            return patientRecordObj;
+        } else {
+            return null;
+        }
+    }
+
+}
