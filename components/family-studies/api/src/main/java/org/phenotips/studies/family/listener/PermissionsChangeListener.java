1931f94e7a3da0662dfeccbcd6bef606e3068ee8
ItaiGershtansky <itaig.phenotips@gmail.com>
2015-08-06 12:37:37 -0400

    family-studies - Improve support for family studies Updated PermissionsChangeListener to work with new interface.

--- components/family-studies/api/src/main/java/org/phenotips/studies/family/listener/PermissionsChangeListener.java
+++ components/family-studies/api/src/main/java/org/phenotips/studies/family/listener/PermissionsChangeListener.java
@@ -17,26 +17,21 @@
  */
 package org.phenotips.studies.family.listener;
 
+import org.phenotips.data.Patient;
+import org.phenotips.data.PatientRepository;
 import org.phenotips.data.events.PatientChangedEvent;
 import org.phenotips.data.events.PatientDeletedEvent;
-import org.phenotips.studies.family.FamilyUtils;
-import org.phenotips.studies.family.Processing;
+import org.phenotips.studies.family.Family;
+import org.phenotips.studies.family.FamilyRepository;
 
 import org.xwiki.component.annotation.Component;
-import org.xwiki.context.Execution;
 import org.xwiki.observation.AbstractEventListener;
 import org.xwiki.observation.event.Event;
-
-import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.slf4j.Logger;
-
-import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
 
 /**
@@ -52,16 +47,10 @@ import com.xpn.xwiki.doc.XWikiDocument;
 public class PermissionsChangeListener extends AbstractEventListener
 {
     @Inject
-    private Logger logger;
+    private FamilyRepository familyRepository;
 
     @Inject
-    private Execution execution;
-
-    @Inject
-    private FamilyUtils utils;
-
-    @Inject
-    private Processing processingUtils;
+    private PatientRepository patientRepository;
 
     /** Default constructor, sets up the listener name and the list of events to subscribe to. */
     public PermissionsChangeListener()
@@ -69,26 +58,24 @@ public class PermissionsChangeListener extends AbstractEventListener
         super("family-studies-permissions-listener", new PatientChangedEvent(), new PatientDeletedEvent());
     }
 
+    // TODO: test!
     @Override
     public void onEvent(Event event, Object source, Object data)
     {
-        XWikiDocument doc = (XWikiDocument) source;
-        XWikiContext context = (XWikiContext) this.execution.getContext().getProperty("xwikicontext");
-        try {
-            XWikiDocument familyDoc = utils.getFamilyDoc(doc);
-            if (familyDoc != null) {
-                List<String> members = utils.getFamilyMembers(familyDoc);
-                if (event instanceof PatientDeletedEvent) {
-                    members.remove(doc.getDocumentReference().getName());
-                    utils.setFamilyMembers(familyDoc, members);
-                }
-                processingUtils.setUnionOfUserPermissions(familyDoc, members);
-                // todo. delete family doc if no members left
-                context.getWiki().saveDocument(familyDoc, context);
-            }
-        } catch (XWikiException ex) {
-            logger.warn("Could not update family permissions");
+        XWikiDocument xwikiDoc = (XWikiDocument) source;
+        String familyId = xwikiDoc.getDocumentReference().getName();
+        Family family = this.familyRepository.getFamilyById(familyId);
+
+        if (event instanceof PatientDeletedEvent) {
+            String patientId = (String) data;
+            Patient p = this.patientRepository.getPatientById(patientId);
+
+            family.removeMember(p);
+
+            // TODO delete family if no members left
+        } else {
+            // if member was deleted (true branch of if), this is done in removing member.
+            family.updatePermissions();
         }
     }
 }
-
