6309650353fdfac16035dd23d3b18f55ba465580
DeanWay <deanway93@gmail.com>
2015-08-24 20:47:22 -0400

    Check before casting. It's possible that AbstractComplexController is not the immediate parent

--- components/patient-data/impl/src/main/java/org/phenotips/data/internal/controller/AbstractComplexController.java
+++ components/patient-data/impl/src/main/java/org/phenotips/data/internal/controller/AbstractComplexController.java
@@ -29,6 +29,7 @@ import org.xwiki.component.util.DefaultParameterizedType;
 import org.xwiki.model.reference.ObjectPropertyReference;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -147,7 +148,11 @@ public abstract class AbstractComplexController<T> implements PatientDataControl
      */
     protected boolean isCodeFieldsOnly()
     {
-        ParameterizedType t = (ParameterizedType) this.getClass().getGenericSuperclass();
+        Type type = this.getClass().getGenericSuperclass();
+        if(! (type instanceof ParameterizedType)) {
+            return false;
+        }
+        ParameterizedType t = (ParameterizedType) type;
         return new DefaultParameterizedType(null, List.class, VocabularyProperty.class).equals(
             t.getActualTypeArguments()[0]);
     }
