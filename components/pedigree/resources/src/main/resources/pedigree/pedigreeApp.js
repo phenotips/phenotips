ff0e4f76b20ed74e8e5f4b6c88914b840249a16b
mjshepherd <matthewshepherd@live.com>
2015-08-28 17:04:58 -0400

    Issue: Minify Pedigree

--- components/pedigree/resources/src/main/resources/pedigree/pedigreeApp.js
+++ components/pedigree/resources/src/main/resources/pedigree/pedigreeApp.js
@@ -1,9 +1,4 @@
 //Configures requireJS and starts the PedigreeEditor
-require.config({
-  paths : {
-    pedigree : "$stringtool.substringBefore($xwiki.getSkinFile('uicomponents/pedigree/pedigree.js', true), 'pedigree.js')" 
-  }
-})
 
 require(["pedigree/pedigree"], function (PedigreeEditor){
   (XWiki && XWiki.domIsLoaded && new PedigreeEditor()) || document.observe("xwiki:dom:loaded", function(){new PedigreeEditor();});
---
+++ components/pedigree/resources/src/main/resources/pedigree/requireConfigs/optimizerConfig.js
@@ -0,0 +1,5 @@
+require.config({
+  paths : {
+    pedigree : "../pedigree"
+  }
+})
