52d339924f1fe94cd7c1a7391f1ed9d323c9a44f
Sergiu Dumitriu <sergiu@phenotips.org>
2015-09-26 20:55:48 -0400

    [cleanup] Applied codestyle

--- components/patient-data/rest/src/test/java/org/phenotips/data/rest/internal/DefaultPatientsResourceImplTest.java
+++ components/patient-data/rest/src/test/java/org/phenotips/data/rest/internal/DefaultPatientsResourceImplTest.java
@@ -17,22 +17,13 @@
  */
 package org.phenotips.data.rest.internal;
 
-import static org.mockito.Mockito.*;
-
-import com.xpn.xwiki.XWikiContext;
-import net.sf.json.JSONObject;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.phenotips.data.Patient;
 import org.phenotips.data.PatientRepository;
 import org.phenotips.data.rest.DomainObjectFactory;
 import org.phenotips.data.rest.PatientsResource;
-import org.phenotips.data.Patient;
 import org.phenotips.data.rest.model.PatientSummary;
 import org.phenotips.data.rest.model.Patients;
+
 import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.component.util.ReflectionUtils;
@@ -40,7 +31,6 @@ import org.xwiki.context.Execution;
 import org.xwiki.context.ExecutionContext;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.EntityReference;
-import org.xwiki.model.reference.EntityReferenceResolver;
 import org.xwiki.query.Query;
 import org.xwiki.query.QueryException;
 import org.xwiki.query.QueryManager;
@@ -51,19 +41,39 @@ import org.xwiki.test.mockito.MockitoComponentMockingRule;
 import org.xwiki.users.User;
 import org.xwiki.users.UserManager;
 
-import javax.inject.Provider;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Provider;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 
-public class DefaultPatientsResourceImplTest {
+import com.xpn.xwiki.XWikiContext;
 
+import net.sf.json.JSONObject;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DefaultPatientsResourceImplTest
+{
     @Rule
     public MockitoComponentMockingRule<PatientsResource> mocker =
         new MockitoComponentMockingRule<PatientsResource>(DefaultPatientsResourceImpl.class);
@@ -98,9 +108,9 @@ public class DefaultPatientsResourceImplTest {
 
     private XWikiContext context;
 
-
     @Before
-    public void setUp() throws ComponentLookupException, URISyntaxException {
+    public void setUp() throws ComponentLookupException, URISyntaxException
+    {
         MockitoAnnotations.initMocks(this);
         Execution execution = mock(Execution.class);
         ExecutionContext executionContext = mock(ExecutionContext.class);
@@ -114,7 +124,7 @@ public class DefaultPatientsResourceImplTest {
         this.repository = this.mocker.getInstance(PatientRepository.class);
         this.users = this.mocker.getInstance(UserManager.class);
         this.access = this.mocker.getInstance(AuthorizationManager.class);
-        this.patientsResource = (DefaultPatientsResourceImpl)this.mocker.getComponentUnderTest();
+        this.patientsResource = (DefaultPatientsResourceImpl) this.mocker.getComponentUnderTest();
         this.logger = this.mocker.getMockedLogger();
         this.queries = this.mocker.getInstance(QueryManager.class);
         this.uri = new URI("http://uri");
@@ -131,13 +141,14 @@ public class DefaultPatientsResourceImplTest {
     }
 
     @Test
-    public void addPatientUserDoesNotHaveAccess() {
+    public void addPatientUserDoesNotHaveAccess()
+    {
         WebApplicationException exception = null;
-        doReturn(false).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class), any(EntityReference.class));
+        doReturn(false).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class),
+            any(EntityReference.class));
         try {
             Response response = this.patientsResource.addPatient("");
-        }
-        catch (WebApplicationException ex){
+        } catch (WebApplicationException ex) {
             exception = ex;
         }
         Assert.assertNotNull(exception);
@@ -145,8 +156,10 @@ public class DefaultPatientsResourceImplTest {
     }
 
     @Test
-    public void addEmptyPatient() {
-        doReturn(true).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class), any(EntityReference.class));
+    public void addEmptyPatient()
+    {
+        doReturn(true).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class),
+            any(EntityReference.class));
         doReturn(this.patient).when(this.repository).createNewPatient();
         Response response = this.patientsResource.addPatient(null);
         Assert.assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
@@ -154,10 +167,12 @@ public class DefaultPatientsResourceImplTest {
     }
 
     @Test
-    public void creatingPatientFails() {
+    public void creatingPatientFails()
+    {
         JSONObject json = new JSONObject();
         Exception exception = new NullPointerException();
-        doReturn(true).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class), any(EntityReference.class));
+        doReturn(true).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class),
+            any(EntityReference.class));
         doThrow(exception).when(this.repository).createNewPatient();
         Response response = this.patientsResource.addPatient(json.toString());
         Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
@@ -165,8 +180,10 @@ public class DefaultPatientsResourceImplTest {
     }
 
     @Test
-    public void addPatientAsJSON() {
-        doReturn(true).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class), any(EntityReference.class));
+    public void addPatientAsJSON()
+    {
+        doReturn(true).when(this.access).hasAccess(eq(Right.EDIT), any(DocumentReference.class),
+            any(EntityReference.class));
         doReturn(this.patient).when(this.repository).createNewPatient();
         JSONObject jsonPatient = new JSONObject();
         Response response = this.patientsResource.addPatient(jsonPatient.toString());
@@ -175,43 +192,53 @@ public class DefaultPatientsResourceImplTest {
     }
 
     @Test
-    public void listPatientsNullOrderField() throws QueryException {
+    public void listPatientsNullOrderField() throws QueryException
+    {
         Query query = mock(DefaultQuery.class);
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(new ArrayList<Object[]>()).when(query).execute();
         Patients result = this.patientsResource.listPatients(0, 30, null, "asc");
-        verify(this.queries).createQuery("select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
+        verify(this.queries).createQuery(
+            "select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
                 + " from Document doc, doc.object(PhenoTips.PatientClass) p where doc.name <> :t order by "
-                + "doc.name" + " asc", "xwql");
+                + "doc.name" + " asc",
+            "xwql");
     }
 
     @Test
-    public void listPatientsNullOrder() throws QueryException {
+    public void listPatientsNullOrder() throws QueryException
+    {
         Query query = mock(DefaultQuery.class);
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(new ArrayList<Object[]>()).when(query).execute();
         Patients result = this.patientsResource.listPatients(0, 30, "id", null);
-        verify(this.queries).createQuery("select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
+        verify(this.queries).createQuery(
+            "select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
                 + " from Document doc, doc.object(PhenoTips.PatientClass) p where doc.name <> :t order by "
-                + "doc.name" + " asc", "xwql");
+                + "doc.name" + " asc",
+            "xwql");
     }
 
     @Test
-    public void listPatientsNonDefaultBehaviour() throws QueryException {
+    public void listPatientsNonDefaultBehaviour() throws QueryException
+    {
         Query query = mock(DefaultQuery.class);
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(new ArrayList<Object[]>()).when(query).execute();
         Patients result = this.patientsResource.listPatients(0, 30, "eid", "desc");
-        verify(this.queries).createQuery("select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
+        verify(this.queries).createQuery(
+            "select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
                 + " from Document doc, doc.object(PhenoTips.PatientClass) p where doc.name <> :t order by "
-                + "p.external_id" + " desc", "xwql");
+                + "p.external_id" + " desc",
+            "xwql");
     }
 
     @Test
-    public void listPatientsNoUserAccess() throws QueryException {
+    public void listPatientsNoUserAccess() throws QueryException
+    {
         Object[] patientSummaryData = new Object[7];
         List<Object[]> patientList = new ArrayList<Object[]>();
         patientList.add(patientSummaryData);
@@ -219,17 +246,21 @@ public class DefaultPatientsResourceImplTest {
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(patientList).when(query).execute();
-        doReturn(false).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class), any(EntityReference.class));
+        doReturn(false).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class),
+            any(EntityReference.class));
         doReturn(null).when(this.factory).createPatientSummary(patientSummaryData, this.uriInfo);
         Patients result = this.patientsResource.listPatients(0, 30, "id", "asc");
-        verify(this.queries).createQuery("select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
+        verify(this.queries).createQuery(
+            "select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
                 + " from Document doc, doc.object(PhenoTips.PatientClass) p where doc.name <> :t order by "
-                + "doc.name" + " asc", "xwql");
+                + "doc.name" + " asc",
+            "xwql");
         Assert.assertTrue(result.getPatientSummaries().isEmpty());
     }
 
     @Test
-    public void listPatientsUserHasAccess() throws QueryException {
+    public void listPatientsUserHasAccess() throws QueryException
+    {
         Object[] patientSummaryData = new Object[7];
         List<Object[]> patientList = new ArrayList<Object[]>();
         patientList.add(patientSummaryData);
@@ -237,19 +268,23 @@ public class DefaultPatientsResourceImplTest {
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(patientList).when(query).execute();
-        doReturn(true).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class), any(EntityReference.class));
+        doReturn(true).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class),
+            any(EntityReference.class));
         doReturn(new PatientSummary()).when(this.factory).createPatientSummary(any(Object[].class), eq(this.uriInfo));
         Patients result = this.patientsResource.listPatients(0, 30, "id", "asc");
-        verify(this.queries).createQuery("select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
+        verify(this.queries).createQuery(
+            "select doc.fullName, p.external_id, doc.creator, doc.creationDate, doc.version, doc.author, doc.date"
                 + " from Document doc, doc.object(PhenoTips.PatientClass) p where doc.name <> :t order by "
-                + "doc.name" + " asc", "xwql");
+                + "doc.name" + " asc",
+            "xwql");
         Assert.assertFalse(result.getPatientSummaries().isEmpty());
     }
 
     @Test
-    public void listPatientsSpecificNumberOfRecords() throws QueryException{
+    public void listPatientsSpecificNumberOfRecords() throws QueryException
+    {
         List<Object[]> patientList = new ArrayList<Object[]>();
-        for(int i=0; i<30; i++ ){
+        for (int i = 0; i < 30; i++) {
             Object[] patientSummaryData = new Object[7];
             patientList.add(patientSummaryData);
         }
@@ -257,7 +292,8 @@ public class DefaultPatientsResourceImplTest {
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(patientList).when(query).execute();
-        doReturn(true).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class), any(EntityReference.class));
+        doReturn(true).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class),
+            any(EntityReference.class));
         doReturn(new PatientSummary()).when(this.factory).createPatientSummary(any(Object[].class), eq(this.uriInfo));
 
         Patients allPatients = this.patientsResource.listPatients(0, 30, "id", "asc");
@@ -266,17 +302,18 @@ public class DefaultPatientsResourceImplTest {
         Patients selectedNumberOfPatients = this.patientsResource.listPatients(15, 15, "id", "asc");
         Assert.assertEquals(15, selectedNumberOfPatients.getPatientSummaries().size());
 
-        Patients onePatient = this.patientsResource.listPatients(15, 1, "id","asc");
+        Patients onePatient = this.patientsResource.listPatients(15, 1, "id", "asc");
         Assert.assertEquals(1, onePatient.getPatientSummaries().size());
 
-        Patients incorrectLookup = patientsResource.listPatients(31, 5, "id", "asc");
+        Patients incorrectLookup = this.patientsResource.listPatients(31, 5, "id", "asc");
         Assert.assertEquals(0, incorrectLookup.getPatientSummaries().size());
     }
 
     @Test
-    public void listPatientsGetMoreRecordsThanAdded() throws QueryException{
+    public void listPatientsGetMoreRecordsThanAdded() throws QueryException
+    {
         List<Object[]> patientList = new ArrayList<Object[]>();
-        for(int i=0; i<15; i++ ){
+        for (int i = 0; i < 15; i++) {
             Object[] patientSummaryData = new Object[7];
             patientList.add(patientSummaryData);
         }
@@ -284,29 +321,29 @@ public class DefaultPatientsResourceImplTest {
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doReturn(patientList).when(query).execute();
-        doReturn(true).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class), any(EntityReference.class));
+        doReturn(true).when(this.access).hasAccess(eq(Right.VIEW), any(DocumentReference.class),
+            any(EntityReference.class));
         doReturn(new PatientSummary()).when(this.factory).createPatientSummary(any(Object[].class), eq(this.uriInfo));
         Patients result = this.patientsResource.listPatients(0, 30, "id", "asc");
         Assert.assertEquals(15, result.getPatientSummaries().size());
     }
 
     @Test
-    public void listPatientFailureHandling() throws QueryException {
+    public void listPatientFailureHandling() throws QueryException
+    {
         Query query = mock(DefaultQuery.class);
         WebApplicationException exception = null;
-        QueryException queryException= new QueryException("query.execute() failed", query, new Exception());
+        QueryException queryException = new QueryException("query.execute() failed", query, new Exception());
         doReturn(query).when(this.queries).createQuery(anyString(), anyString());
         doReturn(query).when(query).bindValue(anyString(), anyString());
         doThrow(queryException).when(query).execute();
         try {
             Patients result = this.patientsResource.listPatients(0, 30, "id", "asc");
-        }
-        catch (WebApplicationException ex){
+        } catch (WebApplicationException ex) {
             exception = ex;
         }
         Assert.assertNotNull(exception);
         Assert.assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), exception.getResponse().getStatus());
         verify(this.logger).error("Failed to list patients: {}", queryException.getMessage(), queryException);
     }
-
 }
