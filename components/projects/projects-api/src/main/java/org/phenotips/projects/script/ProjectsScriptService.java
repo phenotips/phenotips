fd8a47ebdfdb10944a192d3400de84a3ad503b0c
ItaiGershtansky <itaig.phenotips@gmail.com>
2015-10-01 13:31:14 -0400

    issue-1987-projects User can now add contributors and leadrs to a project.

---
+++ components/projects/projects-api/src/main/java/org/phenotips/projects/script/ProjectsScriptService.java
@@ -0,0 +1,193 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.projects.script;
+
+import org.phenotips.data.permissions.AccessLevel;
+import org.phenotips.data.permissions.Collaborator;
+import org.phenotips.data.permissions.PermissionsManager;
+import org.phenotips.projects.access.DefaultProjectCollaborator;
+import org.phenotips.projects.data.Project;
+
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.context.Execution;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceSerializer;
+import org.xwiki.script.service.ScriptService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+/**
+ * @version $Id$
+ */
+
+@Component
+@Named("projects")
+@Singleton
+public class ProjectsScriptService implements ScriptService
+{
+    private static final String ACCESS_KEY = "access";
+
+    private static final String COLLABORATOR_KEY = "collaborator";
+
+    @Inject
+    @Named("current")
+    private DocumentReferenceResolver<String> stringResolver;
+
+    @Inject
+    private DocumentReferenceResolver<EntityReference> entityResolver;
+
+    @Inject
+    private EntityReferenceSerializer<String> entitySerializer;
+
+    @Inject
+    private DocumentAccessBridge bridge;
+
+    @Inject
+    private Execution execution;
+
+    @Inject
+    @Named("leader")
+    private AccessLevel leaderAccessLevel;
+
+    @Inject
+    @Named("contributor")
+    private AccessLevel contributorAccessLevel;
+
+    @Inject
+    private PermissionsManager manager;
+
+    @Inject
+    private Logger logger;
+
+    /**
+     * Returns a collection project collaborators, both leaders and contributors.
+     *
+     * @param projectId identifier of the project
+     * @return a collection of collaborators
+     */
+    public Collection<Collaborator> getCollaborators(String projectId)
+    {
+        List<Collaborator> collaborators = new ArrayList<Collaborator>();
+        XWikiDocument projectDocument = this.getProjectObject(projectId);
+        DocumentReference projectReference = projectDocument.getDocumentReference();
+        DocumentReference classReference = this.entityResolver.resolve(Collaborator.CLASS_REFERENCE, projectReference);
+        Collection<BaseObject> xCollaborators = projectDocument.getXObjects(classReference);
+
+        if (xCollaborators != null) {
+            for (BaseObject o : xCollaborators) {
+                if (o == null) {
+                    continue;
+                }
+                String collaboratorName = o.getStringValue(COLLABORATOR_KEY);
+                String accessName = o.getStringValue(ACCESS_KEY);
+                if (StringUtils.isBlank(collaboratorName) || StringUtils.isBlank(accessName)) {
+                    continue;
+                }
+                EntityReference userOrGroup = this.stringResolver.resolve(collaboratorName, projectReference);
+                AccessLevel access = this.manager.resolveAccessLevel(accessName);
+                collaborators.add(new DefaultProjectCollaborator(userOrGroup, access));
+            }
+        }
+
+        return collaborators;
+    }
+
+    /**
+     * Sets the list of project collaborators.
+     *
+     * @param projectId identifier of the project
+     * @param contributors collection of contributors
+     * @param leaders collection of contributors
+     * @return true if successful
+     */
+    public boolean setCollaborators(String projectId, Collection<EntityReference> contributors,
+        Collection<EntityReference> leaders)
+    {
+        // Convert EntityReference lists to Collaborators
+        Collection<Collaborator> collaborators = new ArrayList<Collaborator>();
+        if (contributors != null) {
+            for (EntityReference contributorRef : contributors) {
+                collaborators.add(new DefaultProjectCollaborator(contributorRef, contributorAccessLevel));
+            }
+        }
+        if (leaders != null) {
+            for (EntityReference leaderRef : leaders) {
+                collaborators.add(new DefaultProjectCollaborator(leaderRef, leaderAccessLevel));
+            }
+        }
+
+        return this.setCollaborators(projectId, collaborators);
+    }
+
+    /**
+     * Sets the list of project collaborators.
+     *
+     * @param projectId identifier of the project
+     * @param collaborators collection of contributors
+     * @return true if successful
+     */
+    public boolean setCollaborators(String projectId, Collection<Collaborator> collaborators)
+    {
+        XWikiDocument projectDocument = this.getProjectObject(projectId);
+        DocumentReference projectReference = projectDocument.getDocumentReference();
+        try {
+            DocumentReference classReference =
+                this.entityResolver.resolve(Collaborator.CLASS_REFERENCE, projectReference);
+            XWikiContext context = (XWikiContext) this.execution.getContext().getProperty("xwikicontext");
+            projectDocument.removeXObjects(classReference);
+            for (Collaborator collaborator : collaborators) {
+                BaseObject o = projectDocument.newXObject(classReference, context);
+                o.setStringValue(COLLABORATOR_KEY, this.entitySerializer.serialize(collaborator.getUser()));
+                o.setStringValue(ACCESS_KEY, collaborator.getAccessLevel().getName());
+            }
+            context.getWiki().saveDocument(projectDocument, "Updated collaborators", true, context);
+            return true;
+        } catch (Exception e) {
+            this.logger.error("Error in ProjectScriptService.setCollaborators: {}", e.getMessage(), e);
+        }
+        return false;
+
+    }
+
+    private XWikiDocument getProjectObject(String projectId)
+    {
+        DocumentReference reference = this.stringResolver.resolve(projectId, Project.DEFAULT_DATA_SPACE);
+        try {
+            return (XWikiDocument) this.bridge.getDocument(reference);
+        } catch (Exception ex) {
+            this.logger.warn("Failed to access project with id [{}]: {}", projectId, ex.getMessage(), ex);
+        }
+        return null;
+    }
+}
