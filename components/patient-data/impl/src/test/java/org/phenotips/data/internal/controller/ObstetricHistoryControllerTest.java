3712539b852124492f986728f8fea592cf12f0d7
Tarang <tarang.marathe@gmail.com>
2015-10-05 18:11:04 -0400

    initial ObstetricHistoryController test

---
+++ components/patient-data/impl/src/test/java/org/phenotips/data/internal/controller/ObstetricHistoryControllerTest.java
@@ -0,0 +1,93 @@
+package org.phenotips.data.internal.controller;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.phenotips.data.Patient;
+import org.phenotips.data.PatientData;
+import org.phenotips.data.PatientDataController;
+import org.slf4j.Logger;
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.test.mockito.MockitoComponentMockingRule;
+
+import javax.inject.Provider;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+public class ObstetricHistoryControllerTest {
+
+    private static final String PREFIX = "pregnancy_history__";
+
+    private static final String GRAVIDA = "gravida";
+
+    private static final String PARA = "para";
+
+    private static final String TERM = "term";
+
+    private static final String PRETERM = "preterm";
+
+    private static final String SAB = "sab";
+
+    private static final String TAB = "tab";
+
+    private static final String LIVE_BIRTHS = "births";
+
+    @Rule
+    MockitoComponentMockingRule<PatientDataController> mocker =
+            new MockitoComponentMockingRule<PatientDataController>(ObstetricHistoryController.class);
+
+    private DocumentAccessBridge documentAccessBridge;
+
+    private ObstetricHistoryController obstetricHistoryController;
+
+    private DocumentReference patientDocument;
+
+    @Mock
+    private Patient patient;
+
+    @Mock
+    private Logger logger;
+
+    @Mock
+    private XWikiDocument doc;
+
+    @Mock
+    private BaseObject data;
+
+    private Provider<XWikiContext> provider;
+
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        this.patientDocument = new DocumentReference("wiki", "patient", "00000001");
+        this.obstetricHistoryController =
+                (ObstetricHistoryController) this.mocker.getComponentUnderTest();
+        this.logger = this.mocker.getMockedLogger();
+        this.provider = this.mocker.getInstance(Provider.class);
+        this.documentAccessBridge = this.mocker.getInstance(DocumentAccessBridge.class);
+
+        doReturn(this.patientDocument).when(this.patient).getDocument();
+        doReturn(this.doc).when(this.documentAccessBridge).getDocument(this.patientDocument);
+        //doReturn(this.data).when(this.doc).getXObject(this.obstetricHistoryController.getXClassReference());
+
+    }
+
+    @Test
+    public void loadHandlesEmptyPatientTest(){
+
+        doReturn(null).when(this.doc).getXObject(this.obstetricHistoryController.getXClassReference());
+
+        PatientData<Integer> testPatientData= this.obstetricHistoryController.load(this.patient);
+
+        verify(this.logger).debug("No data for patient [{}]", this.patientDocument);
+
+    }
+
+}
