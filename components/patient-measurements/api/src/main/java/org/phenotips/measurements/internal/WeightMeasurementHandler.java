09de3c339adb4eea09800102ddf55a09e8395f7a
danielpgross <daniel.gross.2@gmail.com>
2015-10-08 13:29:44 -0400

    PT-1951 Patient measurements - UI improvements PT-2019 finish smart phenotype terms

--- components/patient-measurements/api/src/main/java/org/phenotips/measurements/internal/WeightMeasurementHandler.java
+++ components/patient-measurements/api/src/main/java/org/phenotips/measurements/internal/WeightMeasurementHandler.java
@@ -49,12 +49,13 @@ public class WeightMeasurementHandler extends AbstractMeasurementHandler
     }
 
     @Override
-    public List<String> getAssociatedTerms(double standardDeviation)
+    public List<String> getAssociatedTerms(Double standardDeviation)
     {
         List<String> terms = new LinkedList<>();
-        if (standardDeviation >= 2.0) {
+        if (standardDeviation == null || standardDeviation >= 2.0) {
             terms.add("HP:0004324");
-        } else if (standardDeviation <= -2.0) {
+        }
+        if (standardDeviation == null || standardDeviation <= -2.0) {
             terms.add("HP:0004325");
         }
 
