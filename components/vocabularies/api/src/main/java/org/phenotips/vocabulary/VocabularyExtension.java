d331b8df81954a369b6fa1c9df8e04a314b42984
Sergiu Dumitriu <sergiu@phenotips.org>
2015-10-10 22:38:54 -0400

    PT-2105: Add a VocabularyExtension @Role Moved in the right package

--- components/vocabularies/api/src/main/java/org/phenotips/vocabulary/VocabularyExtension.java
+++ components/vocabularies/api/src/main/java/org/phenotips/vocabulary/VocabularyExtension.java
@@ -15,9 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/
  */
-package org.phenotips.vocabulary.internal.solr;
-
-import org.phenotips.vocabulary.VocabularyInputTerm;
+package org.phenotips.vocabulary;
 
 import org.xwiki.component.annotation.Role;
 import org.xwiki.stability.Unstable;
