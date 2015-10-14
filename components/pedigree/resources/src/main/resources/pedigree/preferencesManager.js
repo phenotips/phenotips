39c8a906ced9dba234326dd7b2baa0157a065f3c
Andrew Misyura <asm@phenotips.org>
2015-10-14 17:03:27 -0400

    PT-2111: Incorrect user name appears on printed pedigree: fixed PT-2043: Changing the pedigree configuration doesn't take effect until refreshing the browser cache: fixed

--- components/pedigree/resources/src/main/resources/pedigree/preferencesManager.js
+++ components/pedigree/resources/src/main/resources/pedigree/preferencesManager.js
@@ -1,44 +1,29 @@
 define(["pedigree/model/helpers"], function(Helpers){
     var PreferencesManager = Class.create( {
-        initialize: function( defaultPreferences ) {
-            this.preferences = defaultPreferences;
+        initialize: function( templatePreferences ) {
+            this.preferences = templatePreferences;
         },
 
         load: function(callWhenReady) {
-            var numConfigs = $xwiki.getDocument('XWiki.XWikiPreferences').getObjectNumbers('PhenoTips.PedigreeGlobalSettings');
-            if (numConfigs > 0) {
-                this.preferences.global.dateDisplayFormat = "$!{xwiki.getDocument('XWiki.XWikiPreferences').getObject('PhenoTips.PedigreeGlobalSettings').getProperty('dateDisplayFormat').value}";
-                this.preferences.global.dateEditFormat    = "$!{xwiki.getDocument('XWiki.XWikiPreferences').getObject('PhenoTips.PedigreeGlobalSettings').getProperty('dateInputFormat').value}";
-                this.preferences.global.nonStandardAdoptedOutGraphic = ("$!{xwiki.getDocument('XWiki.XWikiPreferences').getObject('PhenoTips.PedigreeGlobalSettings').getProperty('nonStandardAdoptedOutGraphic').value}" == "1");
-                this.preferences.global.propagateFatherLastName      = ("$!{xwiki.getDocument('XWiki.XWikiPreferences').getObject('PhenoTips.PedigreeGlobalSettings').getProperty('propagateFatherLastName').value}" == "1");
-                this.preferences.global.useGradientOnNodes           = ("$!{xwiki.getDocument('XWiki.XWikiPreferences').getObject('PhenoTips.PedigreeGlobalSettings').getProperty('uiGradientNodes').value}" == "1");
-            }
-
-            /* The current way of loading properties is sub-optimal, in the future may have to introduce
-             * a velocity service and AJAX it.
-             *
-            var preferencesJsonURL = new XWiki.Document('...', 'PhenoTips').getURL('get', '');
-            // IE caches AJAX requests, use a random URL to break that cache (TODO: investigate)
+        // TODO: get URL from endpoints.js after merger with family-studies
+        var preferencesJsonURL = new XWiki.Document('PedigreeInterface', 'PhenoTips').getURL('get', 'action=getPreferences');
             preferencesJsonURL += "&rand=" + Math.random();
+
             new Ajax.Request(preferencesJsonURL, {
                 method: "GET",
                 onSuccess: this.onPreferencesAvailable.bind(this),
                 onComplete: callWhenReady ? callWhenReady : {}
             });
-            */
-            this.onPreferencesAvailable();
-            callWhenReady();
         },
 
         onPreferencesAvailable : function(response) {
-            if (!this.preferences) {
-                this.preferences = {};
+        if (response.responseJSON) {
+            // only set preferences which are given in the template
+            setByTemplate(this.preferences, response.responseJSON);
+            console.log("Loaded preferences: " + stringifyObject(response.responseJSON));
+        } else {
+            console.log("Failed to loaded properties, no JSNO");
             }
-            if (!this.preferences.hasOwnProperty("global")) {
-                this.preferences.global = {};
-            }
-            //TODO: use response.responseJSON when using AJAX
-            console.log("Loaded global preferences: " + Helpers.stringifyObject(this.preferences.global));
         },
 
         /**
