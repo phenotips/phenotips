c21b79c4c87d0bdeb0fce9e3d8dc99e8804a1d38
Tarang <tarang.marathe@gmail.com>
2015-11-02 18:04:05 -0500

    load tests

--- components/patient-data/impl/src/test/java/org/phenotips/data/internal/controller/ParentalAgeControllerTest.java
+++ components/patient-data/impl/src/test/java/org/phenotips/data/internal/controller/ParentalAgeControllerTest.java
@@ -3,6 +3,7 @@ package org.phenotips.data.internal.controller;
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 import org.junit.Assert;
 import org.mockito.Mock;
 import org.junit.Before;
@@ -22,12 +23,18 @@ import javax.inject.Provider;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class ParentalAgeControllerTest {
 
     private static final String MATERNAL_AGE = "maternal_age";
 
     private static final String PATERNAL_AGE = "paternal_age";
+
+    private static final Integer AGE_NON_ZERO = 25; // Arbitrary age.
+
+    private static final Integer AGE_ZERO = 0;
 
     @Rule
     public MockitoComponentMockingRule<PatientDataController<Integer>> mocker =
@@ -65,7 +72,7 @@ public class ParentalAgeControllerTest {
         this.provider = this.mocker.getInstance(XWikiContext.TYPE_PROVIDER);
         this.xWikiContext = this.provider.get();
         doReturn(this.xwiki).when(this.xWikiContext).getWiki();
-
+        this.documentAccessBridge = this.mocker.getInstance(DocumentAccessBridge.class);
         doReturn(this.patientDocument).when(this.patient).getDocument();
         doReturn(this.doc).when(this.documentAccessBridge).getDocument(this.patientDocument);
 
@@ -76,13 +83,34 @@ public class ParentalAgeControllerTest {
         doReturn(null).when(this.doc).getXObject(any(EntityReference.class));
         PatientData<Integer> testData = this.parentalAgeController.load(this.patient);
         Assert.assertNull(testData);
+        verify(this.logger).debug("No parental information for patient [{}]", this.patientDocument);
     }
 
     @Test
-    public void loadDefaultBehaviourTest(){
+    public void loadMaternalAndPaternalAgeNonZero(){
+        BaseObject data = mock(BaseObject.class);
+        doReturn(data).when(this.doc).getXObject(any(EntityReference.class));
+
+        doReturn(AGE_NON_ZERO).when(data).getIntValue(MATERNAL_AGE);
+        doReturn(AGE_NON_ZERO).when(data).getIntValue(PATERNAL_AGE);
 
         PatientData<Integer> testData = this.parentalAgeController.load(this.patient);
 
+        Assert.assertEquals("parentalAge", testData.getName());
+        Assert.assertTrue(testData.get(MATERNAL_AGE) == 25);
+        Assert.assertTrue(testData.get(PATERNAL_AGE) == 25);
+
+    }
+
+    @Test
+    public void loadMaternalAndPaternalAgeZero(){
+        BaseObject data = mock(BaseObject.class);
+
+        doReturn(AGE_ZERO).when(data).getIntValue(MATERNAL_AGE);
+        doReturn(AGE_ZERO).when(data).getIntValue(PATERNAL_AGE);
+
+        PatientData<Integer> testData = this.parentalAgeController.load(this.patient);
+        Assert.assertNull(testData);
 
     }
 
