0235fbe2fb29bbea71ea822949abe61822982553
Andrew Misyura <asm@phenotips.org>
2015-11-02 19:36:13 -0500

    PT-2213: Father's last name in a childless relationship in pedigree editor gets propagated to a non-existent placeholder child Fixed
    (cherry picked from commit 912fa1b5fb32bdc4775714097bd21e8d09b38ceb)

--- components/pedigree/resources/src/main/resources/pedigree/view/personPlaceholderVisuals.js
+++ components/pedigree/resources/src/main/resources/pedigree/view/personPlaceholderVisuals.js
@@ -33,5 +33,16 @@ var PersonPlaceholderVisuals = Class.create(PersonVisuals, {
         var shape = editor.getPaper().rect(x - radius, y - radius, radius * 2, radius * 2).hide();
         this._genderShape = shape;
         this._genderGraphics = editor.getPaper().set(shape);
+    },
+
+    /**
+     * To make sure some runaway labal does not appear for a placeholder
+     * (E.g. when father's name is propagated)
+     */
+    drawLabels: function() {
+        var labels = this.getLabels();
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].hide();
+        }
     }
 });
