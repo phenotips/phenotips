5357e222c7080ff783b24b48e8096dc0d5ff0988
ItaiGershtansky <itaig.phenotips@gmail.com>
2015-11-04 17:21:02 -0500

    issue-1987-projects Now that DefaultGroup implements equals() and hashCode() it's better to make use of them.

--- components/users/default-impl/src/main/java/org/phenotips/groups/internal/DefaultGroupManager.java
+++ components/users/default-impl/src/main/java/org/phenotips/groups/internal/DefaultGroupManager.java
@@ -150,17 +150,6 @@ public class DefaultGroupManager implements GroupManager
     @Override
     public boolean isUserInGroup(User user, Group group)
     {
-        Set<Group> userGroups = this.getGroupsForUser(user);
-
-        // note that Group and default Group implementation do not overwrite equals(), thus
-        // using Set.contains() with groupManager.getGroup(groupName) is not possible
-
-        String groupName = group.getReference().getName();
-        for (Group userGroup : userGroups) {
-            if (userGroup.getReference().getName().equals(groupName)) {
-                return true;
-            }
-        }
-        return false;
+        return this.getGroupsForUser(user).contains(group);
     }
 }
