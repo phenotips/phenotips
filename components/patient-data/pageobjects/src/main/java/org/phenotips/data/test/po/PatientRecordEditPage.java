1a9df4ddcb11772faead35c9c7c44373b3e17c4b
Tarang <tarang.marathe@gmail.com>
2015-11-11 18:36:14 -0500

    webelements for maternal and paternal age

--- components/patient-data/pageobjects/src/main/java/org/phenotips/data/test/po/PatientRecordEditPage.java
+++ components/patient-data/pageobjects/src/main/java/org/phenotips/data/test/po/PatientRecordEditPage.java
@@ -27,7 +27,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
 /**
- * Represents the actions possible on a patient record in view mode.
+ * Represents the actions possible on a patient record in edit mode.
  *
  * @version $Id$
  * @since 1.0RC1
@@ -45,6 +45,12 @@ public class PatientRecordEditPage extends InlinePage
 
     @FindBy(id = "PhenoTips.PatientClass_0_paternal_ethnicity_2")
     private WebElement paternalEthnicity;
+
+    @FindBy(id = "PhenoTips.ParentalInformationClass_0_maternal_age")
+    private WebElement maternalAgeAtEDD;
+
+    @FindBy(id = "PhenoTips.ParentalInformationClass_0_paternal_age")
+    private WebElement paternalAgeAtEDD;
 
     @FindBy(id = "HPrenatalandperinatalhistory")
     private WebElement prenatalAndPerinatalHistorySectionHeading;
@@ -781,6 +787,24 @@ public class PatientRecordEditPage extends InlinePage
     }
 
     /**
+     * Sets the mother's age at the estimated date of delivery.
+     * @param years
+     */
+    public void setMaternalAgeAtEDD(String years){
+        this.maternalAgeAtEDD.clear();
+        this.maternalAgeAtEDD.sendKeys(years);
+    }
+
+    /**
+     * Sets the father's age at the estimated date of delivery.
+     * @param years
+     */
+    public void setPaternalAgeAtEDD(String years){
+        this.paternalAgeAtEDD.clear();
+        this.paternalAgeAtEDD.sendKeys(years);
+    }
+
+    /**
      * Sets the yes and no values for assisted reproduction boxes
      */
     public void setAssistedReproduction()
@@ -793,7 +817,7 @@ public class PatientRecordEditPage extends InlinePage
      * Sets APGAR scores from the one and five minute options
      *
      * @param oneMinute a string that this one of the numbers "1" to "10" or "Unknown" for the one minute APGAR
-     * @param fiveMinute a string that this one of the numbers "1" to "10" or "Unknown" for the five minute APGAR
+     * @param fiveMinutes a string that this one of the numbers "1" to "10" or "Unknown" for the five minute APGAR
      */
     public void setAPGARScores(String oneMinute, String fiveMinutes)
     {
