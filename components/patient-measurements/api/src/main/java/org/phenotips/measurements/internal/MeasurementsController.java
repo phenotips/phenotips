489744086cca7708479b1478a39c897e4e5dc527
Anton Kats <antonkats@gmail.com>
2015-12-17 13:51:28 -0500

    PT-1968: Backend: Controllers for exporting measurements in the patient JSON.

--- components/patient-measurements/api/src/main/java/org/phenotips/measurements/internal/MeasurementsController.java
+++ components/patient-measurements/api/src/main/java/org/phenotips/measurements/internal/MeasurementsController.java
@@ -1,3 +1,20 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
 package org.phenotips.measurements.internal;
 
 import org.phenotips.components.ComponentManagerRegistry;
@@ -19,9 +36,11 @@ import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -37,33 +56,43 @@ import com.xpn.xwiki.objects.BaseObject;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
+/**
+ * Handles the patient's measurements.
+ *
+ * @version $Id$
+ * @since 1.0M10
+ */
 @Component(roles = { PatientDataController.class })
 @Named("measurements")
 @Singleton
 public class MeasurementsController implements PatientDataController<MeasurementsController.MeasurementEntry>
 {
-    private final static String xClass = "PhenoTips.MeasurementClass";
+    private static final String XCLASS = "PhenoTips.MeasurementClass";
 
-    private final static String DATE = "date";
+    private static final String DATE = "date";
 
-    private final static String AGE = "age";
+    private static final String AGE = "age";
 
-    private final static String TYPE = "type";
+    private static final String TYPE = "type";
 
-    private final static String SIDE = "side";
+    private static final String SIDE = "side";
 
-    private final static String VALUE = "value";
+    private static final String VALUE = "value";
 
-    private final static String UNIT = "unit";
+    private static final String UNIT = "unit";
 
-    private final static String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String SD = "sd";
+
+    private static final String PERCENTILE = "precentile";
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
 
     @Inject
     private Logger logger;
 
     /** Provides access to the underlying data storage. */
     @Inject
-    protected DocumentAccessBridge documentAccessBridge;
+    private DocumentAccessBridge documentAccessBridge;
 
     /** Parses string representations of document references into proper references. */
     @Inject
@@ -108,47 +137,13 @@ public class MeasurementsController implements PatientDataController<Measurement
                     MeasurementHandler handler = getHandler(type, handlers);
                     final String units = handler.getUnit();
 
-                    MeasurementEntry entry = new MeasurementEntry()
-                    {
-                        @Override public Date getDate()
-                        {
-                            return date;
-                        }
-
-                        @Override public String getAge()
-                        {
-                            return age;
-                        }
-
-                        @Override public String getType()
-                        {
-                            return type;
-                        }
-
-                        @Override public String getSide()
-                        {
-                            return side;
-                        }
-
-                        @Override public Double getValue()
-                        {
-                            return value;
-                        }
-
-                        @Override public String getUnits()
-                        {
-                            return units;
-                        }
-                    };
+                    MeasurementEntry entry = new MeasurementEntry(date, age, type, side, value, units);
                     result.add(entry);
                 } catch (Exception e) {
                     this.logger.error("Failed to load a particular measurement", e.getMessage());
                 }
             }
 
-            if (result.isEmpty()) {
-                return null;
-            }
             return new IndexedPatientData<>(getName(), result);
         } catch (Exception e) {
             this.logger.error("Could not find requested document or some unforeseen"
@@ -159,7 +154,7 @@ public class MeasurementsController implements PatientDataController<Measurement
 
     private EntityReference getXClassReference()
     {
-        return stringResolver.resolve(xClass);
+        return stringResolver.resolve(XCLASS);
     }
 
     private List<MeasurementHandler> getHandlers()
@@ -229,10 +224,12 @@ public class MeasurementsController implements PatientDataController<Measurement
 
             JSONArray result = new JSONArray();
             Iterator<MeasurementEntry> iterator = data.iterator();
+            List<MeasurementHandler> handlers = getHandlers();
             while (iterator.hasNext()) {
                 MeasurementEntry entry = iterator.next();
-                result.add(entryToJson(entry));
-                // todo. add additional info
+                JSONObject jsonEntry = entryToJson(entry);
+                jsonEntry.putAll(computeDependantInfo(entry, patient, handlers));
+                result.add(jsonEntry);
             }
 
             if (!result.isEmpty()) {
@@ -241,21 +238,58 @@ public class MeasurementsController implements PatientDataController<Measurement
         }
     }
 
+    private Map<String, Object> computeDependantInfo(MeasurementEntry entry,
+        Patient patient, List<MeasurementHandler> handlers)
+    {
+        // not adding these constants into the class definition yet
+        String maleConst = "M";
+        String femaleConst = "F";
+        // an age string and type must be present
+        if (StringUtils.isBlank(entry.getAge()) || StringUtils.isBlank(entry.getType()) || entry.getValue() == null) {
+            return new HashMap<>();
+        }
+        Double ageInMonths = MeasurementUtils.convertAgeStrToNumMonths(entry.getAge());
+
+        // getting gender for computations
+        PatientData<String> sexData = patient.getData("sex");
+        if (sexData != null) {
+            String sex = sexData.getValue();
+            if (StringUtils.isNotBlank(sex)) {
+                // ideally, the constants from the SexController should be used here,
+                // but that would introduce a hard dependency. Maybe add it to constants?
+                if (StringUtils.equals(sex, maleConst) || StringUtils.equals(sex, femaleConst)) {
+                    // has to be either male or female, otherwise how can we compute SD?
+                    // finding the handler
+                    MeasurementHandler handler = getHandler(entry.getType(), handlers);
+                    Boolean isMale = StringUtils.equals(maleConst, sex);
+                    Double sd = handler.valueToStandardDeviation(isMale, ageInMonths.floatValue(), entry.getValue());
+                    Integer percentile = handler.valueToPercentile(isMale, ageInMonths.floatValue(), entry.getValue());
+
+                    Map<String, Object> result = new HashMap<>();
+                    result.put(SD, sd);
+                    result.put(PERCENTILE, percentile);
+                    return result;
+                }
+            }
+        }
+        return new HashMap<>();
+    }
+
     private JSONObject entryToJson(MeasurementEntry entry)
     {
         JSONObject json = new JSONObject();
         if (entry.getDate() != null) {
             // not efficient to create a new one for every entry
             SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
-            json.put("date", formatter.format(entry.getDate()));
+            json.put(DATE, formatter.format(entry.getDate()));
         }
-        json.put("age", entry.getAge());
-        json.put("type", entry.getType());
+        json.put(AGE, entry.getAge());
+        json.put(TYPE, entry.getType());
         if (StringUtils.isNotBlank(entry.getSide())) {
-            json.put("side", entry.getSide());
+            json.put(SIDE, entry.getSide());
         }
-        json.put("value", entry.getValue());
-        json.put("unit", entry.getUnits());
+        json.put(VALUE, entry.getValue());
+        json.put(UNIT, entry.getUnits());
         return json;
     }
 
@@ -300,39 +334,8 @@ public class MeasurementsController implements PatientDataController<Measurement
         final Date date =
             j.getString(DATE) != null ? formatter.parse(j.getString(DATE)) : null;
 
-        return new MeasurementEntry()
-        {
-            @Override public Date getDate()
-            {
-                return date;
-            }
-
-            @Override public String getAge()
-            {
-                return j.getString(AGE);
-            }
-
-            @Override public String getType()
-            {
-                return j.getString(TYPE);
-            }
-
-            @Override public String getSide()
-            {
-                return j.getString(SIDE);
-            }
-
-            @Override public Double getValue()
-            {
-                return j.getDouble(VALUE);
-            }
-
-            @Override public String getUnits()
-            {
-                // although this does not get saved, and currently affects nothing
-                return j.getString(UNIT);
-            }
-        };
+        return new MeasurementEntry(
+            date, j.getString(AGE), j.getString(TYPE), j.getString(SIDE), j.getDouble(VALUE), j.getString(UNIT));
     }
 
     @Override
@@ -341,18 +344,73 @@ public class MeasurementsController implements PatientDataController<Measurement
         return "measurements";
     }
 
-    abstract public class MeasurementEntry
+    /** A class that represents a measurement entry with easy access. */
+    public class MeasurementEntry
     {
-        abstract public Date getDate();
+        private Date date;
 
-        abstract public String getAge();
+        private String age;
 
-        abstract public String getType();
+        private String type;
 
-        abstract public String getSide();
+        private String side;
 
-        abstract public Double getValue();
+        private Double value;
 
-        abstract public String getUnits();
+        private String units;
+
+        /** The default constructor that takes all of the data stored in the patient record.
+         * @param date see the doc for the respective method
+         * @param age see the doc for the respective method
+         * @param type see the doc for the respective method
+         * @param side see the doc for the respective method
+         * @param value see the doc for the respective method
+         * @param units see the doc for the respective method
+         */
+        public MeasurementEntry(Date date, String age, String type, String side, Double value, String units)
+        {
+            this.date = date;
+            this.age = age;
+            this.type = type;
+            this.side = side;
+            this.value = value;
+            this.units = units;
+        }
+
+        /** @return the date of the measurement */
+        public Date getDate()
+        {
+            return this.date;
+        }
+
+        /** @return string representing age */
+        public String getAge()
+        {
+            return this.age;
+        }
+
+        /** @return the name of the measurement handler */
+        public String getType()
+        {
+            return this.type;
+        }
+
+        /** @return a letter representing the side, if applicable */
+        public String getSide()
+        {
+            return this.side;
+        }
+
+        /** @return the measurement itself */
+        public Double getValue()
+        {
+            return this.value;
+        }
+
+        /** @return the units that the value is measured in */
+        public String getUnits()
+        {
+            return this.units;
+        }
     }
 }
