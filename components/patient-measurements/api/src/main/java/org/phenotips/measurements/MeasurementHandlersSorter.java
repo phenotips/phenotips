56527b46b208d9085f28c74c2aafca938c2f4e42
danielpgross <daniel.gross.2@gmail.com>
2016-01-11 16:48:34 -0500

    PT-1951 Patient measurements - UI improvements sort saved measurements according to age

---
+++ components/patient-measurements/api/src/main/java/org/phenotips/measurements/MeasurementHandlersSorter.java
@@ -0,0 +1,45 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+package org.phenotips.measurements;
+
+import org.xwiki.component.annotation.Role;
+
+import java.util.Comparator;
+
+/**
+ * Sorting utilities for measurement handlers using a configured ordering.
+ *
+ * @version $Id$
+ * @since 1.3M4
+ */
+@Role
+public interface MeasurementHandlersSorter
+{
+    /**
+     * @return a comparator instance for measurement handler objects. The comparator will sort based on the configured
+     *         measurement ordering.
+     */
+    Comparator<MeasurementHandler> getMeasurementHandlerComparator();
+
+    /**
+     * @return a comparator instance for measurement names. The comparator will sort based on the configured measurement
+     *         ordering.
+     */
+    Comparator<String> getMeasurementNameComparator();
+}
