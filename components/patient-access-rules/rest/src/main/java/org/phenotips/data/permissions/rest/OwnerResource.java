151d5f9afb66f70b1e07463e4297e3ba63b6f620
Anton Kats <antonkats@gmail.com>
2016-01-26 18:57:22 -0500

    PT-2349: RESTful API for accessing/managing the owner

---
+++ components/patient-access-rules/rest/src/main/java/org/phenotips/data/permissions/rest/OwnerResource.java
@@ -0,0 +1,78 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.data.permissions.rest;
+
+import org.phenotips.data.rest.model.PhenotipsUser;
+
+import org.xwiki.component.annotation.Role;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resource for working with patient record's owners, identified by patient record's internal PhenoTips identifier.
+ *
+ * @version $Id$
+ * @since 1.3M1
+ */
+@Role
+@Path("/patients/{patient-id}/permissions/owner")
+public interface OwnerResource
+{
+    /**
+     * Retrieves the owner of a patient record - which is found by the passed in patient identifier - if the indicated
+     * patient record doesn't exist, or if the user sending the request doesn't have the right to view the target
+     * patient record, an error is returned.
+     *
+     * @param patientId internal identifier of a patient record
+     * @return REST representation of an owner of a patient record
+     */
+    @GET PhenotipsUser getOwner(@PathParam("patient-id") String patientId);
+
+    /**
+     * Updates the owner of a patient record - identified by `patientId` - with the owner specified in JSON. If the
+     * indicated patient record doesn't exist, or if the user sending the request doesn't have the right to edit the
+     * target patient record, no change is performed and an error is returned.
+     *
+     * @param json that contains a "id" property, with a value of either a fully qualified username or a plain username
+     * (eg. "xwiki:XWiki.username" or "username")
+     * @param patientId identifier of the patient, whose owner should be changed
+     * @return a status message
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON) Response putOwnerWithJson(String json,
+        @PathParam("patient-id") String patientId);
+
+    /**
+     * Updates the owner of a patient record - identified by `patientId` - with the owner specified in JSON. If the
+     * indicated patient record doesn't exist, or if the user sending the request doesn't have the right to edit the
+     * target patient record, no change is performed and an error is returned. The request must contain an "owner"
+     * field.
+     *
+     * @param patientId identifier of the patient, whose owner should be changed
+     * @return a status message
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED) Response putOwnerWithForm(
+        @PathParam("patient-id") String patientId);
+}
---
+++ components/patient-access-rules/rest/src/main/java/org/phenotips/data/permissions/rest/internal/DefaultOwnerResourceImpl.java
@@ -0,0 +1,158 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.data.permissions.rest.internal;
+
+import org.phenotips.data.permissions.PatientAccess;
+import org.phenotips.data.permissions.PermissionsManager;
+import org.phenotips.data.permissions.rest.DomainObjectFactory;
+import org.phenotips.data.permissions.rest.OwnerResource;
+import org.phenotips.data.permissions.rest.PermissionsResource;
+import org.phenotips.data.permissions.rest.Relations;
+import org.phenotips.data.permissions.rest.internal.utils.PatientUserContext;
+import org.phenotips.data.permissions.rest.internal.utils.SecureContextFactory;
+import org.phenotips.data.permissions.script.SecurePatientAccess;
+import org.phenotips.data.rest.PatientResource;
+import org.phenotips.data.rest.model.Link;
+import org.phenotips.data.rest.model.PhenotipsUser;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.container.Container;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceResolver;
+import org.xwiki.rest.XWikiResource;
+import org.xwiki.security.authorization.Right;
+import org.xwiki.text.StringUtils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Default implementation for {@link OwnerResource} using XWiki's support for REST resources.
+ *
+ * @version $Id$
+ * @since 1.3M1
+ */
+@Component
+@Named("org.phenotips.data.permissions.rest.internal.DefaultOwnerResourceImpl")
+@Singleton
+public class DefaultOwnerResourceImpl extends XWikiResource implements OwnerResource
+{
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private SecureContextFactory secureContextFactory;
+
+    /** Fills in missing reference fields with those from the current context document to create a full reference. */
+    @Inject
+    @Named("current")
+    private EntityReferenceResolver<String> currentResolver;
+
+    @Inject
+    private DomainObjectFactory factory;
+
+    @Inject
+    private PermissionsManager manager;
+
+    /** Needed for retrieving the `owner` parameter during the PUT request (as part of setting a new owner). */
+    @Inject
+    private Container container;
+
+    @Override
+    public PhenotipsUser getOwner(String patientId)
+    {
+        this.logger.debug("Retrieving patient record's owner [{}] via REST", patientId);
+        // besides getting the patient, checks that the user has view access
+        PatientUserContext patientUserContext = this.secureContextFactory.getContext(patientId, Right.VIEW);
+
+        PhenotipsUser result = this.factory.createPatientOwner(patientUserContext.getPatient());
+
+        // adding links relative to this context
+        result.getLinks().add(new Link().withRel(Relations.SELF).withHref(this.uriInfo.getRequestUri().toString()));
+        result.getLinks().add(new Link().withRel(Relations.PATIENT_RECORD)
+            .withHref(this.uriInfo.getBaseUriBuilder().path(PatientResource.class).build(patientId).toString()));
+        result.getLinks().add(new Link().withRel(Relations.PERMISSIONS)
+            .withHref(this.uriInfo.getBaseUriBuilder().path(PermissionsResource.class).build(patientId).toString()));
+
+        return result;
+    }
+
+    @Override public Response putOwnerWithJson(String json, String patientId)
+    {
+        try {
+            String id = JSONObject.fromObject(json).getString("id");
+            return putOwner(id, patientId);
+        } catch (Exception ex) {
+            this.logger.error("The json was not properly formatted", ex.getMessage());
+            throw new WebApplicationException(Status.BAD_REQUEST);
+        }
+    }
+
+    @Override public Response putOwnerWithForm(String patientId)
+    {
+        Object ownerIdInRequest = container.getRequest().getProperty("owner");
+        if (ownerIdInRequest instanceof String) {
+            String ownerId = ownerIdInRequest.toString();
+            if (StringUtils.isNotBlank(ownerId)) {
+                return putOwner(ownerId, patientId);
+            }
+        }
+        this.logger.error("The owner id was not provided or is invalid");
+        throw new WebApplicationException(Status.BAD_REQUEST);
+    }
+
+    private Response putOwner(String ownerId, String patientId)
+    {
+        if (StringUtils.isBlank(ownerId)) {
+            this.logger.error("The owner id was not provided");
+            throw new WebApplicationException(Status.BAD_REQUEST);
+        }
+        this.logger.debug("Setting owner of the patient record [{}] to [{}] via REST", patientId, ownerId);
+        // besides getting the patient, checks that the current user has edit access
+        PatientUserContext patientUserContext = this.secureContextFactory.getContext(patientId, Right.EDIT);
+
+        EntityReference ownerReference =
+            this.currentResolver.resolve(ownerId, EntityType.DOCUMENT, new EntityReference("XWiki", EntityType.SPACE));
+        // todo. ask Sergiu as to what the right thing to do is
+        // the code in DefaultPatientAccessHelper needs to be changed
+        // this is just a hack
+        // the helper needs to use this.entitySerializer.serialize
+        DocumentReference ownerDocRef = new DocumentReference(ownerReference);
+
+        PatientAccess patientAccess =
+            new SecurePatientAccess(this.manager.getPatientAccess(patientUserContext.getPatient()), this.manager);
+        // fixme. there should be a check for current user being the owner
+        // existence and validity of the passed in owner should be checked by .setOwner
+        if (!patientAccess.setOwner(ownerDocRef)) {
+            // todo. should this status be an internal server error, or a bad request?
+            throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+        }
+
+        return Response.noContent().build();
+    }
+}
