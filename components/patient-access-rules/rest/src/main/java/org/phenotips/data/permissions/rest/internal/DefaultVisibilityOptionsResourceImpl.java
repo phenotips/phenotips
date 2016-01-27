76c15adcf52c23334fe2955dc4b9452bd8442a5d
Anton Kats <antonkats@gmail.com>
2016-01-26 19:07:27 -0500

    PT-2357: RESTful API for listing all visibility options

---
+++ components/patient-access-rules/rest/src/main/java/org/phenotips/data/permissions/rest/internal/DefaultVisibilityOptionsResourceImpl.java
@@ -0,0 +1,59 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.data.permissions.rest.internal;
+
+import org.phenotips.data.permissions.PermissionsManager;
+import org.phenotips.data.permissions.Visibility;
+import org.phenotips.data.permissions.rest.Relations;
+import org.phenotips.data.permissions.rest.VisibilityOptionsResource;
+import org.phenotips.data.rest.model.Link;
+import org.phenotips.data.rest.model.VisibilityOptions;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rest.XWikiResource;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * Default implementation for {@link VisibilityOptionsResource} using XWiki's support for REST resources.
+ *
+ * @version $Id$
+ * @since 1.3M1
+ */
+@Component
+@Named("org.phenotips.data.permissions.rest.internal.DefaultVisibilityOptionsResourceImpl")
+@Singleton
+public class DefaultVisibilityOptionsResourceImpl extends XWikiResource implements VisibilityOptionsResource
+{
+    @Inject
+    private PermissionsManager manager;
+
+    @Override
+    public VisibilityOptions getVisibilityOptions()
+    {
+        VisibilityOptions result = new VisibilityOptions();
+        for (Visibility visibility : this.manager.listVisibilityOptions()) {
+            result.withLevels(visibility.getName());
+        }
+
+        result.getLinks().add(new Link().withRel(Relations.SELF).withHref(this.uriInfo.getRequestUri().toString()));
+        return result;
+    }
+}
