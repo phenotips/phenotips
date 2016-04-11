fb0ef29a311f5a8e3e59df49bf0e2889d8f52f03
ItaiGershtansky <itaig.phenotips@gmail.com>
2016-04-11 18:38:08 -0400

    PT-1987-projects Moved ProjectsController from patient-data-defualt-impl to projects-api project.

--- components/projects/api/src/main/java/org/phenotips/projects/internal/controller/ProjectsController.java
+++ components/projects/api/src/main/java/org/phenotips/projects/internal/controller/ProjectsController.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/
  */
-package org.phenotips.data.internal.controller;
+package org.phenotips.projects.internal.controller;
 
 import org.phenotips.data.IndexedPatientData;
 import org.phenotips.data.Patient;
