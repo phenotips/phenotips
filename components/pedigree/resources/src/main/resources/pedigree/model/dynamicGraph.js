2b34dff93f3762e4db3d0269bf58bb5349ca1106
veronikaslc <veronika.koltunov@gmail.com>
2016-04-13 18:14:53 -0400

    PT-2434: The consanguinity status of a patient should be set when identifiable from the pedigree.

--- components/pedigree/resources/src/main/resources/pedigree/model/dynamicGraph.js
+++ components/pedigree/resources/src/main/resources/pedigree/model/dynamicGraph.js
@@ -256,6 +256,7 @@ define([
             // - disorders
             // - genes
             // - maternal_ethnicity + paternal_ethnicity (merged with own ethnicities entered in pedigree editor)
+            // - family_history: consanguinity
 
             if (patientObject.hasOwnProperty("patient_name")) {
                 if (patientObject.patient_name.hasOwnProperty("first_name")) {
@@ -370,6 +371,18 @@ define([
                         this.DG.GG.properties[id][prop] = patientObject.pedigreeProperties[prop];
                     }
                 }
+            }
+
+            if (patientObject.hasOwnProperty("family_history")) {
+				this.DG.GG.properties[id]["family_history"] = patientObject["family_history"];
+				var consang = "A";
+				if (patientObject.family_history.hasOwnProperty("consanguinity")) {
+					consang = (patientObject.family_history.consanguinity) ? "Y" : "N";
+				}
+				var parentRel = this.DG.GG.getProducingRelationship(id);
+				if (parentRel) {
+					this.DG.GG.properties[parentRel]["consangr"] = consang;
+				}
             }
 
             return genderOK;
