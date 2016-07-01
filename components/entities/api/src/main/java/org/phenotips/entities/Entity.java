0fa06d893cb6697458a1bdcbf62b35e117c23d0e
danielpgross <daniel.gross.2@gmail.com>
2016-07-01 15:16:45 -0400

    PT-2640: Add generic APIs for working with dominant entities and entity groups - complete first iteration API design - add binding class

---
+++ components/entities/api/src/main/java/org/phenotips/entities/Entity.java
@@ -0,0 +1,36 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.entities;
+
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.stability.Unstable;
+
+@Unstable
+public interface Entity
+{
+    /**
+     * @return a reference to the main XClass associated with this entity.
+     */
+    EntityReference getType();
+
+    /**
+     * @return the reference to this entity's document
+     */
+    DocumentReference getReference();
+}
