e4bb4c59c17362ae0ba3c0eb7e54a5a7afb2e96a
Sergiu Dumitriu <sergiu@phenotips.org>
2016-07-28 16:02:08 -0400

    PT-2640: Add generic APIs for working with primary entities and entity groups Add abstract base classes for implementing container-type groups

---
+++ components/entities/api/src/main/java/org/phenotips/entities/internal/AbstractContainerPrimaryEntityGroup.java
@@ -0,0 +1,156 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.entities.internal;
+
+import org.phenotips.Constants;
+import org.phenotips.entities.PrimaryEntity;
+import org.phenotips.entities.PrimaryEntityGroup;
+import org.phenotips.entities.PrimaryEntityManager;
+
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.query.Query;
+import org.xwiki.query.QueryException;
+import org.xwiki.stability.Unstable;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+/**
+ * Base class for implementing specific entity groups, where groups declare the members that they contain.
+ * <p>
+ * By default, this uses the document name as the identifier, the document title as the name, and either a
+ * {@code description} property in the main XObject, or the document content, as the description. If two objects use the
+ * same document for storage, they are assumed to be equal, and no actual data equality is checked.
+ * </p>
+ * <p>
+ * The group document declares its members by attaching XObjects of type {@link #GROUP_MEMBER_CLASS} pointing to each
+ * member document.
+ * </p>
+ * <p>
+ * In order to function properly, a {@link PrimaryEntityManager} component with the hint ({@code @Named}) set to the
+ * name of {@link #getMemberType() the XClass used for members} must exist. Shorter versions of the XClass name are also
+ * accepted. For example, for members of type {@code PhenoTips.PatientClass}, the following names are supported:
+ * <ol>
+ * <li>PhenoTips.PatientClass</li>
+ * <li>PatientClass</li>
+ * <li>Patient</li>
+ * </ol>
+ * </p>
+ *
+ * @param <E> the type of entities belonging to this group; if more than one type of entities can be part of the group,
+ *            then a generic {@code PrimaryEntity} should be used instead
+ * @version $Id$
+ * @since 1.3M2
+ */
+@Unstable("New class and interface added in 1.3")
+public abstract class AbstractContainerPrimaryEntityGroup<E extends PrimaryEntity>
+    extends AbstractPrimaryEntityGroup<E> implements PrimaryEntityGroup<E>
+{
+    /** The XClass used for storing membership information by default. */
+    public static final EntityReference GROUP_MEMBER_CLASS =
+        new EntityReference("GroupMemberClass", EntityType.DOCUMENT,
+            Constants.CODE_SPACE_REFERENCE);
+
+    protected AbstractContainerPrimaryEntityGroup(XWikiDocument document)
+    {
+        super(document);
+    }
+
+    @Override
+    public Collection<E> getMembersOfType(EntityReference type)
+    {
+        Collection<E> result = new LinkedList<>();
+        try {
+            StringBuilder hql = new StringBuilder();
+            hql.append("select distinct binding.name from BaseObject binding, StringProperty groupReference");
+            if (type != null) {
+                hql.append(", BaseObject entity");
+            }
+            hql.append(" where binding.className = :memberClass")
+                .append(" and groupReference.id.id = binding.id and groupReference.id.name = :referenceProperty")
+                .append(" and groupReference.value = :selfReference");
+            if (type != null) {
+                hql.append(" and entity.name = binding.name and entity.className = :entityType");
+            }
+
+            Query q = getQueryManager().createQuery(hql.toString(), Query.HQL);
+
+            q.bindValue("memberClass", getLocalSerializer().serialize(getMembershipClass()));
+            q.bindValue("referenceProperty", getMembershipProperty());
+            q.bindValue("selfReference", getFullSerializer().serialize(getDocument()));
+            q.bindValue("entityType", getLocalSerializer().serialize(type));
+            List<String> memberIds = q.execute();
+            for (String memberId : memberIds) {
+                result.add(this.membersManager.get(memberId));
+            }
+        } catch (QueryException ex) {
+            this.logger.warn("Failed to query members: {}", ex.getMessage());
+        }
+        return result;
+    }
+
+    @Override
+    public boolean addMember(E member)
+    {
+        try {
+            BaseObject obj = this.document.getXObject(getMembershipClass(), getMembershipProperty(),
+                getFullSerializer().serialize(member.getDocument()), false);
+            if (obj != null) {
+                return true;
+            }
+            obj = this.document.newXObject(getMembershipClass(), getXContext());
+            obj.setStringValue(getMembershipProperty(), getFullSerializer().serialize(getDocument()));
+            getXContext().getWiki().saveDocument(this.document, "Added member " + member.getDocument(), true,
+                getXContext());
+            return true;
+        } catch (Exception ex) {
+            this.logger.warn("Failed to add member to group: {}", ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeMember(E member)
+    {
+        try {
+            BaseObject obj = this.document.getXObject(getMembershipClass(), getMembershipProperty(),
+                getFullSerializer().serialize(member.getDocument()), false);
+            if (obj == null) {
+                return true;
+            }
+            this.document.removeXObject(obj);
+            getXContext().getWiki().saveDocument(this.document, "Removed member " + member.getDocument(), true,
+                getXContext());
+            return true;
+        } catch (Exception ex) {
+            this.logger.warn("Failed to remove member from group: {}", ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    protected EntityReference getMembershipClass()
+    {
+        return GROUP_MEMBER_CLASS;
+    }
+}
---
+++ components/entities/api/src/main/java/org/phenotips/entities/internal/AbstractContainerPrimaryEntityGroupManager.java
@@ -0,0 +1,98 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.entities.internal;
+
+import org.phenotips.entities.PrimaryEntity;
+import org.phenotips.entities.PrimaryEntityGroupManager;
+
+import org.xwiki.model.reference.EntityReferenceSerializer;
+import org.xwiki.query.Query;
+import org.xwiki.query.QueryException;
+import org.xwiki.stability.Unstable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Inject;
+
+/**
+ * Base class for implementing specific entity group managers, where the groups are of the
+ * {@link AbstractContainerPrimaryEntityGroup container type}. This can be used as an almost complete base
+ * implementation, since the only unimplemented method is {@link #getDataSpace()}, however, several requirements are
+ * imposed on the subclasses to fully work:
+ * <ul>
+ * <li>the concrete implementation must have its {@code @Named} annotation set to reference the XClass used for the
+ * primary entity group, the one that's also returned by the {@link PrimaryEntity#getType()}</li>
+ * <li>the {@code <G>} parameter must be set to the concrete class being managed</li>
+ * <li>the class used for {@code <G>} must have a constructor that takes as an argument a
+ * {@link org.xwiki.bridge.DocumentModelBridge} or a {@link com.xpn.xwiki.doc.XWikiDocument} argument</li>
+ * <li>all documents {@link #create() created} by this manager will have the name in the format
+ * {@code <PREFIX><7 digit sequential number>}, unless {@link #getNextDocument()} is overridden, where:
+ * <ul>
+ * <li>the prefix is computed from the uppercase letters of the XClass name, excluding {@code Class}, e.g. for
+ * {@code PhenoTips.FamilyGroupClass} the prefix will be {@code FG}; override {@link #getIdPrefix()} to change this
+ * behavior</li>
+ * <li>the number is a 0-padded 7 digit number, starting at {@code 0000001} and automatically incremented for each new
+ * entity created</li>
+ * </ul>
+ * </li>
+ * </ul>
+ *
+ * @param <G> the type of groups handled by this manager
+ * @param <E> the type of entities belonging to the groups handled by this manager; if more than one type of entities
+ *            can be part of the groups, then a generic {@code PrimaryEntity} should be used instead
+ * @version $Id$
+ * @since 1.3M2
+ */
+@Unstable("New class and interface added in 1.3")
+public abstract class AbstractContainerPrimaryEntityGroupManager
+    <G extends AbstractContainerPrimaryEntityGroup<E>, E extends PrimaryEntity>
+    extends AbstractPrimaryEntityManager<G> implements PrimaryEntityGroupManager<G, E>
+{
+    @Inject
+    protected EntityReferenceSerializer<String> defaultSerializer;
+
+    @Override
+    public Collection<G> getGroupsForEntity(PrimaryEntity entity)
+    {
+        try {
+            // FIXME GROUP_MEMBER_CLASS should be replaced with a static method call
+            Query q = this.qm.createQuery(
+                "select gdoc.fullName from Document gdoc, gdoc.object("
+                    + this.localSerializer.serialize(AbstractContainerPrimaryEntityGroup.GROUP_MEMBER_CLASS)
+                    + ") as binding, gdoc.object("
+                    + this.localSerializer.serialize(getEntityXClassReference())
+                    + ") as grp where gdoc.space = :gspace and binding.reference = :name order by gdoc.fullName asc",
+                Query.XWQL);
+            q.bindValue("gspace", this.getDataSpace().getName());
+            q.bindValue("name", this.defaultSerializer.serialize(entity.getDocument()));
+            List<String> docNames = q.execute();
+            Collection<G> result = new ArrayList<>(docNames.size());
+            for (String docName : docNames) {
+                result.add(get(docName));
+            }
+            return result;
+        } catch (QueryException ex) {
+            this.logger.warn("Failed to query all entities of type [{}]: {}", getEntityXClassReference(),
+                ex.getMessage());
+        }
+        return Collections.emptyList();
+    }
+}
