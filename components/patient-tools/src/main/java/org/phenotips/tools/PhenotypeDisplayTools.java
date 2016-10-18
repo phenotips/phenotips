4f5af4a3a49af10f6c8da013c57f9b58e1657a18
veronikaslc <veronika.koltunov@gmail.com>
2016-10-18 12:51:15 -0400

    [misc] Dependency optimization

--- components/patient-tools/src/main/java/org/phenotips/tools/PhenotypeDisplayTools.java
+++ components/patient-tools/src/main/java/org/phenotips/tools/PhenotypeDisplayTools.java
@@ -34,7 +34,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 
 import com.xpn.xwiki.api.Document;
