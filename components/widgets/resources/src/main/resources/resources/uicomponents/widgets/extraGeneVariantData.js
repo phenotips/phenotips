163b859893b4738e2544a64a9902bae17c6621ea
a-gvo <alina.gvo@gmail.com>
2016-10-18 10:28:11 -0400

    PT-2785: A gene is stored as Ensembl ID if it exist, and as a gene symbol or non-standard ID otherwise.
    PT-2785: Requested fixes.
    
    PT-2785: Bug fix - broken gene deletion due to new hidden input field.
    
    PT-2785: Info button is now not displayed for ensembl IDs that are not in the database.

--- components/widgets/resources/src/main/resources/resources/uicomponents/widgets/extraGeneVariantData.js
+++ components/widgets/resources/src/main/resources/resources/uicomponents/widgets/extraGeneVariantData.js
@@ -91,9 +91,9 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
       var deleteTrigger = event.element();
       var className = deleteTrigger.up('td').className;
       var geneIndex = parseInt(className.substring(className.lastIndexOf('-') + 1), 10);
-      var geneSymbol = $$('[name="' + this.geneClassName + '_' + geneIndex + '_gene"]')[0].value;
+      var geneId = $$('[name="' + this.geneClassName + '_' + geneIndex + '_gene"]')[0].value;
 
-      new XWiki.widgets.ConfirmedAjaxRequest(deleteTrigger.href + geneSymbol, {
+      new XWiki.widgets.ConfirmedAjaxRequest(deleteTrigger.href + geneId, {
 
         onCreate : function() {
           deleteTrigger.disabled = true;
@@ -103,6 +103,8 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
           var dataRow = deleteTrigger.up('tr:not(.head-group)');
           var dataTable = deleteTrigger.up('table.gene-table.extradata-list');
           if (dataRow) {
+            var geneIdInput = dataRow.down('input.gene-id');
+            geneIdInput.remove();
             var geneNameInput = dataRow.down('input.gene-name');
             geneNameInput.__validation.destroy();
             $$('.variant-gene-' + geneIndex).each(function(item) {
@@ -216,7 +218,7 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
               $$('.variant-gene-' + geneIndex + '.variant-title-row').each(function(item) {
                 item.remove();
               });
-              $$('[name="' + geneClass + '_' + geneIndex + '_gene"]')[0].toggleClassName('v-collapsed', false);
+              $$('[name="' + geneClass + '_' + geneIndex + '_symbol"]')[0].toggleClassName('v-collapsed', false);
               $$('.gene.col-label.gene-' + geneIndex + '.gene-input-label')[0].toggleClassName('v-collapsed', true);
               dataRow.next().remove();
               dataRow.remove();
@@ -271,7 +273,7 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
       var geneSymbol = $$('.gene.col-label.gene-' + geneIndex)[0].innerHTML;
 
       //lock the genesymbol input field
-      $$('[name="' + this.geneClassName + '_' + geneIndex + '_gene"]')[0].toggleClassName('v-collapsed', true);
+      $$('[name="' + this.geneClassName + '_' + geneIndex + '_symbol"]')[0].toggleClassName('v-collapsed', true);
       $$('.gene.col-label.gene-' + geneIndex + '.gene-input-label')[0].toggleClassName('v-collapsed', false);
 
       var variantFooter = addTrigger.up('tr.variant-footer');
