e76e8f7de9b41044e97c9eab2ceae62c90152bf6
Sergiu Dumitriu <sergiu@phenotips.org>
2016-11-08 10:40:20 -0500

    [cleanup] Removed another unused import

--- components/family-studies/rest/src/main/java/org/phenotips/studies/family/rest/FamilyResource.java
+++ components/family-studies/rest/src/main/java/org/phenotips/studies/family/rest/FamilyResource.java
@@ -23,7 +23,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
