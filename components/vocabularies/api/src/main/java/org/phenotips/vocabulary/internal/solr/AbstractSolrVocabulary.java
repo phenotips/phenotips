phenotips/phenotips

commit e1df4c75657022646950cb4795f1aed901165180

[misc] Use SolrQuery instead of SolrParams internally, since the former is better suited for building queries

components/vocabularies/api/src/main/java/org/phenotips/vocabulary/internal/solr/AbstractSolrVocabulary.java
@@ -37,13 +37,11 @@ import java.util.Set;
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.util.ClientUtils;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.CommonParams;
-import org.apache.solr.common.params.ModifiableSolrParams;
-import org.apache.solr.common.params.SolrParams;
 import org.slf4j.Logger;
 
 /**
@@ -103,9 +101,8 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
         }
         VocabularyTerm result = this.externalServicesAccess.getTermCache().get(id);
         if (result == null) {
-            ModifiableSolrParams params = new ModifiableSolrParams();
-            params.set(CommonParams.Q, ID_FIELD_NAME + ':' + ClientUtils.escapeQueryChars(id));
-            SolrDocumentList allResults = this.search(params);
+            SolrQuery query = new SolrQuery(ID_FIELD_NAME + ':' + ClientUtils.escapeQueryChars(id));
+            SolrDocumentList allResults = this.search(query);
             if (allResults != null && !allResults.isEmpty()) {
                 result = new SolrVocabularyTerm(allResults.get(0), this);
                 this.externalServicesAccess.getTermCache().set(id, result);
@@ -136,7 +133,7 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
 
         // There's at least one more term not found in the cache
         if (query.length() > 5) {
-            for (SolrDocument doc : this.search(SolrQueryUtils.transformQueryToSolrParams(query.toString()))) {
+            for (SolrDocument doc : this.search(new SolrQuery(query.toString()))) {
                 VocabularyTerm term = new SolrVocabularyTerm(doc, this);
                 rawResult.put(term.getId(), term);
             }
@@ -159,8 +156,8 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
     public List<VocabularyTerm> search(Map<String, ?> fieldValues, Map<String, String> queryOptions)
     {
         List<VocabularyTerm> result = new LinkedList<>();
-        for (SolrDocument doc : this.search(
-            SolrQueryUtils.transformQueryToSolrParams(generateLuceneQuery(fieldValues)), queryOptions)) {
+        for (SolrDocument doc : this
+            .search(SolrQueryUtils.generateQuery(new SolrQuery(generateLuceneQuery(fieldValues)), queryOptions))) {
             result.add(new SolrVocabularyTerm(doc, this));
         }
         return result;
@@ -215,38 +212,22 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
      * Perform a search, falling back on the suggested spellchecked query if the original query fails to return any
      * results.
      *
-     * @param params the Solr parameters to use, should contain at least a value for the "q" parameter
+     * @param query the Solr query to use, should contain at least a value for the "q" parameter
      * @return the list of matching documents, empty if there are no matching terms
      */
-    protected SolrDocumentList search(SolrParams params)
-    {
-        return search(params, null);
-    }
-
-    /**
-     * Perform a search, falling back on the suggested spellchecked query if the original query fails to return any
-     * results.
-     *
-     * @param params the Solr parameters to use, should contain at least a value for the "q" parameter
-     * @param queryOptions extra options to include in the query; these override the default values, but don't override
-     *            values already set in the query
-     * @return the list of matching documents, empty if there are no matching terms
-     */
-    protected SolrDocumentList search(SolrParams params, Map<String, String> queryOptions)
+    protected SolrDocumentList search(SolrQuery query)
     {
         try {
-            SolrParams enhancedParams = SolrQueryUtils.enhanceParams(params, queryOptions);
-            this.logger.debug("Searching [{}] with query [{}]", getCoreName(), enhancedParams);
-            QueryResponse response = this.externalServicesAccess.getSolrConnection().query(enhancedParams);
+            this.logger.debug("Searching [{}] with query [{}]", getCoreName(), query);
+            QueryResponse response = this.externalServicesAccess.getSolrConnection().query(query);
             SolrDocumentList results = response.getResults();
             if (response.getSpellCheckResponse() != null && !response.getSpellCheckResponse().isCorrectlySpelled()
                 && StringUtils.isNotEmpty(response.getSpellCheckResponse().getCollatedResult())) {
-                enhancedParams =
-                    SolrQueryUtils.applySpellcheckSuggestion(enhancedParams, response.getSpellCheckResponse()
-                        .getCollatedResult());
-                this.logger.debug("Searching [{}] with spellchecked query [{}]", getCoreName(), enhancedParams);
+                SolrQueryUtils.applySpellcheckSuggestion(query,
+                    response.getSpellCheckResponse().getCollatedResult());
+                this.logger.debug("Searching [{}] with spellchecked query [{}]", getCoreName(), query);
                 SolrDocumentList spellcheckResults =
-                    this.externalServicesAccess.getSolrConnection().query(enhancedParams).getResults();
+                    this.externalServicesAccess.getSolrConnection().query(query).getResults();
                 if (results.getMaxScore() < spellcheckResults.getMaxScore()) {
                     results = spellcheckResults;
                 }
@@ -261,15 +242,14 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
     /**
      * Get the number of entries that match a specific Lucene query.
      *
-     * @param query a valid the Lucene query as string
+     * @param query a valid Lucene query as string
      * @return the number of entries matching the query
      */
     protected long count(String query)
     {
-        ModifiableSolrParams params = new ModifiableSolrParams();
-        params.set(CommonParams.Q, query);
-        params.set(CommonParams.START, "0");
-        params.set(CommonParams.ROWS, "0");
+        SolrQuery params = new SolrQuery(query);
+        params.setStart(0);
+        params.setRows(0);
         SolrDocumentList results;
         try {
             this.logger.debug("Counting terms matching [{}] in [{}]", query, getCoreName());
@@ -344,5 +324,4 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
         }
         return result;
     }
-
 }

components/vocabularies/api/pom.xml
@@ -30,7 +30,7 @@
 
   <properties>
     <!-- More tests should be added when the Solr core becomes a component -->
-    <coverage.instructionRatio>0.22</coverage.instructionRatio>
+    <coverage.instructionRatio>0.19</coverage.instructionRatio>
   </properties>
 
   <dependencies>

components/vocabularies/api/src/main/java/org/phenotips/vocabulary/internal/solr/SolrQueryUtils.java
@@ -17,16 +17,13 @@
  */
 package org.phenotips.vocabulary.internal.solr;
 
-import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.common.params.DisMaxParams;
-import org.apache.solr.common.params.ModifiableSolrParams;
-import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.params.SpellingParams;
 
 /**
@@ -49,33 +46,6 @@ public final class SolrQueryUtils
     }
 
     /**
-     * Convert a Lucene query string into Solr parameters. More specifically, places the input query under the "q"
-     * parameter.
-     *
-     * @param query the lucene query string to use
-     * @return the obtained parameters
-     */
-    public static SolrParams transformQueryToSolrParams(String query)
-    {
-        ModifiableSolrParams result = new ModifiableSolrParams();
-        result.add(CommonParams.Q, query);
-        return result;
-    }
-
-    /**
-     * Adds extra parameters to a Solr query for better term searches. More specifically, adds parameters for requesting
-     * the score to be included in the results, for requesting a spellcheck result, and sets the {@code start} and
-     * {@code rows} parameters when missing.
-     *
-     * @param originalParams the original Solr parameters to enhance
-     * @return the enhanced parameters
-     */
-    public static SolrParams enhanceParams(SolrParams originalParams)
-    {
-        return enhanceParams(originalParams, null);
-    }
-
-    /**
      * Adds extra parameters to a Solr query for better term searches, including custom options. More specifically, adds
      * parameters for requesting the score to be included in the results, for requesting a spellcheck result, and sets
      * the {@code start} and {@code rows} parameters when missing.
@@ -85,95 +55,82 @@ public final class SolrQueryUtils
      *            values already set in the query
      * @return the enhanced parameters
      */
-    public static SolrParams enhanceParams(SolrParams originalParams, Map<String, String> queryOptions)
+    public static SolrQuery generateQuery(SolrQuery originalParams, Map<String, String> queryOptions)
     {
         if (originalParams == null) {
             return null;
         }
-        ModifiableSolrParams newParams = new ModifiableSolrParams();
-        newParams.set(CommonParams.START, "0");
-        newParams.set(CommonParams.ROWS, "1000");
-        newParams.set(CommonParams.FL, "* score");
+        SolrQuery result = new SolrQuery();
+
+        // Default values
+        result.setStart(0);
+        result.setRows(1000);
+        result.setIncludeScore(true);
+
+        // Add the generic query options
         if (queryOptions != null) {
             for (Map.Entry<String, String> item : queryOptions.entrySet()) {
-                newParams.set(item.getKey(), item.getValue());
+                result.set(item.getKey(), item.getValue());
             }
         }
+
+        // Add the original query parameters
         for (Map.Entry<String, Object> item : originalParams.toNamedList()) {
             if (item.getValue() != null && item.getValue() instanceof String[]) {
-                newParams.set(item.getKey(), (String[]) item.getValue());
+                result.set(item.getKey(), (String[]) item.getValue());
             } else {
-                newParams.set(item.getKey(), String.valueOf(item.getValue()));
+                result.set(item.getKey(), String.valueOf(item.getValue()));
             }
         }
-        if (newParams.get(SPELLCHECK) == null) {
-            newParams.set(SPELLCHECK, Boolean.toString(true));
-            newParams.set(SpellingParams.SPELLCHECK_COLLATE, Boolean.toString(true));
+
+        if (result.get(SPELLCHECK) == null) {
+            result.set(SPELLCHECK, Boolean.toString(true));
+            result.set(SpellingParams.SPELLCHECK_COLLATE, Boolean.toString(true));
         }
-        return newParams;
+
+        return result;
     }
 
     /**
      * Replaces the original query in the Solr parameters with the suggested spellchecked query. It also fixes the boost
      * query, if any.
      *
-     * @param originalParams the original Solr parameters to fix
-     * @param suggestedQuery the suggested query
-     * @return new Solr parameters with the query and boost query fixed
+     * @param originalQuery the original Solr query to fix
+     * @param suggestedQuery the suggested query string
+     * @return new Solr query with the query and boost query fixed
      */
-    public static SolrParams applySpellcheckSuggestion(SolrParams originalParams, String suggestedQuery)
+    public static SolrQuery applySpellcheckSuggestion(SolrQuery originalQuery, String suggestedQuery)
     {
-        if (originalParams == null) {
+        if (originalQuery == null) {
             return null;
         }
         if (StringUtils.isBlank(suggestedQuery)) {
-            return originalParams;
+            return originalQuery;
         }
-        ModifiableSolrParams newParams = new ModifiableSolrParams(originalParams);
-        String newQuery = suggestedQuery;
+        String newQueryString = suggestedQuery;
 
         // Since the spelling suggestion might not be that good, also search for the original user input
-        if (StringUtils.isNotEmpty(originalParams.get(SpellingParams.SPELLCHECK_Q))) {
-            newQuery = originalParams.get(CommonParams.Q) + "^10 " + suggestedQuery;
+        if (StringUtils.isNotEmpty(originalQuery.get(SpellingParams.SPELLCHECK_Q))) {
+            newQueryString = "(" + originalQuery.getQuery() + ")^10 " + suggestedQuery;
         }
 
         // Check if the last term in the query is a word stub search which, in case the request comes from a
         // user-triggered search for terms from the UI, is a prefix search for the last typed word
-        Matcher originalStub = WORD_STUB.matcher(newParams.get(CommonParams.Q));
+        Matcher originalStub = WORD_STUB.matcher(originalQuery.getQuery());
         Matcher newStub = WORD_STUB.matcher(suggestedQuery);
         if (originalStub.find() && newStub.find() && !StringUtils.equals(originalStub.group(2), newStub.group(2))) {
             // Since word stubs aren't complete words, they may wrongly be "corrected" to a full word that doesn't match
             // what the user started typing; include both the original stub and the "corrected" stub in the query
-            newQuery += ' ' + originalStub.group() + "^1.5";
+            newQueryString += ' ' + originalStub.group() + "^1.5";
             // Also fix the boost query
-            String boostQuery = newParams.get(DisMaxParams.BQ);
+            String boostQuery = originalQuery.get(DisMaxParams.BQ);
             if (StringUtils.isNotEmpty(boostQuery)) {
-                newParams.add(DisMaxParams.BQ, boostQuery.replace(originalStub.group(2), newStub.group(2)));
+                originalQuery.add(DisMaxParams.BQ, boostQuery.replace(originalStub.group(2), newStub.group(2)));
             }
         }
         // Replace the query
-        newParams.set(CommonParams.Q, newQuery);
+        originalQuery.setQuery(newQueryString);
 
-        return newParams;
-    }
-
-    /**
-     * Serialize Solr parameters into a String.
-     *
-     * @param params the parameters to serialize
-     * @return a String serialization of the parameters
-     */
-    public static String getCacheKey(SolrParams params)
-    {
-        StringBuilder out = new StringBuilder();
-        out.append('{');
-        Iterator<String> parameterNames = params.getParameterNamesIterator();
-        while (parameterNames.hasNext()) {
-            String parameter = parameterNames.next();
-            out.append(parameter).append(":[").append(StringUtils.join(params.getParams(parameter), ", "))
-                .append("]\n");
-        }
-        out.append('}');
-        return out.toString();
+        return originalQuery;
     }
 }

components/vocabularies/api/src/test/java/org/phenotips/vocabulary/internal/solr/SolrQueryUtilsTest.java
@@ -17,10 +17,9 @@
  */
 package org.phenotips.vocabulary.internal.solr;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.DisMaxParams;
-import org.apache.solr.common.params.ModifiableSolrParams;
-import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.params.SpellingParams;
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,27 +31,14 @@ import org.junit.Test;
  */
 public class SolrQueryUtilsTest
 {
-    @Test
-    public void testTransformQueryToSolrParams()
-    {
-        SolrParams output = SolrQueryUtils.transformQueryToSolrParams("field:value");
-        Assert.assertEquals("field:value", output.get(CommonParams.Q));
-    }
-
-    @Test
-    public void testTransformQueryToSolrParamsWithNullValue()
-    {
-        SolrParams output = SolrQueryUtils.transformQueryToSolrParams(null);
-        Assert.assertNull(output.get(CommonParams.Q));
-    }
 
     @Test
     public void testEnhanceParamsDefaultValues()
     {
-        ModifiableSolrParams input = new ModifiableSolrParams();
-        SolrParams output = SolrQueryUtils.enhanceParams(input);
+        SolrQuery input = new SolrQuery();
+        SolrQuery output = SolrQueryUtils.generateQuery(input, null);
         Assert.assertNull(output.get(CommonParams.Q));
-        Assert.assertEquals("* score", output.get(CommonParams.FL));
+        Assert.assertEquals("*,score", output.get(CommonParams.FL));
         Assert.assertEquals(true, output.getBool(SpellingParams.SPELLCHECK_COLLATE));
         Assert.assertEquals(0, (int) output.getInt(CommonParams.START));
         Assert.assertTrue(output.getInt(CommonParams.ROWS) > 100);
@@ -61,12 +47,11 @@ public class SolrQueryUtilsTest
     @Test
     public void testEnhanceParamsDoesntReplaceExistingValues()
     {
-        ModifiableSolrParams input = new ModifiableSolrParams();
-        input.set(CommonParams.Q, "field:value");
+        SolrQuery input = new SolrQuery("field:value");
         input.set(CommonParams.FL, "id");
-        input.set(CommonParams.START, 30);
-        input.set(CommonParams.ROWS, 10);
-        SolrParams output = SolrQueryUtils.enhanceParams(input);
+        input.setStart(30);
+        input.setRows(10);
+        SolrQuery output = SolrQueryUtils.generateQuery(input, null);
         Assert.assertEquals("field:value", output.get(CommonParams.Q));
         Assert.assertEquals("id", output.get(CommonParams.FL));
         Assert.assertEquals(true, output.getBool(SpellingParams.SPELLCHECK_COLLATE));
@@ -77,15 +62,14 @@ public class SolrQueryUtilsTest
     @Test
     public void testEnhanceParamsWithNull()
     {
-        Assert.assertNull(SolrQueryUtils.enhanceParams(null));
+        Assert.assertNull(SolrQueryUtils.generateQuery(null, null));
     }
 
     @Test
     public void testApplySpellcheckSuggestions()
     {
-        ModifiableSolrParams input = new ModifiableSolrParams();
-        input.set(CommonParams.Q, "original");
-        SolrParams output = SolrQueryUtils.applySpellcheckSuggestion(input, "fixed");
+        SolrQuery input = new SolrQuery("original");
+        SolrQuery output = SolrQueryUtils.applySpellcheckSuggestion(input, "fixed");
         Assert.assertNotNull(output);
         Assert.assertEquals("fixed", output.get(CommonParams.Q));
     }
@@ -93,10 +77,9 @@ public class SolrQueryUtilsTest
     @Test
     public void testApplySpellcheckSuggestionsWithBoostQuery()
     {
-        ModifiableSolrParams input = new ModifiableSolrParams();
-        input.set(CommonParams.Q, "original with text:stab*");
+        SolrQuery input = new SolrQuery("original with text:stab*");
         input.set(DisMaxParams.BQ, "text:stab* name:stab*^5");
-        SolrParams output = SolrQueryUtils.applySpellcheckSuggestion(input, "fixed with text:stub*");
+        SolrQuery output = SolrQueryUtils.applySpellcheckSuggestion(input, "fixed with text:stub*");
         Assert.assertNotNull(output);
         Assert.assertEquals("fixed with text:stub* text:stab*^1.5", output.get(CommonParams.Q));
         Assert.assertArrayEquals(new String[] { "text:stab* name:stab*^5", "text:stub* name:stub*^5" },
@@ -106,28 +89,15 @@ public class SolrQueryUtilsTest
     @Test
     public void testApplySpellcheckSuggestionsWithNull()
     {
-        SolrParams output = SolrQueryUtils.applySpellcheckSuggestion(null, null);
+        SolrQuery output = SolrQueryUtils.applySpellcheckSuggestion(null, null);
         Assert.assertNull(output);
 
-        ModifiableSolrParams input = new ModifiableSolrParams();
-        input.set(CommonParams.Q, "original");
+        SolrQuery input = new SolrQuery("original");
         output = SolrQueryUtils.applySpellcheckSuggestion(input, null);
         Assert.assertNotNull(output);
         Assert.assertEquals("original", output.get(CommonParams.Q));
 
         output = SolrQueryUtils.applySpellcheckSuggestion(null, "fixed");
         Assert.assertNull(output);
-    }
-
-    @Test
-    public void testGetCacheKey()
-    {
-        ModifiableSolrParams input = new ModifiableSolrParams();
-        input.set(CommonParams.Q, "some value");
-        input.add(DisMaxParams.BQ, "text:stub*");
-        input.add(DisMaxParams.BQ, "stub*");
-        input.set(CommonParams.FQ, "is_a:HP\\:0000108");
-        String output = SolrQueryUtils.getCacheKey(input);
-        Assert.assertEquals("{q:[some value]\nbq:[text:stub*, stub*]\nfq:[is_a:HP\\:0000108]\n}", output);
     }
 }

components/vocabularies/hgnc/api/src/main/java/org/phenotips/vocabulary/internal/GeneNomenclature.java
@@ -44,13 +44,12 @@ import javax.inject.Singleton;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.util.ClientUtils;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.DisMaxParams;
-import org.apache.solr.common.params.ModifiableSolrParams;
-import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.params.SpellingParams;
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
@@ -224,13 +223,16 @@ public class GeneNomenclature extends AbstractCSVSolrVocabulary
         return requestTerm(ALTERNATIVE_ID_FIELD_NAME + ':' + id, null);
     }
 
-    private SolrParams produceDynamicSolrParams(String originalQuery, Integer rows, String sort, String customFilter)
+    private SolrQuery produceDynamicSolrParams(Map<String, String> staticOptions, String originalQuery, Integer rows,
+        String sort, String customFilter)
     {
         String escapedQuery = ClientUtils.escapeQueryChars(originalQuery.trim());
 
-        ModifiableSolrParams params = new ModifiableSolrParams();
-        params.add(CommonParams.Q, escapedQuery);
-        params.add(CommonParams.ROWS, rows.toString());
+        SolrQuery params = new SolrQuery(escapedQuery);
+        for (Map.Entry<String, String> option : staticOptions.entrySet()) {
+            params.set(option.getKey(), option.getValue());
+        }
+        params.setRows(rows);
         if (StringUtils.isNotBlank(sort)) {
             params.add(CommonParams.SORT, sort);
         }
@@ -256,9 +258,9 @@ public class GeneNomenclature extends AbstractCSVSolrVocabulary
 
     private List<VocabularyTerm> searchIdentifiers(String input, int maxResults, String sort, String customFilter)
     {
-        SolrParams params = produceDynamicSolrParams(input, maxResults, sort, customFilter);
+        SolrQuery params = produceDynamicSolrParams(IDENTIFIER_SEARCH_OPTIONS, input, maxResults, sort, customFilter);
         List<VocabularyTerm> result = new LinkedList<>();
-        for (SolrDocument doc : this.search(params, IDENTIFIER_SEARCH_OPTIONS)) {
+        for (SolrDocument doc : this.search(params)) {
             result.add(new SolrVocabularyTerm(doc, this));
         }
         return result;
@@ -266,9 +268,9 @@ public class GeneNomenclature extends AbstractCSVSolrVocabulary
 
     private List<VocabularyTerm> searchText(String input, int maxResults, String sort, String customFilter)
     {
-        SolrParams params = produceDynamicSolrParams(input, maxResults, sort, customFilter);
+        SolrQuery params = produceDynamicSolrParams(TEXT_SEARCH_OPTIONS, input, maxResults, sort, customFilter);
         List<VocabularyTerm> result = new LinkedList<>();
-        for (SolrDocument doc : this.search(params, TEXT_SEARCH_OPTIONS)) {
+        for (SolrDocument doc : this.search(params)) {
             result.add(new SolrVocabularyTerm(doc, this));
         }
         return result;
@@ -276,9 +278,10 @@ public class GeneNomenclature extends AbstractCSVSolrVocabulary
 
     private List<VocabularyTerm> searchTextSpellchecked(String input, int maxResults, String sort, String customFilter)
     {
-        SolrParams params = produceDynamicSolrParams(input, maxResults, sort, customFilter);
+        SolrQuery params =
+            produceDynamicSolrParams(SPELLCHECKED_TEXT_SEARCH_OPTIONS, input, maxResults, sort, customFilter);
         List<VocabularyTerm> result = new LinkedList<>();
-        for (SolrDocument doc : this.search(params, SPELLCHECKED_TEXT_SEARCH_OPTIONS)) {
+        for (SolrDocument doc : this.search(params)) {
             result.add(new SolrVocabularyTerm(doc, this));
         }
         return result;

commit 21e6212054f3d3f43fb8e6041a147067311d05b7

[bugfix] Fixed a bug breaking vocabulary searches introduced by e1df4c7

components/vocabularies/api/src/main/java/org/phenotips/vocabulary/internal/solr/AbstractSolrVocabulary.java
@@ -224,6 +224,7 @@ public abstract class AbstractSolrVocabulary implements Vocabulary, Initializabl
     protected SolrDocumentList search(SolrQuery query)
     {
         try {
+            query.setIncludeScore(true);
             this.logger.debug("Extending query [{}] for vocabulary [{}]", query, getCoreName());
             for (VocabularyExtension extension : this.extensions) {
                 extension.extendQuery(query, this);