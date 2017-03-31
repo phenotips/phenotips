e513e04753536f29f684699938cfcedda60cc82a
veronikaslc <veronika.koltunov@gmail.com>
2017-03-30 21:09:48 -0400

    PT-3088: Addressing codereview comments Work in progress

--- components/patient-data-sharing/push-api/src/main/java/org/phenotips/data/push/PushServerInfo.java
+++ components/patient-data-sharing/push-api/src/main/java/org/phenotips/data/push/PushServerInfo.java
@@ -36,7 +36,8 @@ public interface PushServerInfo extends Comparable<PushServerInfo>
     String getServerURL();
 
     /**
-     * @return server registration URL, as configured on the PhenoTips configuration page
+     * @return server registration URL, as configured on the PhenoTips configuration page. An empty string is possible,
+     *         no registration is offered in that case.
      */
     String getServerRegistrationURL();
 
