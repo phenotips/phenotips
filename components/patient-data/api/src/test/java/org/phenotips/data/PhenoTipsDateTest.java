ea27a7e9118bed61df4cab1601268dbe76d01f7b
AlexLapena <alapena@mail.uoguelph.ca>
2017-05-04 12:32:28 -0400

    Fixed Assertions

--- components/patient-data/api/src/test/java/org/phenotips/data/PhenoTipsDateTest.java
+++ components/patient-data/api/src/test/java/org/phenotips/data/PhenoTipsDateTest.java
@@ -14,48 +14,46 @@ public class PhenoTipsDateTest {
     @Test
     public void normalDateTest() throws ComponentLookupException {
         @SuppressWarnings("deprecation")
-        Date date = new Date(2017, 12, 11);
-        Date date2 = new Date(2017);
-        Date empty = null;
+        Date date = new Date(100, 11, 21);
         PhenoTipsDate phenoDate = new PhenoTipsDate(date);
-        PhenoTipsDate phenoDate2 = new PhenoTipsDate(date2);
-        PhenoTipsDate emptyDate = new PhenoTipsDate(empty);
+        
         Assert.assertTrue(phenoDate.isSet());
-        Assert.assertFalse(emptyDate.isSet());
-        // Fix these assertion statements
-        Assert.assertNotSame(new Date(2017, 12, 11), phenoDate.toEarliestPossibleISODate());
-        Assert.assertNotSame("2017-12-11", phenoDate.toYYYYMMDDString());
-        Assert.assertNotSame("2017", phenoDate2.toYYYYMMDDString());
-
+        Assert.assertEquals(date, phenoDate.toEarliestPossibleISODate());
+        Assert.assertEquals("2000-12-21", phenoDate.toYYYYMMDDString());
     }
 
     @Test
     public void nullDateTest() throws ComponentLookupException {
         Date date = null;
         PhenoTipsDate phenoDate = new PhenoTipsDate(date);
+        
         Assert.assertFalse(phenoDate.isSet());
-        Assert.assertSame(null, phenoDate.toYYYYMMDDString()); // Fix this(Expects undefinded date)
+        Assert.assertSame("", phenoDate.toYYYYMMDDString());
+        Assert.assertEquals(null, phenoDate.toEarliestPossibleISODate());
     }
 
     @Test
     public void normalJSONDateTest() throws ComponentLookupException {
         JSONObject date1 = new JSONObject("{\"year\":\"2013\"}");
         JSONObject date2 = new JSONObject("{\"year\":\"2010s\",\"month\":\"01\",\"day\":\"13\"}");
-        JSONObject depricatedDate = new JSONObject("{\"decade\":\"201s\",\"month\":\"01\",\"day\":\"13\"}");
+        JSONObject depricatedDate = new JSONObject("{\"decade\":\"201s\",\"month\":\"01\",\"day\":\"13\"}");       
         PhenoTipsDate phenoDate = new PhenoTipsDate(date1);
         PhenoTipsDate phenoDate2 = new PhenoTipsDate(date2);
         PhenoTipsDate phenoDateDepr = new PhenoTipsDate(depricatedDate);
         phenoDate.toJSON();
         phenoDate2.toJSON();
-        phenoDateDepr.toJSON(); // Debug assertions
-        Assert.assertSame("{\"year\":\"2010s\",\"month\":\"01\",\"day\":\"13\"}", phenoDateDepr.toString());
+        phenoDateDepr.toJSON(); 
+        
+        Assert.assertEquals("{\"month\":1,\"day\":13}", phenoDate2.toString());
+        Assert.assertEquals("{\"month\":1,\"year\":201,\"range\":{\"years\":10},\"day\":13}", phenoDateDepr.toString());
     }
 
     @Test
     public void emptyJSONDateTest() throws ComponentLookupException {
-        JSONObject fuzzyDate = new JSONObject(); // Figure out JSON input
-        PhenoTipsDate phenoDate = new PhenoTipsDate(fuzzyDate);
-        // Assert
+        JSONObject nullDate = null;
+        PhenoTipsDate emptyJSON = new PhenoTipsDate(nullDate);
+        emptyJSON.toJSON();
+        Assert.assertEquals("{}", emptyJSON.toString());
     }
 
     @Test
@@ -69,16 +67,16 @@ public class PhenoTipsDateTest {
         PhenoTipsDate phenoDate3 = new PhenoTipsDate(date3);
         PhenoTipsDate phenoDate4 = new PhenoTipsDate(date4);
 
-        Assert.assertNotNull(phenoDate); // Fix assertions; placeholder
-        Assert.assertNotNull(phenoDate2);
-        Assert.assertNotNull(phenoDate3);
-        Assert.assertNotNull(phenoDate4);
+        Assert.assertEquals("1995-06-23", phenoDate.toYYYYMMDDString());
+        Assert.assertEquals("1995-06", phenoDate2.toYYYYMMDDString());
+        Assert.assertEquals("1995", phenoDate3.toYYYYMMDDString());
+        Assert.assertEquals("1990s", phenoDate4.toYYYYMMDDString());
     }
 
     @Test
     public void nullStringDateTest() throws ComponentLookupException {
         String date = null;
-        PhenoTipsDate phenoDate = new PhenoTipsDate(date);
-        Assert.assertNotNull(phenoDate.toString());
+        PhenoTipsDate phenoDate = new PhenoTipsDate(date);       
+        Assert.assertEquals("{}",phenoDate.toString());
     }
 }
