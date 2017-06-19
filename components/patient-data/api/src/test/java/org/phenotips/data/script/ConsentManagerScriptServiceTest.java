f6dd835a3e834b7d078fd3c109abfd7286ff996c
Sergiu Dumitriu <sergiu@phenotips.org>
2017-06-18 23:51:56 -0400

    PT-3214: Write Tests for Patient Data Applied codestyle

--- components/patient-data/api/src/test/java/org/phenotips/data/script/ConsentManagerScriptServiceTest.java
+++ components/patient-data/api/src/test/java/org/phenotips/data/script/ConsentManagerScriptServiceTest.java
@@ -17,29 +17,31 @@
  */
 package org.phenotips.data.script;
 
-import org.json.JSONArray;
+import org.phenotips.data.Consent;
+import org.phenotips.data.ConsentManager;
+import org.phenotips.data.Patient;
+import org.phenotips.entities.PrimaryEntity;
+
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.test.mockito.MockitoComponentMockingRule;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.phenotips.data.Consent;
-import org.phenotips.data.ConsentManager;
-import org.phenotips.data.Patient;
-import org.xwiki.component.manager.ComponentLookupException;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
 
 import static org.mockito.Mockito.when;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-public class ConsentManagerScriptServiceTest {
-
+public class ConsentManagerScriptServiceTest
+{
     @Rule
     public final MockitoComponentMockingRule<ConsentManagerScriptService> mocker = new MockitoComponentMockingRule<>(
-            ConsentManagerScriptService.class);
+        ConsentManagerScriptService.class);
 
     private ConsentManager consentManager;
 
@@ -53,40 +55,45 @@ public class ConsentManagerScriptServiceTest {
     private Consent consent2;
 
     @Before
-    public void setUp() throws ComponentLookupException {
+    public void setUp() throws ComponentLookupException
+    {
         MockitoAnnotations.initMocks(this);
         this.consentManager = this.mocker.getInstance(ConsentManager.class);
     }
 
     @Test
-    public void hasConsentTest() throws ComponentLookupException {
+    public void hasConsentTest() throws ComponentLookupException
+    {
         when(this.consentManager.hasConsent("P0123456", "consent")).thenReturn(true);
         Assert.assertTrue(this.mocker.getComponentUnderTest().hasConsent("P0123456", "consent"));
     }
 
     @Test
-    public void hasNoConsentTest() throws ComponentLookupException {
+    public void hasNoConsentTest() throws ComponentLookupException
+    {
         when(this.consentManager.hasConsent("P0123456", "consent")).thenReturn(false);
         Assert.assertFalse(this.mocker.getComponentUnderTest().hasConsent("P0123456", "consent"));
     }
 
     @Test
-    public void invalidPatientId() throws ComponentLookupException {
+    public void invalidPatientId() throws ComponentLookupException
+    {
         Set<Consent> consents = new LinkedHashSet<>();
-        consents.add(consent1);
-        consents.add(consent2);
+        consents.add(this.consent1);
+        consents.add(this.consent2);
 
         when(this.consentManager.toJSON(consents)).thenReturn(null);
-        Assert.assertNull(this.mocker.getComponentUnderTest().getAllConsentsForPatient(Patient.JSON_KEY_ID));
+        Assert.assertNull(this.mocker.getComponentUnderTest().getAllConsentsForPatient(PrimaryEntity.JSON_KEY_ID));
     }
 
     @Test
-    public void getConsentsForPatientWithException() throws ComponentLookupException {
+    public void getConsentsForPatientWithException() throws ComponentLookupException
+    {
         Set<Consent> consents = new LinkedHashSet<>();
         Assert.assertTrue(consents.isEmpty());
 
-        when(this.consentManager.getAllConsentsForPatient(patient)).thenThrow(new NullPointerException());
-        Assert.assertNull(this.mocker.getComponentUnderTest().getAllConsentsForPatient(Patient.JSON_KEY_ID));
+        when(this.consentManager.getAllConsentsForPatient(this.patient)).thenThrow(new NullPointerException());
+        Assert.assertNull(this.mocker.getComponentUnderTest().getAllConsentsForPatient(PrimaryEntity.JSON_KEY_ID));
     }
 
 }
