327d4a49f146cacf87a61071343363dc54a44fa1
a-gvo <alina.gvo@gmail.com>
2017-08-15 10:12:24 -0400

    PT-3352: Negative genes are now excluded from Gene Panel suggestions by default.

--- components/gene-panels/api/src/main/java/org/phenotips/panels/GenePanelFactory.java
+++ components/gene-panels/api/src/main/java/org/phenotips/panels/GenePanelFactory.java
@@ -66,7 +66,7 @@ public interface GenePanelFactory
 
     /**
      * Create an object of {@link GenePanel} class for a given {@link Patient} object. Matching genes marked as rejected
-     * in the {@code patient} will not be excluded from the panel.
+     * in the {@code patient} will be excluded from the panel.
      *
      * @param patient the {@link Patient} of interest
      * @return a new {@link GenePanel} object for the patient
