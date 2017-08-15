phenotips/phenotips

commit 34f7a005f502317eb08a99310be9fa0e0f87cac6

PT-3329: Enable checkstyle for test code Fixed checkstyle errors

components/specificity-meter/api/src/main/java/org/phenotips/data/internal/MonarchPatientScorer.java
@@ -35,11 +35,6 @@ import org.xwiki.configuration.ConfigurationSource;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
@@ -49,7 +44,6 @@ import java.util.TimeZone;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
-import javax.net.ssl.SSLContext;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -57,14 +51,9 @@ import org.apache.http.Consts;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
-import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -110,15 +99,6 @@ public class MonarchPatientScorer implements PatientScorer, Initializable
             this.cache = this.cacheManager.createNewCache(config);
         } catch (CacheException ex) {
             throw new InitializationException("Failed to create cache", ex);
-        }
-        try {
-            SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(null, new TrustAllStrategy()).build();
-            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, null, null,
-                NoopHostnameVerifier.INSTANCE);
-            this.client = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException ex) {
-            this.logger.warn("Failed to set custom certificate trust, using the default", ex);
-            this.client = HttpClients.createSystem();
         }
     }
 
@@ -210,14 +190,5 @@ public class MonarchPatientScorer implements PatientScorer, Initializable
     private Date now()
     {
         return Calendar.getInstance(TimeZone.getTimeZone("UTC"), Locale.ROOT).getTime();
-    }
-
-    private static final class TrustAllStrategy implements TrustStrategy
-    {
-        @Override
-        public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException
-        {
-            return true;
-        }
     }
 }

components/consents/api/src/test/java/org/phenotips/consents/internal/DefaultConsentAuthorizerTest.java
@@ -171,9 +171,7 @@ public class DefaultConsentAuthorizerTest
     @Test
     public void authorizeInteractionForPatientWhenPatientIsNull()
     {
-        final Patient patient = null;
-        final boolean authorizeInteraction = this.component.authorizeInteraction(patient);
-        Assert.assertFalse(authorizeInteraction);
+        Assert.assertFalse(this.component.authorizeInteraction((Patient) null));
     }
 
     @Test
@@ -265,11 +263,10 @@ public class DefaultConsentAuthorizerTest
         final Set<Consent> missingConsents = new HashSet<>();
         missingConsents.add(this.consentA);
 
-        final Patient patient = mock(Patient.class);
-        when(this.consentManager.getMissingConsentsForPatient(patient)).thenReturn(missingConsents);
+        when(this.consentManager.getMissingConsentsForPatient(this.patient)).thenReturn(missingConsents);
         when(this.consentA.isRequired()).thenReturn(true);
 
-        final boolean isElementConsented = this.component.isElementConsented(element, patient);
+        final boolean isElementConsented = this.component.isElementConsented(element, this.patient);
         Assert.assertFalse(isElementConsented);
     }
 
@@ -286,15 +283,14 @@ public class DefaultConsentAuthorizerTest
         missingConsents.add(this.consentA);
         missingConsents.add(this.consentB);
 
-        final Patient patient = mock(Patient.class);
-        when(this.consentManager.getMissingConsentsForPatient(patient)).thenReturn(missingConsents);
+        when(this.consentManager.getMissingConsentsForPatient(this.patient)).thenReturn(missingConsents);
         when(this.consentA.affectsAllFields()).thenReturn(true);
         when(this.consentB.affectsAllFields()).thenReturn(false);
 
         final List<String> nonConsentedFields = Arrays.asList(FIELD_A_LABEL, FIELD_B_LABEL);
         when(this.consentA.getFields()).thenReturn(nonConsentedFields);
 
-        final boolean isElementConsented = this.component.isElementConsented(element, patient);
+        final boolean isElementConsented = this.component.isElementConsented(element, this.patient);
         Assert.assertFalse(isElementConsented);
     }
 
@@ -311,8 +307,7 @@ public class DefaultConsentAuthorizerTest
         missingConsents.add(this.consentA);
         missingConsents.add(this.consentB);
 
-        final Patient patient = mock(Patient.class);
-        when(this.consentManager.getMissingConsentsForPatient(patient)).thenReturn(missingConsents);
+        when(this.consentManager.getMissingConsentsForPatient(this.patient)).thenReturn(missingConsents);
         when(this.consentA.affectsAllFields()).thenReturn(false);
         when(this.consentB.affectsAllFields()).thenReturn(false);
         when(this.consentA.affectsSomeFields()).thenReturn(false);
@@ -321,7 +316,7 @@ public class DefaultConsentAuthorizerTest
         final List<String> nonConsentedFields = Arrays.asList(FIELD_A_LABEL, FIELD_B_LABEL);
         when(this.consentA.getFields()).thenReturn(nonConsentedFields);
 
-        final boolean isElementConsented = this.component.isElementConsented(element, patient);
+        final boolean isElementConsented = this.component.isElementConsented(element, this.patient);
         Assert.assertTrue(isElementConsented);
     }
 
@@ -338,8 +333,7 @@ public class DefaultConsentAuthorizerTest
         missingConsents.add(this.consentA);
         missingConsents.add(this.consentB);
 
-        final Patient patient = mock(Patient.class);
-        when(this.consentManager.getMissingConsentsForPatient(patient)).thenReturn(missingConsents);
+        when(this.consentManager.getMissingConsentsForPatient(this.patient)).thenReturn(missingConsents);
         when(this.consentA.affectsAllFields()).thenReturn(false);
         when(this.consentB.affectsAllFields()).thenReturn(false);
         when(this.consentA.affectsSomeFields()).thenReturn(true);
@@ -348,7 +342,7 @@ public class DefaultConsentAuthorizerTest
         when(this.consentA.getFields()).thenReturn(Arrays.asList(FIELD_A_LABEL, FIELD_B_LABEL));
         when(this.consentB.getFields()).thenReturn(Collections.singletonList(FIELD_C_LABEL));
 
-        final boolean isElementConsented = this.component.isElementConsented(element, patient);
+        final boolean isElementConsented = this.component.isElementConsented(element, this.patient);
         Assert.assertTrue(isElementConsented);
     }
 
@@ -365,8 +359,7 @@ public class DefaultConsentAuthorizerTest
         missingConsents.add(this.consentA);
         missingConsents.add(this.consentB);
 
-        final Patient patient = mock(Patient.class);
-        when(this.consentManager.getMissingConsentsForPatient(patient)).thenReturn(missingConsents);
+        when(this.consentManager.getMissingConsentsForPatient(this.patient)).thenReturn(missingConsents);
         when(this.consentA.affectsAllFields()).thenReturn(false);
         when(this.consentB.affectsAllFields()).thenReturn(false);
         when(this.consentA.affectsSomeFields()).thenReturn(true);
@@ -375,7 +368,7 @@ public class DefaultConsentAuthorizerTest
         when(this.consentA.getFields()).thenReturn(Arrays.asList(FIELD_A_LABEL, FIELD_B_LABEL));
         when(this.consentB.getFields()).thenReturn(Collections.singletonList(FIELD_C_LABEL));
 
-        final boolean isElementConsented = this.component.isElementConsented(element, patient);
+        final boolean isElementConsented = this.component.isElementConsented(element, this.patient);
         Assert.assertFalse(isElementConsented);
     }
 }

components/consents/api/src/test/java/org/phenotips/consents/internal/PhenoTipsPatientConsentManagerTest.java
@@ -21,6 +21,7 @@ import org.phenotips.consents.Consent;
 import org.phenotips.consents.ConsentManager;
 import org.phenotips.data.Patient;
 import org.phenotips.data.PatientRepository;
+
 import org.xwiki.bridge.DocumentAccessBridge;
 import org.xwiki.bridge.DocumentModelBridge;
 import org.xwiki.component.util.DefaultParameterizedType;
@@ -60,7 +61,7 @@ public class PhenoTipsPatientConsentManagerTest
 {
     @Rule
     public final MockitoComponentMockingRule<ConsentManager> mocker =
-        new MockitoComponentMockingRule<ConsentManager>(PhenoTipsPatientConsentManager.class);
+        new MockitoComponentMockingRule<>(PhenoTipsPatientConsentManager.class);
 
     /** Sets up initialization of the component with the given {@code baseObjects}. */
     @SuppressWarnings({ "rawtypes", "unchecked" })

components/constants/src/main/java/org/phenotips/Constants.java
@@ -26,6 +26,7 @@ import org.xwiki.model.reference.EntityReference;
  * @version $Id$
  * @since 1.0M9
  */
+@SuppressWarnings("checkstyle:InterfaceIsType")
 public interface Constants
 {
     /** The name of the space where most of the PhenoTips code resides in. */

components/diagnosis-suggestion/api/src/test/java/org/phenotips/diagnosis/internal/DefaultDiagnosisServiceTest.java
@@ -57,11 +57,11 @@ public class DefaultDiagnosisServiceTest
 {
     @Rule
     public final MockitoComponentMockingRule<DiagnosisService> mocker =
-        new MockitoComponentMockingRule<DiagnosisService>(DefaultDiagnosisService.class);
+        new MockitoComponentMockingRule<>(DefaultDiagnosisService.class);
 
     @Rule
     public final MockitoComponentMockingRule<Utils> workingUtils =
-        new MockitoComponentMockingRule<Utils>(BoqaUtils.class);
+        new MockitoComponentMockingRule<>(BoqaUtils.class);
 
     @Test
     public void returnsCorrectDiagnosis() throws ComponentLookupException, IOException, InterruptedException
@@ -137,7 +137,8 @@ public class DefaultDiagnosisServiceTest
         List<String> nonstandardPhenotypeSet = new LinkedList<>();
         nonstandardPhenotypeSet.add("Non-standard term");
         for (List<String> phenotypeSet : phenotypes) {
-            List<VocabularyTerm> diagnoses = diagnosisService.getDiagnosis(phenotypeSet, nonstandardPhenotypeSet, limit);
+            List<VocabularyTerm> diagnoses =
+                diagnosisService.getDiagnosis(phenotypeSet, nonstandardPhenotypeSet, limit);
             List<String> diagnosisIds = new LinkedList<>();
             for (VocabularyTerm diagnosis : diagnoses) {
                 diagnosisIds.add(diagnosis.getId());

components/gene-panels/api/src/main/java/org/phenotips/panels/GenePanel.java
@@ -27,8 +27,8 @@ import java.util.Set;
 import org.json.JSONObject;
 
 /**
- * A Gene Panel links {@link #getPresentTerms() a collection of observed symptoms} to {@link #getTermsForGeneList()
- * a list of genes} frequently associated with these symptoms, in descending order of relevance.
+ * A Gene Panel links {@link #getPresentTerms() a collection of observed symptoms} to {@link #getTermsForGeneList() a
+ * list of genes} frequently associated with these symptoms, in descending order of relevance.
  *
  * @version $Id$
  * @since 1.3
@@ -75,7 +75,7 @@ public interface GenePanel
      * @throws IndexOutOfBoundsException if (<tt>fromIndex &lt; 0 || toIndex &gt; {@link #size()} ||
      *         fromIndex &gt; toIndex</tt>)
      */
-    JSONObject toJSON(final int fromIndex, final int toIndex);
+    JSONObject toJSON(int fromIndex, int toIndex);
 
     /**
      * Returns the number of genes represented by the gene panel.

components/gene-panels/api/src/main/java/org/phenotips/panels/GenePanelFactory.java
@@ -44,7 +44,7 @@ public interface GenePanelFactory
      * @param features a collection of {@link Feature} objects, both observed to be present and observed to be absent
      * @return a new {@link GenePanel} object for the collection of features
      */
-    GenePanel build(@Nonnull final Collection<? extends Feature> features);
+    GenePanel build(@Nonnull Collection<? extends Feature> features);
 
     /**
      * Creates an object of {@link GenePanel} class, given a collection of present and absent {@link VocabularyTerm}
@@ -54,8 +54,8 @@ public interface GenePanelFactory
      * @param absentTerms absent {@link VocabularyTerm} objects
      * @return a new {@link GenePanel} object for the collection of present and absent {@link VocabularyTerm} objects
      */
-    GenePanel build(@Nonnull final Collection<VocabularyTerm> presentTerms,
-        @Nonnull final Collection<VocabularyTerm> absentTerms);
+    GenePanel build(@Nonnull Collection<VocabularyTerm> presentTerms,
+        @Nonnull Collection<VocabularyTerm> absentTerms);
 
     /**
      * Create an object of {@link GenePanel} class for a given {@link Patient} object.
@@ -63,5 +63,5 @@ public interface GenePanelFactory
      * @param patient the {@link Patient} of interest
      * @return a new {@link GenePanel} object for the patient
      */
-    GenePanel build(@Nonnull final Patient patient);
+    GenePanel build(@Nonnull Patient patient);
 }

commit e5a5170c033389aa8cc8880bb82d9435176a65a2

[bugfix] Fixed regression introduced in 34f7a005f

components/specificity-meter/api/src/main/java/org/phenotips/data/internal/MonarchPatientScorer.java
@@ -54,6 +54,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -97,6 +98,7 @@ public class MonarchPatientScorer implements PatientScorer, Initializable
                 .getProperty("phenotips.patientScoring.monarch.serviceURL", "https://monarchinitiative.org/score");
             CacheConfiguration config = new LRUCacheConfiguration("monarchSpecificityScore", 2048, 3600);
             this.cache = this.cacheManager.createNewCache(config);
+            this.client = HttpClients.createSystem();
         } catch (CacheException ex) {
             throw new InitializationException("Failed to create cache", ex);
         }