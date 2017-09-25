470b39ebebc21ffb1f9e857ee9413cb26566fa10
Andrew Misyura <asm@phenotips.org>
2017-09-25 10:19:20 -0400

    PT-3431: Update format of stored/serialized pedigrees
    Made pedigree editor aware of PhenoTips version (which is necessary to corretly read/write Patient JSONs)

--- components/pedigree/resources/src/main/resources/pedigree/preferencesManager.js
+++ components/pedigree/resources/src/main/resources/pedigree/preferencesManager.js
@@ -1,4 +1,11 @@
-define(["pedigree/model/helpers"], function(Helpers){
+define([
+      "pedigree/model/helpers",
+      "pedigree/model/phenotipsJSON"
+    ], function(
+      Helpers,
+      PhenotipsJSON
+    ){
+
     var PreferencesManager = Class.create( {
         initialize: function( templatePreferences ) {
             this.preferences = templatePreferences;
@@ -20,14 +27,24 @@ define(["pedigree/model/helpers"], function(Helpers){
             if (response.responseJSON) {
                 // only set preferences which are given in the template
                 Helpers.setByTemplate(this.preferences, response.responseJSON);
-                console.log("Loaded preferences: " + Helpers.stringifyObject(response.responseJSON));
+                this.preferences.phenotipsVersion = response.responseJSON.phenotipsVersion;
+
+                console.log("Loaded pedigree system preferences: " + Helpers.stringifyObject(response.responseJSON));
+
+                if (!PhenotipsJSON.isVersionSupported(this.getPhenotipsVersion())) {
+                    alert("Current PhenoTips Patient JSON version (" + this.getPhenotipsVersion() + ") may not be supported");
+                }
             } else {
-                console.log("Failed to loaded properties, no JSON");
+                console.log("Failed to load pedigree system preferences, no JSON");
             }
 
             // read user preferences from cookies
             // TODO: read from user profile instead of using cookies?
             this.preferences.user = Helpers.getAllCookies(this._cookiePrefix);
+        },
+
+        getPhenotipsVersion: function() {
+            return this.preferences.hasOwnProperty("phenotipsVersion") ? this.preferences.phenotipsVersion : "";
         },
 
         /**
