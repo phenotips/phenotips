7ed15da1defa93d7530dcf73e938828402432fef
a-gvo <alina.gvo@gmail.com>
2017-10-17 15:19:44 -0400

    PT-3104: Different/inconsistent behaviour when filtering in Advanced data filtering and Export [fixed]

--- components/widgets/resources/src/main/resources/resources/uicomponents/widgets/exportData.js
+++ components/widgets/resources/src/main/resources/resources/uicomponents/widgets/exportData.js
@@ -357,6 +357,8 @@ document.observe('xwiki:dom:loading', function() {
       document.observe('xwiki:multisuggestpicker:selectionchanged', updateMatchCounter);
       content.select('input[type=radio]').invoke('observe', 'click', updateMatchCounter);
       content.select('input[type=checkbox]').invoke('observe', 'click', updateMatchCounter);
+      content.select('input[name=external_id]').invoke('observe', 'input', updateMatchCounter);
+      // IE9 is buggy, and does not fire input on character delete.
       content.select('input[name=external_id]').invoke('observe', 'keyup', updateMatchCounter);
     }
   };
