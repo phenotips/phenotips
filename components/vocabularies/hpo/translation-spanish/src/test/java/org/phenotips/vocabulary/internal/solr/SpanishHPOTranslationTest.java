dd478590e68796966f7c176282894907eb44fcc4
Sergiu Dumitriu <sergiu@phenotips.org>
2017-12-12 22:56:17 -0500

    PT-3532: Add support for German and Russian HPO translations

---
+++ components/vocabularies/hpo/translation-spanish/src/test/java/org/phenotips/vocabulary/internal/solr/SpanishHPOTranslationTest.java
@@ -0,0 +1,217 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.vocabulary.internal.solr;
+
+import org.phenotips.vocabulary.SolrVocabularyResourceManager;
+import org.phenotips.vocabulary.Vocabulary;
+import org.phenotips.vocabulary.VocabularyExtension;
+import org.phenotips.vocabulary.VocabularyInputTerm;
+import org.phenotips.vocabulary.translation.internal.SpanishHPOTranslation;
+
+import org.xwiki.cache.CacheException;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.configuration.ConfigurationSource;
+import org.xwiki.localization.LocalizationContext;
+import org.xwiki.test.mockito.MockitoComponentMockingRule;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrResponse;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.request.schema.SchemaRequest;
+import org.apache.solr.client.solrj.request.schema.SchemaRequest.AddFieldType;
+import org.apache.solr.common.params.DisMaxParams;
+import org.apache.solr.common.util.NamedList;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.CapturingMatcher;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for the Spanish translation of HPO.
+ *
+ * @version $Id$
+ */
+public class SpanishHPOTranslationTest
+{
+    @Rule
+    public final MockitoComponentMockingRule<VocabularyExtension> mocker =
+        new MockitoComponentMockingRule<>(SpanishHPOTranslation.class);
+
+    @Mock
+    private Vocabulary vocabulary;
+
+    @Mock
+    private SolrClient solrClient;
+
+    @Mock
+    private VocabularyInputTerm term;
+
+    private VocabularyExtension component;
+
+    private LocalizationContext localizationContext;
+
+    private ConfigurationSource config;
+
+    private CapturingMatcher<? extends SolrRequest<? extends SolrResponse>> coreRequests = new CapturingMatcher<>();
+
+    @Before
+    public void setUp()
+        throws ComponentLookupException, IOException, SolrServerException, CacheException
+    {
+        MockitoAnnotations.initMocks(this);
+
+        SolrVocabularyResourceManager solrManager = this.mocker.getInstance(SolrVocabularyResourceManager.class);
+        when(solrManager.getSolrConnection(any(String.class))).thenReturn(this.solrClient);
+        when(this.solrClient.request(Matchers.argThat(this.coreRequests), any())).thenReturn(new NamedList<>());
+
+        this.localizationContext = this.mocker.getInstance(LocalizationContext.class);
+        when(this.localizationContext.getCurrentLocale()).thenReturn(new Locale("es"));
+
+        this.config = this.mocker.getInstance(ConfigurationSource.class, "wiki");
+        when(this.config.getProperty("multilingual", false)).thenReturn(true);
+        when(this.config.getProperty("languages", Collections.singleton("es")))
+            .thenReturn(Collections.singleton("es"));
+
+        when(this.vocabulary.getIdentifier()).thenReturn("hpo");
+
+        this.component = this.mocker.getComponentUnderTest();
+    }
+
+    @Test
+    public void spanishFieldTypeIsAddedDuringInitialization() throws JSONException, IOException
+    {
+        SchemaRequest.AddFieldType fieldTypeRequest = (AddFieldType) this.coreRequests.getAllValues().stream()
+            .filter(item -> item instanceof SchemaRequest.AddFieldType).findAny().get();
+        JSONObject request =
+            new JSONObject(IOUtils.toString(fieldTypeRequest.getContentStreams().iterator().next().getReader()))
+                .getJSONObject("add-field-type");
+        Assert.assertEquals("text_general_es", request.get("name"));
+        Assert.assertEquals("org.apache.lucene.analysis.es.SpanishAnalyzer",
+            request.getJSONObject("analyzer").get("class"));
+    }
+
+    @Test
+    public void spanishFieldsAreAddedDuringInitialization() throws JSONException, IOException
+    {
+        List<JSONObject> fieldRequests = this.coreRequests.getAllValues().stream()
+            .filter(item -> item instanceof SchemaRequest.AddField)
+            .map(item -> {
+                try {
+                    return new JSONObject(IOUtils.toString(item.getContentStreams().iterator().next().getReader()))
+                        .getJSONObject("add-field");
+                } catch (JSONException | IOException e) {
+                    return null;
+                }
+            })
+            .collect(Collectors.toList());
+        Set<String> fieldNames = fieldRequests.stream().map(item -> item.getString("name")).collect(Collectors.toSet());
+        Assert.assertTrue(fieldNames.contains("name_es"));
+        Assert.assertTrue(fieldNames.contains("def_es"));
+        Assert.assertTrue(fieldNames.contains("synonym_es"));
+        for (JSONObject request : fieldRequests) {
+            Assert.assertEquals("text_general_es", request.get("type"));
+            Assert.assertTrue(request.getBoolean("stored"));
+            Assert.assertTrue(request.getBoolean("indexed"));
+        }
+    }
+
+    @Test
+    public void queriesAreExtendedToIncludeSpanishFieldsWhenLocaleIsEs()
+    {
+        SolrQuery query = new SolrQuery("seizures");
+        query.set(DisMaxParams.QF, "name");
+        query.set(DisMaxParams.PF, "name");
+        this.component.extendQuery(query, this.vocabulary);
+        Assert.assertEquals("name name_es^60 synonym_es^45 def_es^12 ", query.get(DisMaxParams.PF));
+        Assert.assertEquals("name name_es^30 synonym_es^21 def_es^6 ", query.get(DisMaxParams.QF));
+    }
+
+    @Test
+    public void queriesAreExtendedToIncludeSpanishFieldsWhenLocaleIsEsES()
+    {
+        when(this.localizationContext.getCurrentLocale()).thenReturn(new Locale("es", "ES"));
+        SolrQuery query = new SolrQuery("seizures");
+        query.set(DisMaxParams.QF, "name");
+        query.set(DisMaxParams.PF, "name");
+        this.component.extendQuery(query, this.vocabulary);
+        Assert.assertEquals("name name_es^60 synonym_es^45 def_es^12 ", query.get(DisMaxParams.PF));
+        Assert.assertEquals("name name_es^30 synonym_es^21 def_es^6 ", query.get(DisMaxParams.QF));
+    }
+
+    @Test
+    public void queriesAreNotModifiedWhenLocaleIsFr()
+    {
+        when(this.localizationContext.getCurrentLocale()).thenReturn(Locale.FRENCH);
+        SolrQuery query = new SolrQuery("seizures");
+        query.set(DisMaxParams.QF, "name");
+        query.set(DisMaxParams.PF, "name");
+        this.component.extendQuery(query, this.vocabulary);
+        Assert.assertEquals("name", query.get(DisMaxParams.PF));
+        Assert.assertEquals("name", query.get(DisMaxParams.QF));
+    }
+
+    @Test
+    public void queriesAreNotModifiedWhenLocaleIsNull()
+    {
+        when(this.localizationContext.getCurrentLocale()).thenReturn(null);
+        SolrQuery query = new SolrQuery("seizures");
+        query.set(DisMaxParams.QF, "name");
+        query.set(DisMaxParams.PF, "name");
+        this.component.extendQuery(query, this.vocabulary);
+        Assert.assertEquals("name", query.get(DisMaxParams.PF));
+        Assert.assertEquals("name", query.get(DisMaxParams.QF));
+    }
+
+    @Test
+    public void queriesAreNotModifiedWhenFieldsAreInitiallyEmpty()
+    {
+        SolrQuery query = new SolrQuery("seizures");
+        query.set(DisMaxParams.BQ, "hello");
+        this.component.extendQuery(query, this.vocabulary);
+        Assert.assertNull(query.get(DisMaxParams.PF));
+        Assert.assertNull(query.get(DisMaxParams.QF));
+    }
+
+    @Test
+    public void termsAreExtendedWhenLocaleIsSupported()
+    {
+        when(this.term.getId()).thenReturn("HP:0000001");
+        this.component.extendTerm(this.term, this.vocabulary);
+        Mockito.verify(this.term).set("name_es", "Todo");
+    }
+}
