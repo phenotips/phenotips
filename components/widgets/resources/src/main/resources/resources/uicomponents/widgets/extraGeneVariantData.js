0c77df50d5e00803e53653c0add09100af34589b
Alina Gvozdik <alina.gvo@gmail.com>
2018-02-15 13:46:12 -0500

    PT-3585: Newly added genes and variants do not have correct delete urls

--- components/widgets/resources/src/main/resources/resources/uicomponents/widgets/extraGeneVariantData.js
+++ components/widgets/resources/src/main/resources/resources/uicomponents/widgets/extraGeneVariantData.js
@@ -168,7 +168,7 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
           case /_\d+_/.test(placeholder):
             return '_' + geneIndex + '_';
           case /=\d+&/.test(placeholder):
-            return '_' + geneIndex + '_';
+            return '=' + geneIndex + '&';
           default:
             return "";
         }
@@ -319,6 +319,7 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
 
       data.VRCOUNT_PLACEHOLDER = geneCount + '.' + varCount;
 
+      var _this = this;
       var evaluateTemplate = function evaluateTemplate(placeholder) {
         switch(true) {
           case /_\d+_/.test(placeholder):
@@ -326,7 +327,7 @@ var ExtraGeneVariantData = (function (ExtraGeneVariantData) {
           case /=\d+&/.test(placeholder):
             return '=' + varIndex + '&';
           case /\$\{object.xWikiClass.name\}/.test(placeholder):
-            return this.geneVariantClassName;
+            return _this.geneVariantClassName;
           case /\$\{object.number\}/.test(placeholder):
             return varIndex;
           default:
