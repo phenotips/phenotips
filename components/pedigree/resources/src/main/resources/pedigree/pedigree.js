7e22cd0278bf87d1b0b8e2b4d7f8861eef4e5e70
Andrew Misyura <asm@phenotips.org>
2018-06-08 17:18:28 -0400

    PT-3718: Pedigree editor should warn user about unsaved changes before closing
    Simplified and fixed on-close/on-save logic (no more saving without a warning, removed redundant code)

--- components/pedigree/resources/src/main/resources/pedigree/pedigree.js
+++ components/pedigree/resources/src/main/resources/pedigree/pedigree.js
@@ -167,12 +167,6 @@ define([
                     this._patientLegend = new PatientDropLegend();
                     this._addNewMember = new AddNewMemberDialog();
 
-                    var newPatientId = window.self.location.href.toQueryParams().new_patient_id;
-                    if (newPatientId && newPatientId != ""){
-                        this.getPatientLegend().addCase(newPatientId);
-                        this._unsavedNewPatient = true;
-                    }
-
                     this._nodeMenu = this.generateNodeMenu();
                     this._deceasedMenu = this.generateDeceasedMenu();
                     this._nodeGroupMenu = this.generateNodeGroupMenu();
@@ -180,9 +174,15 @@ define([
                     this._exportSelector = new ExportSelector();
                     this._printDialog = new PrintDialog();
 
-                    // finally, load the pedigree
                     var newPatientId = window.self.location.href.toQueryParams().new_patient_id;
-                    if (newPatientId && newPatientId != ""){
+
+                    // finally, load the pedigree
+                    if (newPatientId && newPatientId != "") {
+                        // since this is a new patient, we do not want to load that patients data, we explicitly
+                        // want to load the family document (either new family or existing), and add new patient
+                        // to the un-llinked patients legend (note: in case it is a new family and a template is
+                        // loaded, patient may be auto-assigned to a node in the template)
+                        this.getPatientLegend().addCase(newPatientId);
                         this._saveLoadEngine.load(XWiki.currentDocument.page);
                     } else {
                         var documentId = editor.getGraph().getCurrentPatientId();
@@ -307,13 +307,8 @@ define([
 
             var unsavedChanges = editor.getUndoRedoManager().hasUnsavedChanges();
 
-            if (!unsavedChanges && this._unsavedNewPatient) {
-                // we want the explicit message that the current patient is unlinked
-                noPatientsAreLinked = false;
-            }
-
-            var noPatientsLinkedMessage = "There are no patients linked to this pedigree and thus family has no members.<br/><br/>";
-            var currentPatientUnlinkedMessage = "Current patient is not linked to the pedigree and thus not part of the family.<br/><br/>";
+            var noPatientsLinkedMessage = "There are no patients in this family.<br/><br/>";
+            var currentPatientUnlinkedMessage = "Current patient is not a member of this family.<br/><br/>";
 
             //-----------------
             var saveWithChecks = function(quitAfterSave) {
@@ -322,10 +317,7 @@ define([
                     editor.getSaveLoadEngine().save(editor.closePedigree);
                 }
                 var saveAndKeepEditingFunc = function() {
-                    var onSuccessfulSave = function() {
-                        editor._unsavedNewPatient = false;
-                    };
-                    editor.getSaveLoadEngine().save(onSuccessfulSave);
+                    editor.getSaveLoadEngine().save();
                 }
                 var removeFamily = function() {
                     var removed = false;
@@ -399,11 +391,7 @@ define([
                             " Don't save and quit ", editor.closePedigree,
                             " Keep editing pedigree ", undefined, true );
                 } else {
-                    if (this._unsavedNewPatient) {
-                        saveWithChecks(true);
-                    } else {
-                        editor.closePedigree();
-                    }
+                    editor.closePedigree();
                 }
             } else {
                 saveWithChecks(false);
