999401bcf85857dbd406807a73cdcf07753922ee
Brian Li <brian.li@sickkids.ca>
2019-04-25 15:55:24 -0400

    PT-3979 JS injection via group names - Escape the alias id too.

--- components/base-war/src/main/webapp/resources/uicomponents/widgets/userpicker/userPicker.js
+++ components/base-war/src/main/webapp/resources/uicomponents/widgets/userpicker/userPicker.js
@@ -131,7 +131,7 @@ widgets.UserPicker = Class.create(widgets.Suggest, {
     var userName = source.highlight ? this.emphasizeMatches(this.sInput.escapeHTML(), data.info.escapeHTML()) : data.info.escapeHTML();
     container.insert(new Element('div', {'class': 'user-name'}).update(userName));
     var referenceWrapper = new Element('div');
-    var userAlias = source.highlight ? this.emphasizeMatches(this.sInput, data.id.replace(/.*\//g, '')) : data.id;
+    var userAlias = source.highlight ? this.emphasizeMatches(this.sInput, data.id.replace(/.*\//g, '')) : data.id.escapeHTML();
     referenceWrapper.insert(new Element('span', {'class': 'user-alias'}).update(userAlias));
     var userReference = XWiki.Model.resolve(data.value, XWiki.EntityType.DOCUMENT);
     var wiki = userReference.extractReferenceValue(XWiki.EntityType.WIKI) || XWiki.currentWiki;
