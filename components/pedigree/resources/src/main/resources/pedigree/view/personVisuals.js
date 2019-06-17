00e3db5588ecf50935ae8fc5fc77ab7d14cccf90
Daniel Gross <daniel.gross.2@gmail.com>
2019-06-17 12:19:23 -0400

    PT-4020 In the pedigree editor, cancer labels are sometimes vertically
    offset from the relevant individual
    
    Fixed

--- components/pedigree/resources/src/main/resources/pedigree/view/personVisuals.js
+++ components/pedigree/resources/src/main/resources/pedigree/view/personVisuals.js
@@ -822,10 +822,11 @@ define([
          */
         updateCommentsLabel: function() {
             this.getCommentsLabel() && this.getCommentsLabel().remove();
-            if (this.getNode().getComments() != "") {
-                // note: raphael positions text which starts with a new line in a strange way
-                //       also, blank lines are ignored unless replaced with a space
-                var text = this.getNode().getComments().replace(/^\s+|\s+$/g,'').replace(/\n\n/gi,'\n \n');
+            var text = typeof this.getNode().getComments() === 'string' ? this.getNode().getComments() : "";
+            // note: raphael positions text which starts with a new line in a strange way
+            //       also, blank lines are ignored unless replaced with a space
+            text = text.replace(/^\s+|\s+$/g,'').replace(/\n\n/gi,'\n \n');
+            if (text != "") {
                 this._commentsLabel = editor.getPaper().text(this.getX(), this.getY(), text).attr(PedigreeEditorParameters.attributes.commentLabel);
                 this._commentsLabel.node.setAttribute("class", "field-no-user-select");
                 this._commentsLabel.alignTop = true;
